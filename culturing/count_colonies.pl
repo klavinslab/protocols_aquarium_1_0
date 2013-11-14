0977f630458457e4bdadda41028947925655aacb
sunshineyyy <yaoyu.yang88@gmail.com>
2013-11-13 21:59:58 -0800

    update the count colonies

--- culturing/count_colonies.pl
+++ culturing/count_colonies.pl
@@ -10,5 +10,20 @@ for the plate and use a software to process the image to get the count"
 	end
 	
 	step
+		description: "Take the plates out from the incubator"
+	end
+
+	step
+		description: "Image the plate with lid off"
+	end
+
+	step
+		description: "Put the picture into software and get the count"
+		getdata
+			x: number, "Enter the count you got from the software"
+		end
+	end
+
+
 	
 	
