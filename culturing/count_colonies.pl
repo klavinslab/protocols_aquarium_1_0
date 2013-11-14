22dad93928e8377ac236abb6e1863fe40b987e1d
sunshineyyy <yaoyu.yang88@gmail.com>
2013-11-13 21:42:33 -0800

    update

--- culturing/count_colonies.pl
+++ culturing/count_colonies.pl
@@ -4,9 +4,9 @@
 
 	step
 		description: "This protocol describes how to take a plate from an incubator and 
-									count number of colonies on the plate"
+count number of colonies on the plate"
 		note: "In this protocol you need to use camera to take picture
-					for the plate and use a software to process the image to get the count"
+for the plate and use a software to process the image to get the count"
 	end
 	
 	
