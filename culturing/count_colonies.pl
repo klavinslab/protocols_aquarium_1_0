9499f579346bf904d8292799a11bfd62aa19e0f4
sunshineyyy <yaoyu.yang88@gmail.com>
2013-11-13 22:17:44 -0800

    update

--- culturing/count_colonies.pl
+++ culturing/count_colonies.pl
@@ -4,23 +4,26 @@
 
 	step
 		description: "This protocol describes how to take a plate from an incubator and 
-count number of colonies on the plate"
+count number of colonies on the plate."
 		note: "In this protocol you need to use camera to take picture
-for the plate and use a software to process the image to get the count"
+for the plate and use a software to process the image to get the count."
 	end
 	
 	step
-		description: "Take the plates out from the incubator"
+		description: "Take the plates out from the incubator."
 	end
 
 	step
-		description: "Image the plate with lid off"
+		description: "Image the plate with lid off."
+		note: "Place the plate inverted on the transilluminator, take the lid off, put the camera case on and take a picture by centering around the plate."
 	end
 
 	step
-		description: "Put the picture into software and get the count"
+		description: "Put the picture into software and get the count."
+		note: "Wait several seconds for the picture uploaded to Dropbox, open Dropbox/GelImages, under today's date file, you will find the picutre you just took.\n
+		Open the software, drag the picture into the software and wait for the software to count the colonies"
 		getdata
-			x: number, "Enter the count you got from the software"
+			x: number, "Enter the count you got from the software."
 		end
 	end
 
