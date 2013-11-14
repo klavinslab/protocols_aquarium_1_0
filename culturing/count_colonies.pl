24199db08b54ae901c64f73a1892adf1b69cc93c
sunshineyyy <yaoyu.yang88@gmail.com>
2013-11-13 21:25:17 -0800

    creat count_colonies

---
+++ culturing/count_colonies.pl
@@ -0,0 +1,12 @@
+	argument
+		x: sample, "Choose the plate you incubated yesterday"
+	end
+
+	step
+		description: "This protocol describes how to take a plate from an incubator and 
+									count number of colonies on the plate"
+		note: "In this protocol you need to use camera to take picture
+					for the plate and use a software to process the image to get the count"
+	end
+	
+	
