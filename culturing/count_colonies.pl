ba8448f7c8737716598569c77b7f454d22043cf1
sunshineyyy <yaoyu.yang88@gmail.com>
2013-11-17 17:01:34 -0800

    update count_colonies.pl

--- culturing/count_colonies.pl
+++ culturing/count_colonies.pl
@@ -15,20 +15,33 @@ for the plate and use a software on the plate reader computer to process the ima
 
 	step
 		description: "Image the plate with lid off."
-		note: "Place the plate inverted on the transilluminator, take the lid off, put the camera hood on, turn on the transilluminator, and take a picture by centering around the plate."\n
-		You need to set the camera at manual focus, zoom in to center around the plate until the whole plate takes up the screen. Then click the flower button, choose macro, set the focus at 2, move the box to the leftmost and take the picture.
+		note: "Place the plate inverted on the transilluminator, take the lid off, put the camera hood on, turn on the transilluminator, and take a picture by centering around the plate.\n
+		You need to set the camera at manual focus, zoom in to center around the plate until the whole plate takes up the screen. Then click the flower button, choose macro. Set the focus at 2, move the box to the leftmost and take the picture."
 	end
 
 	step
 		description: "Put the picture into software and get the count."
 		note: "Wait several seconds for the picture uploading to Dropbox, open Dropbox/GelImages, under today's date folder, you will find the picture you just took.\n
 		Open the software, drag the picture into the software and wait for the software to count the colonies"
+	end
+	step
+		description: "Record the count."
+		note: "If the software recognizes the coloines correctly and give a reasonbale count, record that number in below. If not, count the number of colonies by dividing up the plate in four regions, get the count in each region and sum up as the final count." 
 		getdata
-			count: number, "Enter the count you got from the software."
+			count: number, "Enter the count you got from software or human counting."
 		end
 	end
+
+	step
+		description: "Store the plate in 4C fridge."
+		note: "Turn off the transillumniator and camera, take the plate from transilluminator, wrap up the plate with parafilm and put it the in the fridge located at D2.100."
+	end
 	
-	release y
+	modify
+ 		y[0]
+		location: "D2.100"
+		inuse: 0
+	end
 
 
 	
