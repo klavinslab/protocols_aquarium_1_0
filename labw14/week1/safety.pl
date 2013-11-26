cf57d8a3c8cf6b3a8b07f54a26eb7c3209c5fe1f
Yaoyu Yang <yaoyu.yang88@gmail.com>
2013-11-26 09:25:15 -0800

    Create safety.pl

---
+++ labw14/week1/safety.pl
@@ -0,0 +1,10 @@
+information "This protocol teaches the essential safety practices and procedures of the lab."
+step
+  description:
+  "No eating or drinking in the lab."
+  note:
+  "This is a wet lab that works with live cultures. Do not bring food or drinks into the lab, 
+  do not apply cosmetics in the lab, and do not make any other type of contact with mucous 
+  (ex. contact lenses or touching one's mouth)."
+  image: "sign_caution-chem-no-eat-drinking-area1-lg"
+end
