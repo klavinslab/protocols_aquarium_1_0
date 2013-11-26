0baf88bcbab11730c83436916dc4130e41292359
Yaoyu Yang <yaoyu.yang88@gmail.com>
2013-11-26 10:06:44 -0800

    Update safety.pl

--- labw14/week1/safety.pl
+++ labw14/week1/safety.pl
@@ -8,3 +8,101 @@ step
   (ex. contact lenses or touching one's mouth)."
   image: "sign_caution-chem-no-eat-drinking-area1-lg"
 end
+
+step
+  description:
+  "You must wash your hands"
+  note:
+  "Before leaving the lab, immediately following contact with potentially infectious agents, 
+  and immediately after taking off gloves or other protective equipment."
+  image: "handwashing_sink_without_map"
+end
+
+step
+  description:
+  "In case of a fire be able to locate AND use fire extinguisher."
+  note:
+  "Be aware when using flammable reagents. In case of a serious fire call 911. No smoking in the lab."
+  image: "Fire_Extinguisher_without_map"
+end
+
+step
+  description:
+  "In case of a serious chemical spill be able to locate and operate both the eye wash station and chemical shower."
+  note:
+  "Familiarize yourself with the directions."
+  image:"shower_and_eyewash_without_map"
+end
+
+step
+  description:
+  "All biologically contaminated disposables must be disposed in the biohazard waste."
+  note:
+  "This includes items such as pipette tips, petri dishes, and gloves. Do not dispose of sharps in the biohazard waste bag."
+  image:"Biohazard_Box_without_map"
+end
+
+step
+  description:
+  "When using flammable, acidic, or basic reagents the chemicals MUST be used within the fume hood with the sash as low as possible."
+  note:
+  "Always wear gloves when using ANY regeants in the fume hood. Be aware of which chemicals are present in the hood while doing any work within the fume hood."
+  image:"Fume_Hood_without_map"
+end
+
+step
+  description:
+  "In case of a cut, burn, or other health concern that arises while in the lab use one of the first aid kits."
+  note:
+  "Familiarize yourself with the contents and locations of each first aid kit."
+  image:"First_aid_kits_without_map"
+end
+
+step
+  description:
+  "All broken glassware must be disposed of in broken glass container."
+  note:
+  "Never place any broken glassware in the biohazard waste box, trash cans, or any other recepticles."
+  image:"Sharps_Container_without_map"
+end
+
+step
+  description:
+  "Sharps, such as needles, scalpels, microscope slides, razor blades etc. must be disposed of in sharps container."
+  note:
+  "Never place any sharps in the biohazard waste box, trash cans, or any other recepticles."
+  image:"Sharps_Container_without_map"
+end
+
+step
+  description:
+  "All dirty glassware should be cleaned at the dishwashing station"
+  note:
+  " Do not place dirty glassware with clean items that may be hung and drying or drying in the drying area. To clean up /decontaminate a biohazard spill, first dispose of biohazard waste in biohazard bag and then to decontaminate a surface or disposable allow the object in question to soak in 10 percent bleach solution for 20min then dispose of bleached waste in regular waste stream."
+  image:"dishwashing_sink_without_map"
+end
+
+step
+  description:
+  "UV Shield must be used when using UV transilluminator."
+  note:
+  "Do not look directly at or expose skin to UV light as it may cause permanent damage."
+  image:"UV_transilluminator_without_map"
+end
+
+step
+  description:
+  "Gloves must be used when using potentially dangerous chemicals."
+  note:
+  "Dispose of goves after each use when handling dangerous chemicals and/or potentially infectious agents. Immediately replace punctured or torn gloves. All gloves must be disposed of in the biohazard box. "
+  image:"Latex_Gloves_without_map"
+end
+
+step
+  description:
+  "Eye Protection"
+  note:
+  "Eye Protection must be: used during any activites with risk of splashing or aerosolization and as required by laboratory instructor."
+end
+
+
