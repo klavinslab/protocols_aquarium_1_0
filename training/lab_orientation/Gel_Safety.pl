b2cd513818ebcceda32ef26041ffbd948e7017f7
cotem2 <cotem2@uw.edu>
2013-12-04 15:30:39 -0800

    Create Gel_Safety.pl

---
+++ training/lab_orientation/Gel_Safety.pl
@@ -0,0 +1,53 @@
+step
+	description:"Gels and Gel Room Safety"
+	note: "This protocol will teach you safety measures that need
+	to be taken when working in with gels or in the gel room."
+	end
+
+step
+	description: "Follow regular lab safety"
+	note: "Do not touch your mouth or eyes and always wash your hands after
+	working with gels or touching anything in the gel room."
+	end
+
+step
+	description: "Always wear gloves"
+	note: "Gloves should be worn at all times when working with gels or
+	in the gel room. Do not touch anything with your bare skin if it
+	has ever touched the gel. Assume that everything in the gel room
+	has been touched by a gel. The reason for this is that the dye used with
+	gels is known to cause cancer. Use extreme caution."
+	image: "Latex_Gloves_without_map"
+	end
+
+step
+	description: "Always wear goggles when using the transilluminator"
+	note: "The transilluminator is used to visualize
+	DNA bands and uncomfortable to look at with the naked eye. One of
+	the trasilluminators uses UV light which is unhealthy for your eyes 
+	so make sure to use appropriate goggles."
+	image: "transilluminatorgoggles"
+	end
+
+step
+	description: "Razor Safety"
+	note: "Be careful when working with razors. Always wash your hands immediately
+	if you cut your glove or cut your hand. For cuts, use the first aid kit which 
+	is located at B7.500. Razor blades should be disposed of in the sharps container."
+	image: "Razor_Blades"
+	end
+
+step
+	description: "Power Supply Safety"
+	note: "Make sure the voltage is off before plugging
+	anything into the power supply."
+	image: "Power_Supplies"
+	end
+
+step
+	description: "Use the Biohazard trash to dispose of gels"
+	note: "When finished with a gel, make sure to dispose of it in
+	the appropriate biohazard trash."
+	image: "Biohazard_Box_without_map"
+	end
+end 
