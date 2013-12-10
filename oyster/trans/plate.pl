00fde98371806d1035efc086912be6078afadb84
Yaoyu Yang <yaoyu.yang88@gmail.com>
2013-12-09 17:11:10 -0800

    Update plate.pl

--- oyster/trans/plate.pl
+++ oyster/trans/plate.pl
@@ -28,8 +28,8 @@ end
 
 step
   description: "Transfer the sample to the center of the plate"
-  note: "Invert the plate so the beads are on the agar surface. 
-         Add %{volume} µL of sample using the P1000 micropipettor 
+  note: "Invert the plate so the beads are on the agar surface. Lift the lid and 
+         pipette %{volume} µL of sample on the agar surface
          and put the lid back on the plate."
   warning: "Do not place the plate lid on lab bench while you do this."
   image: "pipette_culture_on_plate"
@@ -37,7 +37,7 @@ end
 
 step
   description: "Shake the plate to spread the sample over the surface."
-  note: "Use 3-4 sharp shakes, then pour the beads out into the waste bead container. 
+  note: "Use 4 sharp shakes, turn 90 degrees between two shakes, then pour the beads out into the waste bead container. 
          When done, place the plate lid down on the bench in preparation for incubation."
   image: "spread_beads_on_plate"
 end
