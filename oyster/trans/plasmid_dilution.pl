5eea597a1b80595bba23c0cfe6fb6e6f56d481e8
Yaoyu Yang <yaoyu.yang88@gmail.com>
2013-12-18 15:18:02 -0800

    Update plasmid_dilution.pl

--- oyster/trans/plasmid_dilution.pl
+++ oyster/trans/plasmid_dilution.pl
@@ -33,7 +33,7 @@ step
   description: 
     "Take out %{count} 1.5 mL tube"
   note: 
-    "Add 10 µL of diluted plasmid from tube D to each tube."
+    "Add %{aliquot} µL of diluted plasmid from tube D to each tube."
 end
 
 x = 0
