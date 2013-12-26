e58f901002700538ad6dc4934bd085c13d3b0e55
Nick Bolten <nbolten@gmail.com>
2013-12-26 15:12:59 -0800

    roadmap for printing subprotocol

--- plankton/includes/labeling/print.pl
+++ plankton/includes/labeling/print.pl
@@ -2,7 +2,12 @@
 # http://bioturk.ee.washington.edu:3008/devices/4e5becf4-d281-03d3-cfbc-6aab729a7fb5/command?cmd=print&text=Hello&qrcode=World
 # TODO:
 # (1) Get basic http requests working properly - arguments aren't being sent?
-# (2) Send
+# (2) Get printer text + qrcodes working
+# (3) Make this file print subprotocol with qrcode/text arguments
+# (4) Use in labeling protocol
+# (5) Troubleshoot possible bugs:
+#     barcodes don't work
+#     a request sent to /devices/<uuid>/command, without arguments or with bad argument, returns helpful text but not useful error code
 
 #argument
 #  text: string, "Text to be printed on the label. Cannot be blank."
