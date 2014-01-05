d2e81cad8cd0f1594fdf941c80dc6bfc9aff13dc
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-04 21:46:16 -0800

    Update incubate.pl

--- labw14/week1/trans/incubate.pl
+++ labw14/week1/trans/incubate.pl
@@ -1,20 +1,28 @@
 argument
-  e_coli_strain_id: sample, "The plate to be incubated."
+  e_coli_strain_id_1: sample, "The first plate to be incubated."
+  e_coli_strain_id_2: sample, "The second plate to be incubated"
   temp: number, "The temperature (in °C) at which to incubate the plate."
 end
 
 take
-  y = item e_coli_strain_id
+  y1 = item e_coli_strain_id_1
+  y2 = item e_coli_strain_id_2
 end
 
 step
-  description: "Put the plate in the %{temp}°C incubator, location is B14.310."
-  note: "Incubate the plate upside-down (lid on the bottom) to prevent condensation."
+  description: "Put the plates in the %{temp}°C incubator, location is B14.310."
+  note: "Incubate the plates upside-down (lid on the bottom) to prevent condensation."
   image: "put_plate_incubator"
 end
 
 modify
-  y[0]
+  y1[0]
+  location:"B14.310"
+  inuse:0
+end
+
+modify
+  y2[0]
   location:"B14.310"
   inuse:0
 end
