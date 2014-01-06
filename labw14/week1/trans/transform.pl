dad14f81a84d2915cf3c6ae9877fd76b70fd8155
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-05 21:44:19 -0800

    Update transform.pl

--- labw14/week1/trans/transform.pl
+++ labw14/week1/trans/transform.pl
@@ -106,5 +106,5 @@ log
   return: { transformed_cells_id: r[:id], tube_incubation_choice : tube_incubation_choice }
 end
 
-release [falcon_tube[0],plasmid[0],iceblock[0],strain[0],alrack[0],cuvette[0]]
+release [falcon_tube[0],plasmid[0],alrack[0],iceblock[0],strain[0],cuvette[0]]
 
