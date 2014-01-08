6d17b0942b94453c54e49384bc5907fd179b69fe
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-08 13:46:41 -0800

    Update dispose_tube.pl

--- labw14/week1/trans/dispose_tube.pl
+++ labw14/week1/trans/dispose_tube.pl
@@ -1,8 +1,8 @@
 argument
-   transformed_E_Coli_Strain_id: sample, "A sample"
+   Transformed_E_Coli_Strain_id: sample, "A sample"
 end
 take
-  strain = item transformed_E_Coli_Strain_id
+  strain = item Transformed_E_Coli_Strain_id
 end
 
 release [strain[0]]
