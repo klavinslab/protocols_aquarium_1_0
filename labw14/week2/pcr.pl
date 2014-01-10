579f641f466d6d352997dea673a2890e5051822b
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-10 02:46:04 -0800

    Update pcr.pl

--- labw14/week2/pcr.pl
+++ labw14/week2/pcr.pl
@@ -1,7 +1,16 @@
 argument
-
+  primer1: sample, "The first primer"
+  primer2: sample, "The second primer"
+  plasmid_id: sample, "The plasmid stock"
 end
 
 step
     description: "PCR Protocol"
 end
+
+produce
+  r = 1 "PCR Result" of "fLAB2"
+  note: "Check the upcoming job time. (no need to edit the location below)"
+  location: "Thermal Cycler"
+end
+
