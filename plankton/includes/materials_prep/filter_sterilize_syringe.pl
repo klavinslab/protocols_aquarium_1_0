1e891b71ae7606f6c6c4376f55c9145b1e33b5b5
Nick Bolten <nbolten@gmail.com>
2014-01-09 17:08:20 -0800

    Fix to filter sterilization protocol, first draft of 20% glucose protocol

--- plankton/includes/materials_prep/filter_sterilize_syringe.pl
+++ plankton/includes/materials_prep/filter_sterilize_syringe.pl
@@ -1,6 +1,6 @@
 argument
   volume: number, "The volume to sterilize in mL (maximum of 30 mL)."
-  name: string, "The name of the solution being sterilized."
+  name: string, "The name of the solution being sterilized, e.g. 'the solution'."
 end
 
 
@@ -13,14 +13,15 @@ end
 
 step
   description: "Draw up %{volume} mL of %{name} into the syringe and attach the filter"
-  note: "Remove the backing from the filter package so the filter is easy to access.\n\nUsing the syringe (without filter), draw up %{volume} mL of %{name}. Screw the syringe onto the filter."
+  note: "Loosen the cap of the falcon tube. Remove the backing from the filter package so the filter is easy to access.\n\nUsing the syringe (without filter), draw up %{volume} mL of %{name}. Screw the syringe onto the filter."
   warning: "Never touch the bottom or sides of the filter - they must remain sterile."
 end
 
 
 step
   description: "Dispense into falcon tube"
-  note: "Dispense the solution through the filter into the falcon tube. Do not put any pressure on the falcon tube itself while doing so. When done dispensing, replace the falcon tube cap."
+  note: "Dispense the solution through the filter into the falcon tube. When done dispensing, replace the falcon tube cap."
+  warning: "Do not touch the filter to the falcon tube."
 end
 
 
