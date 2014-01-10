585866874b749d4d25b5c76585ed721b2dc360d0
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-10 15:28:16 -0800

    Update image_cut_gel.pl

--- labw14/week2/image_cut_gel.pl
+++ labw14/week2/image_cut_gel.pl
@@ -1,3 +1,12 @@
 argument
+  Gel_Lane_id: sample, "The gel lane you got after run the gel"
+end
 
+produce
+  r = "Gel Slice in 1.5 mL tube" of "fLAB2"
+  location = "Bench"
+end
+
+log
+  return: {Gel_Slice_id: r[:id]}
 end
