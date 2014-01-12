bd603a7bd3c04b68f0ec6e7e287a363b8a90bda0
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-11 17:12:48 -0800

    Update pcr_finish.pl

--- labw14/week2/pcr_finish.pl
+++ labw14/week2/pcr_finish.pl
@@ -1,4 +1,14 @@
-argument
-
+step
+  description: "Go to the thermal cycler to pick up the PCR tube you put in the previous pcr_prepare protocol."
+  note: "Put on you bench after you picked up the PCR tube"
 end
 
+produce
+  r = 1 "PCR Result" of "fLAB2"
+  note: "Keep the tube on the bench to use in the next protocol. (no need to edit the location below)"
+  location: "Bench"
+end
+
+log
+  return: { PCR_Result_id: r[:id]}
+end
