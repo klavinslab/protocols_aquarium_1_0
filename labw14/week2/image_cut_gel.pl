1e152388570874af573e33cd3e07a0510eb21edf
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-11 17:28:41 -0800

    Update image_cut_gel.pl

--- labw14/week2/image_cut_gel.pl
+++ labw14/week2/image_cut_gel.pl
@@ -2,6 +2,34 @@ argument
   Gel_Lane_id: sample, "The gel lane you got after run the gel"
 end
 
+step
+  description: "Wear the UV protection glass"
+end
+
+step
+  description: "Take the gel out of gel box and put it on transilluminator"
+end
+
+step
+  description: "Turn on the transilluminator, put on the camera hood"
+end
+
+step
+  description: "Turn on the camera, center around the gel and take a picture"
+end
+
+step
+  description: "Remove the camera hood, cut out the gel slice with length 1799bp"
+end
+
+step
+  description: "Put the gel slice into a 1.5 mL tube"
+end
+
+step
+  description: "Clean up the transilluminator and gel station"
+end
+
 produce
   r = "Gel Slice" of "fLAB2"
   location = "Bench"
