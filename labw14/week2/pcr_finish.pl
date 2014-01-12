bad57b1020b26d9c864d21286c9d91d6d6f67e5d
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-12 02:06:11 -0800

    Update pcr_finish.pl

--- labw14/week2/pcr_finish.pl
+++ labw14/week2/pcr_finish.pl
@@ -1,6 +1,6 @@
 step
   description: "Go to the thermal cycler to pick up the PCR tube you put in the previous pcr_prepare protocol."
-  note: "Put on you bench after you picked up the PCR tube"
+  note: "Put it on you bench after you picked up the PCR tube"
 end
 
 produce
