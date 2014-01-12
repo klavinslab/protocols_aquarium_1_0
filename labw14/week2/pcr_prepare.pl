b2de1da73178489ee08f216b496f2287c395a30d
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-12 01:53:29 -0800

    Update pcr_prepare.pl

--- labw14/week2/pcr_prepare.pl
+++ labw14/week2/pcr_prepare.pl
@@ -1,12 +1,37 @@
 argument
-  primer1: sample, "The first primer"
-  primer2: sample, "The second primer"
+  primer_1_id: sample, "The first primer"
+  primer_2_id: sample, "The second primer"
   plasmid_id: sample, "The plasmid stock"
 end
 
 step
-    description: "This protocol prepares the PCR mix in PCR tube and starts it in thermal cycler"
+  description: "This protocol prepares the PCR mix in PCR tube and starts it in thermal cycler"
 end
+
+step
+  description: "Label a 0.2mL PCR tube. Write your initials on it."
+end
+
+step
+  description: "Pipet 19 µL molecular grade water into the labeled PCR tube."
+end
+
+step
+  description: "Pipet 1 µL of plasmid with id %{plasmid_id} into the tube."
+end
+
+step
+  description: "Pipet 2.5 µL of primer with id %{primer_1_id} into the tube."
+end
+
+step
+  description: "Pipet 2.5 µL of primer with id %{primer_2_id} into the tube."
+end
+
+step
+  description: "Pipet 25 µL of 2X Phusion Master Mix into the tube."
+end
+
 
 
 
