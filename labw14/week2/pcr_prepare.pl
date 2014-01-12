7cd58465d8b991ec6e2340930dfbfce01865bb18
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-11 17:04:21 -0800

    Update pcr_prepare.pl

--- labw14/week2/pcr_prepare.pl
+++ labw14/week2/pcr_prepare.pl
@@ -8,13 +8,5 @@ step
     description: "PCR Protocol"
 end
 
-produce
-  r = 1 "PCR Result" of "fLAB2"
-  note: "Check the upcoming job time. (no need to edit the location below)"
-  location: "Thermal Cycler"
-end
 
-log
-  return: { PCR_Result_id: r[:id]}
-end
 
