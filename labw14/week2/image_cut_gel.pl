0dc2fb110704576a55d517d8a7202ad92f02efcc
David Zong <davidzng@uw.edu>
2014-01-14 15:03:50 -0800

    Update image_cut_gel.pl

--- labw14/week2/image_cut_gel.pl
+++ labw14/week2/image_cut_gel.pl
@@ -24,7 +24,7 @@ step
 end
 
 step
-  description: "Turn on the transilluminator, put on the camera rig"
+  description: "Turn on the transilluminator, put on the camera hood"
   note: "The on switch for the transilluminator is located on the side."
 end
 
@@ -59,7 +59,7 @@ step
 end
 
 step
-  description: "Remove the camera rig, cut out the gel slice with length 1799bp" 
+  description: "Remove the camera hood, cut out the gel slice with length 1799bp" 
   note: "It may help to turn the lights off during this step. Use the ladder to locate where
   1799bp is. Use the razor blade to cut around the band. Try to remove as much excess gel
   as possible. Do not remove any gel that contains the band as this will limit the amount
