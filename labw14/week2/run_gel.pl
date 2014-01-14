00caf271952b5916b8076b97c1f5e95ce1031521
Nick Bolten <nbolten@gmail.com>
2014-01-14 15:29:21 -0800

    images for running gel

--- labw14/week2/run_gel.pl
+++ labw14/week2/run_gel.pl
@@ -19,6 +19,7 @@ end
 step
   description: "Set up the power supply."
   note: "In the gel room, obtain a power supply and set it to 100 V and with a 40 minute timer.\n\nAttach the electrodes of an appropriate gel box lid to the power supply."
+  image: "gel_power_settings"
 end
 
 
@@ -28,6 +29,7 @@ step
   bullet: "Fill the gel box with 1X TAE from A5.500 until it just covers the center of the gel box."
   bullet: "With the gel box electrodes facing away from you, place the gel (in casting tray) back in the gel box such that the top lane is on your left."
   bullet: "Carefully remove the comb(s)."
+  image: "gel_fill_TAE_to_line"
 end
 
 
@@ -35,6 +37,7 @@ dye_volume = fragment_volume / 5.0
 step
   description: "Add loading dye to the PCR fragment"
   note: "Using a 10 µL or 100 µL pipetter, add %{dye_volume} µL of loading dye to the PCR results with id %{fragment_one}."
+  image: "gel_add_loading_dye"
 end
 
 
@@ -43,20 +46,23 @@ release loading_dye
 
 step
   description: "Load the ladder"
-  note: "Using a 100 µL pipetter, pipet 10 µL of ladder (containing loading dye) with id %{ladder} into the first (top-left) well of the gel."
+  note: "Using a 100 µL pipetter, pipet 10 µL of ladder (containing loading dye) with id %{ladder_one} into the first (top-left) well of the gel."
+  image: "gel_begin_loading"
 end
 
 
 step
   description: "Load the PCR"
   note: "Using a 100 µL pipetter, pipet %{fragment_volume} µL of the PCR results (containing loading dye) into the second well of the gel."
+  image: "gel_begin_loading"
 end
 
 
 step
   description: "Start electrophoresis"
-  note: "Carefully attach the gel box lid to the gel box, being careful not to bump the samples out of the wells. Attach the red electrode to the red terminal of the power supply, and the black electrode to the neighboring black terminal. Hit the start button on the gel boxes - usually a small running person icon."
+  note: "Carefully attach a gel box lid from A7.525 to the gel box, being careful not to bump the samples out of the wells. Attach the red electrode to the red terminal of the power supply, and the black electrode to the neighboring black terminal. Hit the start button on the gel boxes - usually a small running person icon."
   warning: "Make sure the power supply is not erroring (no E* messages) and that there are bubbles emerging from the platinum wires in the bottom corners of the gel box."
+  image: "gel_check_for_bubbles"
 end
 
 
