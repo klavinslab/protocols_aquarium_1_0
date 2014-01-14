fcaeeb53e1b43cc4c517b0f9045c02e29bfe49d1
Nick Bolten <nbolten@gmail.com>
2014-01-14 12:22:19 -0800

    Added TAE pouring out step to cleaning of gel box

--- labw14/week2/clean_gel_box.pl
+++ labw14/week2/clean_gel_box.pl
@@ -3,6 +3,7 @@ information "Clean the gel box"
 step
   description: "Clean the gel box"
   bullet: "Throw away any remaining gel pieces in a biohazard container."
+  bullet: "Pour out TAE into the sink at A8."
   bullet: "Rinse all parts of the gel box and casting stray in the sink at A8. Replace the casting tray so it's ready for pouring."
   bullet: "Remove excess moisture with a paper towel."
 end
