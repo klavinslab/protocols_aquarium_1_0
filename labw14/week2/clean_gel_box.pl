26209ebf2d0d0951a03c1a6876ee197ac4afaae1
Nick Bolten <nbolten@gmail.com>
2014-01-14 11:45:10 -0800

    Cleaning gel box protocol - initial commit

---
+++ labw14/week2/clean_gel_box.pl
@@ -0,0 +1,13 @@
+information "Clean the gel box"
+
+step
+  description: "Clean the gel box"
+  bullet: "Throw away any remaining gel pieces in a biohazard container."
+  bullet: "Rinse all parts of the gel box and casting stray in the sink at A8. Replace the casting tray so it's ready for pouring."
+  bullet: "Remove excess moisture with a paper towel."
+end
+
+
+produce
+  gel_box = 1 "49 mL Gel Box With Casting Tray (clean)"
+end
