9173bb582b5cf1ac12f10bfdcc274fb0de06b55c
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-15 00:31:32 -0800

    Update gibson_finish.pl

--- labw14/week2/gibson_finish.pl
+++ labw14/week2/gibson_finish.pl
@@ -1,14 +1,8 @@
 step
   description: "Pick up the tube you incubated on the 50 C heat block"
-  note: "Place on your bench after you picked up"
+  note: "Place in the M20 box slot specified in next page."
 end
 
 produce
   r = 1 "Gibson Reaction Result" of "pLAB3"
-  note: "Keep the tube on the bench to use in the next protocol. (no need to edit the location below)"
-  location: "Bench"
-end
-
-log
-  return: { Gibson_Reaction_Result_id: r[:id]}
 end
