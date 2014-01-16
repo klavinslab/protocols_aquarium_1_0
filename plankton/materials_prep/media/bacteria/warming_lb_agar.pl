61155de004f520128af21ab0994d95b794396ec4
Nick Bolten <nbolten@gmail.com>
2014-01-15 16:01:13 -0800

    Removed options from warming_lb_agar

--- plankton/materials_prep/media/bacteria/warming_lb_agar.pl
+++ plankton/materials_prep/media/bacteria/warming_lb_agar.pl
@@ -1,19 +1,13 @@
 information "Warm up a bottle of solid agar in a microwave to a molten state so that it can be poured into plates."
 
 
-# TODO:
-# 1) Make this just for LB (for now)
-# 2) Add ~same arguments as for bacteria/pour_lb_plates
-# 3) At the end, instead of producing anything, include the pour_lb_plates protocol
 # Chose media type
 step
   description: "What type of agar?"
   note: "Choose the type of agar to warm up."
   getdata
     media_type: string, "Media Type", ["200 mL LB Agar (sterile)",
-                                       "400 mL LB Agar (sterile)",
-                                       "200 mL SDO Agar (sterile)",
-                                       "200 mL YPAD Agar (sterile)"]
+                                       "400 mL LB Agar (sterile)"]
   end
 end
 
