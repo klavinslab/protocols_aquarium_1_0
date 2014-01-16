b7359111b6f3fab81c2ebe747701ef8d8c2fc494
Nick Bolten <nbolten@gmail.com>
2014-01-15 16:11:49 -0800

    Checkboxes in melting agar subprotocol

--- plankton/includes/materials_prep/melt_agar.pl
+++ plankton/includes/materials_prep/melt_agar.pl
@@ -1,13 +1,13 @@
- 
+
 argument
   media: string, "The media type that is being warmed up."
   power: number, "The power level to set the microwave to."
-end  
+end
 
 step
   description: "Insert media in microwave."
   note: "Put %{media} in Microwave M1. Make sure to loosen the cap."
-end 
+end
 
 step
   description: "Set the time and power."
@@ -17,12 +17,16 @@ end
 
 step
   description: "Swirl the media."
-  note: "Using the %{gloves}, take the %{media} out of the microwave carefully. Take of the cap for a few seconds and let some heat be released. Replace the cap, and carefully swirl."
-  warning: "Don't let it bubble over!"  
-end 
+  note: "Using the gloves, take the %{media} out of the microwave carefully. Take off the cap for a few seconds and let some heat be released. Replace the cap, and carefully swirl."
+  warning: "Swirl over the bench. Don't let it bubble over!"
+end
 
 step
-  description: "Repeat this process."
-  note: "Repeat this process for 1 minute 30 seconds, then 1 minute, then 1 minute, then 30 seconds, then 30 seconds until the agar is completely melted."
-  warning: "This is an approximation. To check if the agar is completely melted, CAREFULLY hold the agar up to the light and swirl. If no solid agar is present, it is done."
-end 
+  description: "Continue microwaving until molten"
+  check: "Microwave 1 minute 30 seconds at power %{power}%, then swirl"
+  check: "Microwave 1 minute at power %{power}%, then swirl"
+  check: "Microwave 1 minute at power %{power}%, then swirl"
+  check: "Microwave 30 seconds at power %{power}%, then swirl"
+  check: "Microwave 30 seconds at power %{power}%, then swirl"
+  warning: "To check if the agar is completely melted, CAREFULLY hold the agar up to the light and swirl. If no solid agar is present, it is done."
+end
