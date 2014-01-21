6bbf963806cccf12e2a3083204e6ac4268d5f067
David Zong <davidzng@uw.edu>
2014-01-20 18:01:11 -0800

    fixed indenting

--- labw14/week3/make_cell_lysate.pl
+++ labw14/week3/make_cell_lysate.pl
@@ -8,23 +8,23 @@ end
 
 step
   description: "Label two 0.2mL PCR tubes. Write your initials on both."
-	note: "On one tube write CW for cell water and on the other write B for boil"
+  note: "On one tube write CW for cell water and on the other write B for boil"
 end
 
 step
   description: "Prepare cell water solution"
   note: "This step makes an isotonic solution to temporarly keep cells in while you do the colony PCR"
-	check: "Add 40 µL of PBS (phosphate buffered saline) in the cell water tube"
-	check: "Use 10 µL pipette tip to pick a colony from the plate and add into the tube with swirl the tips several times."
+  check: "Add 40 µL of PBS (phosphate buffered saline) in the cell water tube"
+  check: "Use 10 µL pipette tip to pick a colony from the plate and add into the tube with swirl the tips several times."
 end
 
 step
-	description: "Transfer 20 µL cell water to boil tube"
+  description: "Transfer 20 µL cell water to boil tube"
 end
 
 step
-	description: "Place the boil tube in the thermal cycler"
-	note: "Find and initiate the boil protocol on the thermal cycler"
+  description: "Place the boil tube in the thermal cycler"
+  note: "Find and initiate the boil protocol on the thermal cycler"
 end
 
 # produce the cell water
