7be4ba33db3758b5f2356741ae7713c55b2a8b45
christakahashi <cnt@uw.edu>
2014-01-21 15:43:59 -0800

    added files for week 4.  mostly inherited from week2 so far.

---
+++ labw14/week4/dpnI_digest.pl
@@ -0,0 +1,48 @@
+argument
+  fragment: sample, "The purified fragment that needs to be digested"
+  neb: sample, "Choose NEB4 buffer"
+  bsa: sample, "Choose BSA buffer"
+  dpn: sample, "Choose DpnI enzyme"
+end
+
+take
+  y = item fragment
+  neb4 = item neb
+  bsa_buffer = item bsa
+end
+
+step
+  description: "Use a -20 C blue ice block tube holder to grab the enzyme"
+  note: "The enzyme are temperature sensitive."
+end
+
+take
+  dpn_enzyme = item dpn
+end
+
+step 
+  description: "Prepare DpnI digestion reaction"
+  check: "Pipet 18.2 µL molecular grade water into fragment tube with id %{fragment}."
+  check: "Pipet 5 µL NEB4 buffer with id %{neb} into the tube."
+  check: "Pipet 0.5 µL BSA buffer with id %{bsa} into the tube."
+  check: "Pipet 0.3 µL DpnI enzyme with id %{dpn} into the tube."
+  note: "Be careful to pipet into the liquid, not the side of the tube."
+end
+
+step
+  description: "Put the tube in a small green tube holder."
+  image: "green_tube_holder"
+end
+
+step
+  description: "Place in the 37°C incubator, location is B14.310."
+  image: "put_green_tube_holder_to_incubator"
+end
+
+modify
+  y[0]
+  location:"B14.310"
+  inuse:0
+end
+
+release [neb4[0],bsa_buffer[0],dpn_enzyme[0]]
---
+++ labw14/week4/pcr_prepare.pl
@@ -0,0 +1,55 @@
+argument
+  primer_f: sample array, "forward primer list"
+  primer_r: sample array, "reverse primer list"
+  plasmid_id: sample, "The plasmid stock"
+  enzyme_id: sample, "The Phsion HF Master Mix stock"
+end
+
+num_samples = length(primer_f)
+
+
+take
+  primer1_stock = item primer1
+  primer2_stock = item primer2
+  plasmid_stock = item plasmid_id
+  phusion_stock = item enzyme_id
+end
+
+step
+  description: "This protocol prepares the PCR mix in PCR tube and starts it in thermal cycler."
+end
+
+step
+  description: "Label %{num_samples} 0.2mL PCR tubes from 0 to %{num_samples}."
+end
+
+ii = 0
+while ii< num_samples
+  step
+    description: "Prepare Reaction"
+    check: "Pipet 19 µL molecular grade water into the labeled PCR tube."
+    check: "Pipet 1 µL of plasmid with id %{plasmid_id} into the tube."
+    check: "Pipet 2.5 µL of primer with id %{primer1} into the tube."
+    check: "Pipet 2.5 µL of primer with id %{primer2} into the tube."
+    check: "Pipet 25 µL of Phusion Master Mix with id %{enzyme_id} into the tube."
+    check: "Use the tip to gently mix."
+    note: "Be careful to pipette into the liquid, not the side of the tube."
+  end
+  ii = ii+1
+end
+
+step
+  description: "Place the tube into thermal cycler T2 at B3.335"
+end
+
+step
+  description: "Choose program on the thermal cycler and run"
+  note: ""
+  image: "thermal_cycler_home"
+end
+
+release primer_f
+release primer_r
+release [plasmid_stock[0],phusion_stock[0]]
+
+
