6a69ec0cb2bae859dc1142330149cdf14c4c2d8d
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-28 13:43:45 -0800

    Update quick_comp.pl

--- labw14/week4/quick_comp.pl
+++ labw14/week4/quick_comp.pl
@@ -27,12 +27,12 @@ end
 
 step
   description: "Add 1 ml ice cold sterile molecular grade water"
-  note: "Use your P-1000 add 1 ml ice cold molecular grade water and resuspend the pellet by gently pipetting up and down."
+  note: "Use your P-1000 (100-1000 μL pipettor) add 1 ml ice cold molecular grade water and resuspend the pellet by gently pipetting up and down."
 end
 
 step
   description: "Transfer into one of the prechilled 1.5ml centrifuge tube."
-  note: "Using your P-1000, transfer the culture form the 50ml falcon tube to the 1.5 ml centrifuge tube on ice."
+  note: "Using your P-1000 (100-1000 μL pipettor), transfer the culture form the 50ml falcon tube to the 1.5 ml centrifuge tube on ice."
 end
 
 ii = 0
@@ -63,7 +63,7 @@ end
 
 step
   description: "Make four aliquots"
-  note: "Transfer 50 µL of the resuspended pellet into each of the four remaining pre-chilled 1.5ml centrifuge tubes. Discard the source tube along with any remaining cells."
+  note: "Transfer 50 µL of the resuspended pellet into each of the four remaining pre-chilled 1.5 ml centrifuge tubes. Discard the source tube along with any remaining cells."
 end
 
 produce
