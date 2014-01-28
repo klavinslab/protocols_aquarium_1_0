330930574714d28c4c094c568615141516a27d7b
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-28 14:37:56 -0800

    Update and rename incubate.pl to incubate_tube.pl

---
+++ labw14/week4/incubate_tube.pl
@@ -0,0 +1,24 @@
+argument
+  e_coli_strain_id: sample, "The tube to be incubated."
+  temp: number, "The temperature (in °C) at which to incubate the tube."
+end
+
+take
+  y = item e_coli_strain_id
+end
+
+step
+  description: "Put the tube in a small green tube holder."
+        image: "green_tube_holder"
+end
+
+step
+  description: "Place in the %{temp}°C incubator, location is A1.110."
+        image: "put_green_tube_holder_to_incubator"
+end
+
+modify
+  y[0]
+  location:"A1.110"
+  inuse:0
+end
