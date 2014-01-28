f0b05ab6f8922b9776862074493d19d800231598
tamimeur <tamimeur@gmail.com>
2014-01-27 18:22:03 -0800

    Update transform.pl

--- labw14/week4/transform.pl
+++ labw14/week4/transform.pl
@@ -95,8 +95,8 @@ end
 
 produce
   r = 1 "Transformed E coli 1.5 mL tube" of "pLAB1 in Z1"
-  note: "Keep the tube on the bench to use in the next protocol. (no need to edit the location below)"
-  location: "Bench"
+  note: "Be sure to write down the item number on the tube and move it to the 30C incubator"
+  location: "A1.something"
 end
 
 log
