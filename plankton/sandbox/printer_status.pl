152321644a93aefd829dba6cb515d429cf840b18
Eric Klavins <klavins@uw.edu>
2014-01-28 14:20:07 -0800

    taking out comment

--- plankton/sandbox/printer_status.pl
+++ plankton/sandbox/printer_status.pl
@@ -1,5 +1,3 @@
-# Example query for printer:
-# http://bioturk.ee.washington.edu:3008/devices/4e5becf4-d281-03d3-cfbc-6aab729a7fb5/command?cmd=print&text=Hello&qrcode=World
 
 
 info_string = "info"
