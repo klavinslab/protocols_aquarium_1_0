ae427e953f657f787c694e93906dd6ce590806ac
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-01-28 13:43:15 -0800

    Update quick_comp.pl

--- labw14/week4/quick_comp.pl
+++ labw14/week4/quick_comp.pl
@@ -15,7 +15,7 @@ step
 end
 
 step
-  description: "Centrifuge at 4,600g  for 7 minutes at 4C"
+  description: "Centrifuge at 4,600 g  for 7 minutes at 4C"
   note: "While waiting for the centrifuge put five 1.5 ml tubes on ice."
 end
 
@@ -26,32 +26,32 @@ step
 end
 
 step
-  description: "Add 1ml ice cold sterile molecular grade water"
+  description: "Add 1 ml ice cold sterile molecular grade water"
   note: "Use your P-1000 add 1 ml ice cold molecular grade water and resuspend the pellet by gently pipetting up and down."
 end
 
 step
   description: "Transfer into one of the prechilled 1.5ml centrifuge tube."
-  note: "using your P-1000, transfer the culture form the 50ml falcon tube to the 1.5ml centrifuge tube on ice."
+  note: "Using your P-1000, transfer the culture form the 50ml falcon tube to the 1.5 ml centrifuge tube on ice."
 end
 
 ii = 0
 while ii<3
   step
-    description: "Centrifuge at 10,000 g for 30s at 4c"
-    note: "Centrifuge at 10,000g in refrigerated microcentrifuge for 30 s at 4 C."
+    description: "Centrifuge at 10,000 g for 30 s at 4 C"
+    note: "Centrifuge at 10,000 g in refrigerated microcentrifuge for 30 s at 4 C."
   end
 
   step
     description: "Remove supernatant"
     note: "Using your P-1000, carefully aspirate the supernatant and suspend cells in 1 ml of ice-cold molecular grade water."
-    warning: "the pellet will be very fragile!  Try not to disturbe it."
+    warning: "The pellet will be very fragile! Try not to disturbe it."
   end
 
   if ii<2
     step
-      description: "Add 1ml ice cold sterile molecular grade water"
-      note: "use your P-1000 add 1ml ice cold molecular grade water and resuspend the pellet by gently pipetting up and down."
+      description: "Add 1 ml ice cold sterile molecular grade water"
+      note: "Use your P-1000 add 1 ml ice cold molecular grade water and resuspend the pellet by gently pipetting up and down."
     end
   end
   ii = ii+1
@@ -63,7 +63,7 @@ end
 
 step
   description: "Make four aliquots"
-  note: "Transfer 50 ul of the resuspended pellet into each of the four remaining pre-chilled 1.5ml centrifuge tubes. Discard the source tube along with any remaining cells."
+  note: "Transfer 50 µL of the resuspended pellet into each of the four remaining pre-chilled 1.5ml centrifuge tubes. Discard the source tube along with any remaining cells."
 end
 
 produce
