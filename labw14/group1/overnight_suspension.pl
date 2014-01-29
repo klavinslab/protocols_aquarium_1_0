278f49f27084e8f8fc9fae1137da781b27ffd1b9
pollardt <pollardt@uw.edu>
2014-01-29 13:45:36 -0800

    Update overnight_suspension.pl

--- labw14/group1/overnight_suspension.pl
+++ labw14/group1/overnight_suspension.pl
@@ -8,7 +8,23 @@ take
   y = item Transformed_E_coli_Strain_id
 end
 
+step
+ description: "lable a 14 mL Test Tube with your initials and the date"
+end
 
+step
+ description: "use a 10uL pipette tip to transfer one colony from the plate into your test tube"
+ note: "carefully hold the test tube at a steep angle so the pipette tip can reach the fluid, swirl the pipette tip to mix"
+end
+
+produce
+ r = 1 "overnight suspension culture" of Transformed_E_coli_Strain_id
+ note: "Make sure to write the item number on the tube"
+ release test_tube
+end
+
+
+ 
 
 
 
