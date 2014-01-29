e916035fa8424a736f052e0ab0b8b1ded8e1a1b9
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-01-29 13:32:00 -0800

    Added fisrt step

--- labw14/group4/colony_transfer_pcpm.pl
+++ labw14/group4/colony_transfer_pcpm.pl
@@ -7,3 +7,10 @@
 
 argument
   Transformed_E_coli_Strain_id: sample, "Use the plate you transformed yesterday"
+end
+
+
+step
+  description: "Wear gloves"
+  note: "please put on a pair of gloves"
+end
