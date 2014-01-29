0f607457e5ab14929f870625c5bc97d92656d719
manewton <michael.andrew.newton@gmail.com>
2014-01-29 14:07:16 -0800

    Create overnight_suspension.pl

---
+++ labw14/group3/overnight_suspension.pl
@@ -0,0 +1,57 @@
+
+
+argument
+  Transformed_E_coli_Strain_id: sample, "Choose the plate you incubated yesterday"
+end
+
+take
+  colony_plate = item Transformed_E_coli_Strain_id
+  aliquot = 1 "50 mL LB liquid aliquot (sterile)"
+  test_tube = 1 "14 mL Test Tube"
+end
+
+step
+  description: 
+    "This protocol describes how to create an overnight colony suspension using the colonies of a previously incubated   plate"
+end
+
+step
+  description: "Label a 14 mL tube with your initials AND date"
+  note: "place the tube in a test tube rack.\n
+  This tube will eventually hold the transformed cells"
+end
+
+step
+  description: "Pipette 2 mL of LB into 14 mL tube"
+  note: "Use proper sterile technique. Use the 1000 μL pipetter."
+  bullet: "Set pipetter to 1000μL"
+  bullet: "loosen cap on LB aliquot"
+  bullet: "pipette 2 mL of LB into 14 mL tube"
+  bullet: "Recap test tube and LB"
+  warning: "Use new tip after every ejection"
+end
+
+step
+  description: "Add a colony to suspension"
+  note: "select desired colony prior to opening plate. Mark desired colony with circle and intials and date"
+  note: "Selection of your colony should be based on size (medium-big, not too big), isolated, and round in shape." 
+  bullet: "Take a sterile pipette tip, pick up your desired colony by gently scraping the tip to the colony."
+  bullet: "Tilt 14 mL tube such that you can reach the broth with your tip."
+  bullet: "Scrape colony into broth, using a swirling motion"
+  warning: "DON'T SPILL THE BROTH"
+end
+
+produce
+  s = 1 "Overnight suspension culture" from colony_plate[0]
+  note: "Place in 37 degree incubator at B13.425 for 18-24 hours"
+  location: "B13.425"
+end
+
+step
+  description: "return all equipment to specified areas"
+  note: "remember to wrap plate with parafilm"
+  check: "Return plate to DFP.0"
+  check: "Return LB to Bench"
+  check: "Return test tube rack to Bench"
+  release colony_plate[0]
+end
