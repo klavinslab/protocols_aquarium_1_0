70d83263e2245c5e083b045bcaa070af563aca23
pollardt <pollardt@uw.edu>
2014-01-29 13:54:27 -0800

    Update overnight_suspension.pl

--- labw14/group1/overnight_suspension.pl
+++ labw14/group1/overnight_suspension.pl
@@ -18,7 +18,7 @@ step
 end
 
 produce
- 1 "Overnight suspension culture" of Transformed_E_coli_Strain_id
+ 1 "Overnight suspension culture" of "Transformed_E_coli_Strain_id"
  release test_tube
 end
 
