a363b6f873bcb099e3ec92c31f66f56a6d02f175
sunshineyyy <yaoyu.yang88@gmail.com>
2014-01-29 10:29:53 -0800

    update restreak_kan.pl

--- labw14/week4/restreak_kan.pl
+++ labw14/week4/restreak_kan.pl
@@ -13,47 +13,59 @@ end
 
 step
   description: "Divde the plate into four even sectors."
-  note: "Using fine tipped felt pen and a straight edge, draw a line bisecting the plate, rotate the plate 90 degrees and draw a second line.  You should now have a plate with four even sectors."
-  #TODO: Image would be a great idea
+  note: "Using fine tipped felt pen and a straight edge, draw a line bisecting the plate, rotate the plate 90 degrees and draw a second line.  You shoμLd now have a plate with four even sectors."
+  #TODO: Image woμLd be a great idea
 end
 
 step
-  description: "Take four 1.5 ml centrifuge tubes and pipette 100 uL LB into each one"
+  description: "Take four 1.5 mL tubes and pipette 100 μL LB into each one"
 end
 
 take
   plate = item e_coli_strain_plate
 end
-plate = plate[0]
+#plate = plate[0]
 
 step
-  description: "Pick four colonies and transfer a different colony into each of the four 1.5 ml tubes"
-  bullet: "find a colony that is not touching any other colony."
-  bullet: "take a 200 ul tip (green box) by hand."
-  bullet: "With the pipette tip, scrape up the colony and then mix it into one of the 1.5ml tubes."
-  bullet: "scrape the end of the tip against the wall of the tube below the liquid level to ensure the colony has been deposited"
-  bullet: "repeat the above untill each 1.5 ml tube has a single colony."
-  warning: "be gentle.  The agar is soft."
+  description: "Pick four colonies and transfer a different colony into each of the four 1.5 mL tubes"
+  check: "Find a colony that is not touching any other colony."
+  check: "Take a 200 μL tip (the one used by the 10-100 μL or 20-200 μL pipettor) by hand."
+  check: "With the pipette tip, scrape up the colony and then mix it into one of the 1.5mL tubes."
+  check: "Scrape the end of the tip against the wall of the tube below the liquid level to ensure the colony has been deposited"
+  check: "Repeat the above untill each 1.5 mL tube has a single colony."
+  warning: "Be gentle.  The agar is soft."
 end
 
 step
-  description: "deposit each colony into a sector and streak"
-  bullet: "pipette 20ul from one of the 1.5 ml tubes on to the outer edge of the plate in the center of one of the sectors"
-  bullet: "take a fresh 200 ul tip (green box) by hand"
-  bullet: "At a low angle streak the tip starting from the liquid gently drag the tip back and fourth from sector edge to sector edge in non-overlapping lines eventually reaching near the center."
-  bullet: "discard the 1.5 ml tube you just used"
-  bullet: "repeat until you have run out of tubes and empty sectors"
-  warning: "do not cross the sector lines with a streak"
+  description: "Deposit each colony into a sector and streak"
+  check: "Pipette 20 μL from one of the 1.5 mL tubes on to the outer edge of the plate in the center of one of the sectors"
+  check: "Take a fresh 200 μL tip by hand"
+  check: "At a low angle streak the tip starting from the liquid gently drag the tip back and fourth from sector edge to sector edge in non-overlapping lines eventually reaching near the center."
+  check: "Discard the 1.5 mL tube you just used"
+  check: "Repeat until you have run out of tubes and empty sectors"
+  warning: "Do not cross the sector lines with a streak"
   image: "sector_streak"
 end
 
+step
+  description: "Store the old plate in 4C fridge."
+	note: "Wrap up the plate with id %{e_coli_strain_plate} using parafilm and put it in the Box 1 in deli fridge located at D2.100."
+end
+
+
 #TODO: produce this with location 30C incubator
 produce
-  1 "Agar plate" from plate
+  1 "Agar plate" from plate[0]
+	note: "Place the streaked plate upside-down (lid on the bottom) in 30 C incubator at A1.110"
   release sector_plate
+	location: "A1.110"
 end
 
 release lb
 
-#TODO:  This should be put in the fridge, not back in the incubator.
-release plate
+#TODO:  This shoμLd be put in the fridge, not back in the incubator.
+modify
+  plate[0]
+	location: "DFP.0"
+  inuse: 0
+end
