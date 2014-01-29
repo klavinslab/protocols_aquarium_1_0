fd1a56bae6986d7a681763e84e2b7a9d19a942d3
lavania <chandrashekharla@gmail.com>
2014-01-29 11:58:46 -0800

    Create trial.pl

---
+++ labw14/group1/trial.pl
@@ -0,0 +1,14 @@
+argument
+    plasmid_one: sample
+end
+
+take
+    1 "1.5 mL Tube"
+end
+step
+
+    description: "Increasing Volume"
+    check: "Add plasmid to 1.5 mL tube and label it."
+    check: "Add 20 uL of Molecular Grade Water to the 1.5 mL tube containing the plasmid"
+    
+end
