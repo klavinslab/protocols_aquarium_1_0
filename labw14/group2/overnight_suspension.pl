b055d03107399aaa798ad0071e240d0386c0709d
aquasonal <sonalj@uw.edu>
2014-01-29 14:32:54 -0800

    Update overnight_suspension.pl

--- labw14/group2/overnight_suspension.pl
+++ labw14/group2/overnight_suspension.pl
@@ -3,7 +3,7 @@ argument
 end
 
 take 
-  falcon_tube = 1 "50 mL LB liquid aliqout (sterile)"
+  falcon_tube = 1 "50 mL LB liquid aliquot (sterile)"
   test_tube = 1 "14 mL Test Tube"
   plate = item Transformed_E_coli_Strain_id
 end
