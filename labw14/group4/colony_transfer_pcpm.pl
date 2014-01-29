b072e0362ae7c014706070677b9959a54c421533
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-01-29 13:24:59 -0800

    Create colony_transfer_pcpm.pl

---
+++ labw14/group4/colony_transfer_pcpm.pl
@@ -0,0 +1,9 @@
+# Protocol to transfer colonies from tube to plate
+#
+#Authors: Pramod Chevali, Patrick Ma
+#
+# January 29, 2014
+##
+
+argument
+  Transformed_E_coli_Strain_id: sample, "Use the plate you transformed yesterday"
