a81c0986653afba1a5de0ad811abc04f477b3029
bonghoe <bonghoe@gmail.com>
2014-01-30 14:22:27 -0800

    Update make_glycerol_stock.pl

--- labw14/group5/make_glycerol_stock.pl
+++ labw14/group5/make_glycerol_stock.pl
@@ -8,14 +8,9 @@ end
 
 take
   glycerol_bottle = 1 "50 percent Glycerol (sterile)"
-  glycerol_tube = 1 "Cyro tube"
+  glycerol_tube = 1 "Cryo tube"
   tube = item Transformed_E_coli_Strain_id
 end
-
-#step
-# description: "Retrieve overnight suspension culture"
-# note: "Take an overnight suspension culture out of the 37 degrees incubator/shaker"
-#end
 
 step
  description: "Put the overnight suspension culture tube into a holder"
@@ -23,10 +18,10 @@ end
 
 step
  description: "Mixing glycerol with cell suspension"
- note: "Pipette 900 uL of the glycerol stock into the Cyro tube. Then pipette 900 uL of the overnight
- suspension culture into the same Cyro tube"
- bullet: "Label the Cyro tube with your initials, date and year"
- bullet: "Vortex the Cyro tube for 15-30 seconds"
+ note: "Pipette 900 uL of the glycerol stock into the Cryo tube. Then pipette 900 uL of the overnight
+ suspension culture into the same Cryo tube"
+ bullet: "Label the Cryo tube with your initials, date and year"
+ bullet: "Vortex the Cryo tube for 15-30 seconds"
  warning: "Be as sterile as possible. Do not touch the inside of the glycerol bottle with either a tip or pipette."
 end
 
@@ -34,7 +29,7 @@ end
 produce
  1 "Transformed E coli Glycerol Stock" from tube[0]
  release glycerol_tube
- note: "Place your Cyro tube in the -80 C refrigerator"
+ note: "Place your Cryo tube in the -80 C refrigerator"
 end
 
 release [glycerol_bottle[0], tube[0]]
