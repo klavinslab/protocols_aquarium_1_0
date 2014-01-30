44d35a17df3bc9b4303c47f1a0bb06ae8ec65154
Greg Nelson <gln@georgetown.edu>
2014-01-30 14:20:33 -0800

    Update make_glycerol_stock.pl

--- labw14/group8/make_glycerol_stock.pl
+++ labw14/group8/make_glycerol_stock.pl
@@ -6,9 +6,10 @@ end
 
 step
   note: 
-    "This protocol describes the process for preparing a transformed strain (that has been suspended overnight) 
-     for long-term storage (even for 10's of years), by making a 50 / 50 by volume combination with a 50%% glycerol
-     solution. The steps are below. Check them off as you read them, then continue on."
+    "This protocol describes the process for preparing a transformed and suspended strain 
+     for long-term storage (even for 10's of years). We make a 50 / 50 by volume combination with a 50%% glycerol
+     solution. A summary is shown below."
+  check: "Check that you are wearing gloves and your station is clean"
   check: "Take the suspended strain, gylcerol stock, and a cryo tube that will hold the result"
   check: "Pipette 900 microliters of glycerol stock, then the same amount from the strain, into the cryo tube. (3 steps)"
   check: "Store the result (1 step)"
@@ -16,48 +17,39 @@ step
 end
 
 take
-  note:
-    "First we take the suspended strain from it's location (one of the 37C suspenders ie SI3), gylcerol stock, and a cryo tube that will hold the result."
   strain = item Transformed_E_coli_Strain_id
   glycerol = 1 "50 percent Glycerol (sterile)"
   result_tube = 1 "Cyro Tube"
+  note: "Place the cyro tube in a holder and the tube with suspended culture in a tube rack, both on the bench. The gylcerol can be set on the bench."
 end
 
 
-step 
-  note: "Pipette 900 microliters of glycerol stock, then the same amount from the strain with a different tip, into the cryo tube."
-  
-  check: "Label the 14 mL test tube."
-  check: "Use 1000 uL pipetter to pipett 2 mL of LB Aliquot to the test tube"
-  warning: "Make sure that the LB media is not contaminated"
-  warning: "Make sure that the test tube is capped after transfer of LB Media"
-  warning: "Do not let the pipetter itself touch the LB media"
-end 
-
 step
-
-  note: "Store the result in the -80C fridge"
-  note: "We will add a tranformed E Coli colony to the prepared media"
-  check: "Remove the parafilm from the plate"
-  check: "Use a pipetter tip to pick 1 colony from the plate"
-  check: "Carefully tilt your labeled test tube and scrap the colony inside the media"
-end
-
-step
-
-  note: "Return glycerol stock, tidy your station, and dispose of suspended strain with proper sterile technique. (3 steps)"
-  note: "We will add a tranformed E Coli colony to the prepared media"
-  check: "Remove the parafilm from the plate"
-  check: "Use a pipetter tip to pick 1 colony from the plate"
-  check: "Carefully tilt your labeled test tube and scrap the colony inside the media"
+  description: "Prepare to a 50 / 50 by volume combination of the strain and 50%% glycerol"
+  check: "Take the 1000uL micropipettor and set to 900uL"
+  check: "Loosen the cap on the gylcerol stock and the cryo tube"
+  check: "Pipette from the glycerol stock to the cryotube. Be sure to not touch the inside of the glycerol container to maintain its sterility."
+  check: "Pipette from the suspended strain into the cryotube."
 end
 
 produce
    r = 1 "Transformed E coli Glycerol Stock" from strain[0]
-   location: "B13.425"
+   check: "Also label the cryo tube on the side (not cap) with your initials and the date (with year, since it may be in storage for decades)."
    release result_tube
    note: "We will now place the stock into the -80C Fridge"
 end
+
+
+
+step
+  description: "Return materials"
+  note: "Return glycerol stock, tidy your station, and dispose of suspended strain with proper sterile technique. (3 steps)"
+  check: "Return the glycerol stock to its original location"
+  check: "Take the suspended colony tube to the dish cleaning station. Add twice to 3x its orignal volume of 20% bleach. Recap the tube and place it in the dirty tube rack."
+  check: "Clean your station and dispose of your gloves"
+end
+
+
 
 release glycerol
 release strain
