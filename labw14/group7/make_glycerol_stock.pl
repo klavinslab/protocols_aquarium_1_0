6969522ab2e3a1664be9a6bf28a7195d78a535ec
Erik909 <erik909@uw.edu>
2014-01-30 13:45:11 -0800

    Update make_glycerol_stock.pl

--- labw14/group7/make_glycerol_stock.pl
+++ labw14/group7/make_glycerol_stock.pl
@@ -2,15 +2,15 @@ argument
   Transformed_E_coli_Strain_id: sample, "Choose the overnight suspension culture you incubated yesterday"
 end
 
+step
+  description: 
+    "This protocol describes how to create a long-lasting glycerol stock from an overnight colony suspension"
+end
+
 take
   cells_to_store = item Transformed_E_coli_Strain_id
   glycerol = 1 "50 percent Glycerol (sterile)"
   sterile_tube = 1 "Cryo Tube"
-end
-
-step
-  description: 
-    "This protocol describes how to create a long-lasting glycerol stock from an overnight colony suspension"
 end
 
 step
@@ -22,7 +22,7 @@ end
 step
   description: "Pipette 900ul of glycerol into the sterile tube"
   note: "Be careful not to contaminate the glycerol bottle.  Use the 1000 μL pipetter."
-  #note: "Next: Pipette 900ul of cell suspension into the sterile tube"
+  note: "Next: Pipette 900ul of cell suspension into the sterile tube"
 end
 
 step
@@ -33,13 +33,11 @@ end
 step
   description: "Vortex the sample"
   note: "Use the more powerful VWR analog vortex mixer at speed 10"
-  #note: "Next: Store the sample in the -80C fridge"
-end
+  note: "Next: Store the sample in the -80C fridge"
 
 produce
-  s = 1 "Overnight suspension culture" from cells_to_store[0]
+  s = 1 "Transformed E coli Glycerol Stock" from cells_to_store[0]
   note: "Place in -80 degree freezer, at location:"
-  location: "Bench"
 end
 
 step
