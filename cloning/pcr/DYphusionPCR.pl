fa2146ac5676467b60897d855ba038a14b67a9e3
David Younger <dyounger@gmail.com>
2014-01-31 11:24:16 -0800

    Update DYphusionPCR.pl

--- cloning/pcr/DYphusionPCR.pl
+++ cloning/pcr/DYphusionPCR.pl
@@ -1,87 +1,57 @@
 argument
   tubenumber: number, "The tube number for this set of reactions"
   tanneal: number, "The anneal temperature in degrees C"
-  enzyme_id: sample, "The Phsion HF Master Mix stock"
+  enzyme_id: sample("Enzyme"), "The Phsion HF Master Mix stock"
   num: number, "The number of PCR reactions, 1-12"
-  primer1x1: sample, "The first primer: reaction 1"
-  primer1x2: sample, "The second primer: reaction 1"
-  plasmid1: sample, "The plasmid stock: reaction 1"
-  primer2x1: sample, "The first primer: reaction 2"
-  primer2x2: sample, "The second primer: reaction 2"
-  plasmid2: sample, "The plasmid stock: reaction 2"
-  primer3x1: sample, "The first primer: reaction 3"
-  primer3x2: sample, "The second primer: reaction 3"
-  plasmid3: sample, "The plasmid stock: reaction 3"
-  primer4x1: sample, "The first primer: reaction 4"
-  primer4x2: sample, "The second primer: reaction 4"
-  plasmid4: sample, "The plasmid stock: reaction 4"
-  primer5x1: sample, "The first primer: reaction 5"
-  primer5x2: sample, "The second primer: reaction 5"
-  plasmid5: sample, "The plasmid stock: reaction 5"
-  primer6x1: sample, "The first primer: reaction 6"
-  primer6x2: sample, "The second primer: reaction 6"
-  plasmid6: sample, "The plasmid stock: reaction 6"
-  primer7x1: sample, "The first primer: reaction 7"
-  primer7x2: sample, "The second primer: reaction 7"
-  plasmid7: sample, "The plasmid stock: reaction 7"
-  primer8x1: sample, "The first primer: reaction 8"
-  primer8x2: sample, "The second primer: reaction 8"
-  plasmid8: sample, "The plasmid stock: reaction 8"
-  primer9x1: sample, "The first primer: reaction 9"
-  primer9x2: sample, "The second primer: reaction 9"
-  plasmid9: sample, "The plasmid stock: reaction 9"
-  primer10x1: sample, "The first primer: reaction 10"
-  primer10x2: sample, "The second primer: reaction 10"
-  plasmid10: sample, "The plasmid stock: reaction 10"
-  primer11x1: sample, "The first primer: reaction 11"
-  primer11x2: sample, "The second primer: reaction 11"
-  plasmid11: sample, "The plasmid stock: reaction 11"
-  primer12x1: sample, "The first primer: reaction 12"
-  primer12x2: sample, "The second primer: reaction 12"
-  plasmid12: sample, "The plasmid stock: reaction 12"
+  primer1x1: sample("Primer"), "Forward primer: reaction 1"
+  primer1x2: sample("Primer"), "Reverse primer: reaction 1"
+  plasmid1: sample("Plasmid"), "Template: reaction 1"
+  primer2x1: sample("Primer"), "Forward primer: reaction 2"
+  primer2x2: sample("Primer"), "Reverse primer: reaction 2"
+  plasmid2: sample("Plasmid"), "Template: reaction 2"
+  primer3x1: sample("Primer"), "Forward primer: reaction 3"
+  primer3x2: sample("Primer"), "Reverse primer: reaction 3"
+  plasmid3: sample("Plasmid"), "Template: reaction 3"
+  primer4x1: sample("Primer"), "Forward primer: reaction 4"
+  primer4x2: sample("Primer"), "Reverse primer: reaction 4"
+  plasmid4: sample("Plasmid"), "Template: reaction 4"
+  primer5x1: sample("Primer"), "Forward primer: reaction 5"
+  primer5x2: sample("Primer"), "Reverse primer: reaction 5"
+  plasmid5: sample("Plasmid"), "Template: reaction 5"
+  primer6x1: sample("Primer"), "Forward primer: reaction 6"
+  primer6x2: sample("Primer"), "Reverse primer: reaction 6"
+  plasmid6: sample("Plasmid"), "Template: reaction 6"
+  primer7x1: sample("Primer"), "Forward primer: reaction 7"
+  primer7x2: sample("Primer"), "Reverse primer: reaction 7"
+  plasmid7: sample("Plasmid"), "Template: reaction 7"
+  primer8x1: sample("Primer"), "Forward primer: reaction 8"
+  primer8x2: sample("Primer"), "Reverse primer: reaction 8"
+  plasmid8: sample("Plasmid"), "Template: reaction 8"
+  primer9x1: sample("Primer"), "Forward primer: reaction 9"
+  primer9x2: sample("Primer"), "Reverse primer: reaction 9"
+  plasmid9: sample("Plasmid"), "Template: reaction 9"
+  primer10x1: sample("Primer"), "Forward primer: reaction 10"
+  primer10x2: sample("Primer"), "Reverse primer: reaction 10"
+  plasmid10: sample("Plasmid"), "Template: reaction 10"
+  primer11x1: sample("Primer"), "Forward primer: reaction 11"
+  primer11x2: sample("Primer"), "Reverse primer: reaction 11"
+  plasmid11: sample("Plasmid"), "Template: reaction 11"
+  primer12x1: sample("Primer"), "Forward primer: reaction 12"
+  primer12x2: sample("Primer"), "Reverse primer: reaction 12"
+  plasmid12: sample("Plasmid"), "Template: reaction 12"
 end
 
 take
   phusion_stock = item enzyme_id
-  primer1x1_stock = item primer1x1
-  primer1x2_stock = item primer1x2
-  primer2x1_stock = item primer2x1
-  primer2x2_stock = item primer2x2
-  primer3x1_stock = item primer3x1
-  primer3x2_stock = item primer3x2
-  primer4x1_stock = item primer4x1
-  primer4x2_stock = item primer4x2
-  primer5x1_stock = item primer5x1
-  primer5x2_stock = item primer5x2
-  primer6x1_stock = item primer6x1
-  primer6x2_stock = item primer6x2
-  primer7x1_stock = item primer7x1
-  primer7x2_stock = item primer7x2
-  primer8x1_stock = item primer8x1
-  primer8x2_stock = item primer8x2
-  primer9x1_stock = item primer9x1
-  primer9x2_stock = item primer9x2
-  primer10x1_stock = item primer10x1
-  primer10x2_stock = item primer10x2
-  primer11x1_stock = item primer11x1
-  primer11x2_stock = item primer11x2
-  primer12x1_stock = item primer12x1
-  primer12x2_stock = item primer12x2
 end
-
 take
-  plasmid1_stock = item plasmid1
-  plasmid2_stock = item plasmid2
-  plasmid3_stock = item plasmid3
-  plasmid4_stock = item plasmid4
-  plasmid5_stock = item plasmid5
-  plasmid6_stock = item plasmid6
-  plasmid7_stock = item plasmid7
-  plasmid8_stock = item plasmid8
-  plasmid9_stock = item plasmid9
-  plasmid10_stock = item plasmid10
-  plasmid11_stock = item plasmid11
-  plasmid12_stock = item plasmid12
+    forward_primer_stock = item unique([primer1x1,primer2x1,primer3x1,primer4x1,primer5x1,primer6x1,primer7x1,primer8x1,primer9x1,primer10x1,primer11x1,primer12x1])
+end
+take
+    reverse_primer_stock = item unique([primer1x2,primer2x2,primer3x2,primer4x2,primer5x2,primer6x2,primer7x2,primer8x2,primer9x2,primer10x2,primer11x2,primer12x2])
+end
+take
+    plasmid_stock = item unique([plasmid1,plasmid2,plasmid3,plasmid4,plasmid5,plasmid6,plasmid7,plasmid8,plasmid9,plasmid10,plasmid11,plasmid12])
 end
 
 step
@@ -160,13 +130,7 @@ step
   image: "thermal_cycler_home"
 end
 
-
-release [primer1x1_stock[0],primer1x2_stock[0],primer2x1_stock[0],primer2x2_stock[0],primer3x1_stock[0],primer3x2_stock[0]]
-release [primer4x1_stock[0],primer4x2_stock[0],primer5x1_stock[0],primer5x2_stock[0],primer6x1_stock[0],primer6x2_stock[0]]
-release [primer7x1_stock[0],primer7x2_stock[0],primer8x1_stock[0],primer8x2_stock[0],primer9x1_stock[0],primer9x2_stock[0]]
-release [primer10x1_stock[0],primer10x2_stock[0],primer11x1_stock[0],primer11x2_stock[0],primer12x1_stock[0],primer12x2_stock[0]]
-release [plasmid1_stock[0],plasmid2_stock[0],plasmid3_stock[0],plasmid4_stock[0],plasmid5_stock[0],plasmid6_stock[0]]
-release [plasmid7_stock[0],plasmid8_stock[0],plasmid9_stock[0],plasmid10_stock[0],plasmid11_stock[0],plasmid12_stock[0]]
-
 release [phusion_stock[0]]
-
+release forward_primer_stock
+release reverse_primer_stock
+release plasmid_stock
