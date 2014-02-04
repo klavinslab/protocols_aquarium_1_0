6f7b9c90dbf52217f27efcb70ab783a773f0a22a
dstarkebaum <dasman@uw.edu>
2014-02-04 14:44:37 -0800

    Update make_glycerol_stock.pl

--- labw14/group7/make_glycerol_stock.pl
+++ labw14/group7/make_glycerol_stock.pl
@@ -10,28 +10,29 @@ take
   cell_culture_tube = item Transformed_E_coli_Strain_id
   glycerol = 1 "50 percent Glycerol (sterile)"
   sterile_tube = 1 "Cryo Tube"
-  note: "Be sure to use sterile technique when removing cryo tubes from theie container. 
-         This means, shake out one of the sterile tubes into your gloved hand while holding the outside of the bag,
-         rather than putting your hand into the bag to take one out"
+  note: "Be sure to use sterile technique when removing cryo tubes from their container. This means:"
+  note: " - shake out one of the sterile tubes into your gloved hand while holding the outside of the bag,
+         rather than putting your hand into the bag to take one out."
 end
 
 step
  description: "Suspend the overnight cell culture in 50%% gylcerol"
  check: "Pipette 900uL of the 50%% glycerol stock to the 1.5mL sterile tube"
  note: "Be sure to use STERILE technique while handling the gylcerol stock.  If you contaminate this stock
-        it wil ruin everyone else's experients! This means: loosen caps before loading the pipette tip, and
-        do not touch the inside of the jar with the micropipetter.  You must hold the cap in your hand while
-        extracting fluid with the micro-pipetter, rather than placing it on the lab bench.
-        do not try to re-use pipette tips."
+        it wil ruin everyone else's experients! This means:"
+ note: " - Loosen caps before loading the pipette tip"
+ note: " - Do not touch the inside of the jar with the micropipetter."
+ note: " - Hold the cap in your hand while extracting fluid with the micro-pipetter, rather than placing it on the lab bench."
+ note: " - Do not re-use pipette tips."
  check: "Pipette 900uL of cell suspension into the 1.5mL sterile tube"
- check: "Vortex the 1.5mL sterile tube for 15s on high power. Use the more powerful VWR analog vortex mixer at speed 10"
+ check: "Vortex the 1.5mL sterile tube for 15s on high power. Use the more powerful VWR analog vortex mixer at speed 10."
 end
 
 produce
  s = 1 "Transformed E coli Glycerol Stock" from cell_culture_tube[0]
- note: "Make sure to also add your initials and todays date (mm/dd/yyyy).  This sample will potentially be
-        kept in the freezer for many months, so be sure to label both the cap and the side of the tube for
-        redundancy.  Store your glycerol/cell culture mixture in the -80 C freezer"
+ note: "Make sure to also add your initials and todays date (mm/dd/yyyy) in addition to the item ID number.
+ note: "This glycerol/cell culture mixture will potentially be stored in the -80 C freezer for many months,
+        so be sure to label both the cap and the side of the tube for redundancy."
 end
 
 #release [cell_culture_tube[0], glycerol[0]]
