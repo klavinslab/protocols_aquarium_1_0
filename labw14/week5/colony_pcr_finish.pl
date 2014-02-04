ba9de1ffc86b65dd0bdb4a7e1ec9c45054387673
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-02-04 11:23:03 -0800

    Update colony_pcr_finish.pl

--- labw14/week5/colony_pcr_finish.pl
+++ labw14/week5/colony_pcr_finish.pl
@@ -1,1 +1,4 @@
-nothing
+step
+  description: "Go to the thermal cycler to pick up the PCR tubes you put in the previous protocol."
+  note: "Grab the 8 PCR tubes and go to Gel room to perform run_gel protocol."
+end
