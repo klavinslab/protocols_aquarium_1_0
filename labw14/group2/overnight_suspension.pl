ad69f62eca0d48760183a42d253efca5c2f4b898
aquasonal <sonalj@uw.edu>
2014-02-04 17:34:40 -0800

    Update overnight_suspension.pl

--- labw14/group2/overnight_suspension.pl
+++ labw14/group2/overnight_suspension.pl
@@ -1,5 +1,5 @@
 argument 
-  e_id: sample array, ("Transformed E coli Strain"), "This is the plate from which we draw colonies" 
+  e_id: sample ("Transformed E coli Strain") array, "This is the plate from which we draw colonies" 
 end
 
 take 
