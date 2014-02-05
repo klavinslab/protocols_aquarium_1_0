0f46114754b07439663dcde3a5638203e3be4468
pollardt <pollardt@uw.edu>
2014-02-04 17:49:06 -0800

    Update osusmeta.pl

--- labw14/group1/osusmeta.pl
+++ labw14/group1/osusmeta.pl
@@ -37,8 +37,7 @@ while x < length(plate)
  x = x+1
 end
 
-release concat(falcon_tube, plate)
-release test_tube
+release [concat(falcon_tube, plate), test_tube]
 
 log
  return: {Transformed_E_coli_Strain_id: r[:id]}
