c0fc1d2a57c3ca507cb17bc730a0d8292516117c
Erik909 <erik909@uw.edu>
2014-02-06 15:35:45 -0800

    Update overnight_suspension_array.pl

--- labw14/group7/overnight_suspension_array.pl
+++ labw14/group7/overnight_suspension_array.pl
@@ -36,10 +36,11 @@ end
 
 i=0
 while i<n
+currentPlate = plate_array[i]
 step
   description: "Add a colony to each of %{n} suspensions"
   note: "Match each plate to its corresponding tube.  For each pair:"
-  check: "add a colony from plate %{plate_array[i]} to tube %{i}"
+  check: "add a colony from plate %{currentPlate} to tube %{i}"
   note: "Select the desired colony prior to opening plate. Mark desired colony with circle and intials and date"
   note: "Selection of your colony should be based on size (medium-big, not too big), isolated, and round in shape." 
   bullet: "Take a sterile pipette tip, pick up the desired colony by gently scraping the tip to the colony."
