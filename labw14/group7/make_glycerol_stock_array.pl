78c6657fcb6e575f4db21743e74fdf8e9841a72e
dstarkebaum <dasman@uw.edu>
2014-02-06 15:50:12 -0800

    Update make_glycerol_stock_array.pl

--- labw14/group7/make_glycerol_stock_array.pl
+++ labw14/group7/make_glycerol_stock_array.pl
@@ -17,47 +17,48 @@ take
          rather than putting your hand into the bag to take one out."
 end
 
-step
-  description: "Label Cryo tubes with index numbers from 0 to %{n} - 1"
-end 
-
-i = 0
-r = []
-
-while i < n
 
 step
- description: "Suspend the overnight cell culture in 50%% gylcerol"
- check: "Pipette 900uL of the 50%% glycerol stock to 1.5mL sterile tube number %{n}"
+ description: "Add gylcerol to each cryo tube"
+ check: "Pipette 900uL of the 50%% glycerol stock to each 1.5mL sterile tube"
  note: "Be sure to use STERILE technique while handling the gylcerol stock.  If you contaminate this stock
         it wil ruin everyone else's experients! This means:"
- note: " - Loosen caps before loading the pipette tip"
- note: " - Hold the cap in your gloved hand while extracting fluid with the micro-pipetter, rather than placing it on the lab bench."
- note: " - Do not touch the inside of the tube with the micropipetter."
- note: " - Do not re-use pipette tips."
- check: "Pipette 900uL of cell suspension number %{n} into 1.5mL sterile tube number %{n}"
- check: "Vortex 1.5mL sterile tube number %{n} for 15s at speed 10 using the VWR analog vortex mixer."
-end
-
-produce
- s = 1 "transformed E. Coli in 50%% glycerol" from cell_culture_tube_array[n]
- note: "Make sure to also add your initials and todays date (mm/dd/yyyy) in addition to the item ID number.
-       This glycerol/cell culture mixture will potentially be stored in the -80 C freezer for many months,
-       so be sure to label both the cap and the side of the tube for redundancy."
- r = append(r,s[:id])
-end
-
-i=i+1
-
-end
-
-log
-  return: {glycerol_stock_array_id: r}
+ bullet: "Loosen caps before loading the pipette tip"
+ bullet: "Hold the cap in your gloved hand while extracting fluid with the micro-pipetter, rather than placing it on the lab bench."
+ bullet: "Do not touch the inside of the tube with the micropipetter."
+ bullet: "Do not re-use pipette tips."
 end
 
 step
-  description: "the glycerol_stock_array_id is %{r}"
+ description: "Add cell culture to each cryo tube"
+ note: "In the following steps, you will add cell culture to each cryo tube in turn, label it with its ID number, your initial, todays date" 
 end
+
+i = 0
+while i < n
+ step
+  discription: "Add overnight suspension culture to the cryo tube"
+  check: "Pipette 900uL of cell suspension number %{Transformed_E_coli_Strain_tube_array[i]} to a new 1.5mL sterile tube (containing glycerol)"
+  check: "Vortex 1.5mL this sterile tube for 15s at speed 10 using the VWR analog vortex mixer."
+ end
+ 
+ produce
+  s = 1 "transformed E. Coli in 50%% glycerol" from cell_culture_tube_array[i]
+  note: "Make sure to also add your initials and todays date (mm/dd/yyyy) in addition to the item ID number.
+        This glycerol/cell culture mixture will potentially be stored in the -80 C freezer for many months,
+        so be sure to label both the cap and the side of the tube for redundancy."
+  r = append(r,s[:id])
+ end
+ i=i+1
+end
+
+#log
+#  return: {glycerol_stock_array_id: r}
+#end
+
+#step
+#  description: "the glycerol_stock_array_id is %{r}"
+#end
 
 
 #release [cell_culture_tube_array[0], glycerol[0]]
