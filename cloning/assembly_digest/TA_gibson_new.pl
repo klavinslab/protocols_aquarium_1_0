0b7ba5776845cf54d5aa84a57d4a555a44ee3150
tamimeur <tamimeur@gmail.com>
2014-02-07 12:50:17 -0800

    Update TA_gibson_new.pl

--- cloning/assembly_digest/TA_gibson_new.pl
+++ cloning/assembly_digest/TA_gibson_new.pl
@@ -20,8 +20,10 @@ end
 #  x=x+1
 #end
 
+fragments = [ 23446, 23449]
+
 take
-  my_frag_arr = item [ 23446, 23449 ]
+  my_frag_arr = item fragments
   #my_frag_arr = item fragment1
   master_mix = 1 "Gibson Aliquot"
 end
