96fa5840ecb530176438b3f2787906a0a625cf4d
tamimeur <tamimeur@gmail.com>
2014-02-07 11:36:19 -0800

    Update TA_gibson_new.pl

--- cloning/assembly_digest/TA_gibson_new.pl
+++ cloning/assembly_digest/TA_gibson_new.pl
@@ -1,8 +1,24 @@
 argument
-  fragments: sample("Fragment") array array, "Fragment Names"
+  fragments: sample("Fragment") array, "Fragment Names"
 end
+
+information "Scarlessly assemble DNA using fragments with terminal homologies."
 
 step
   description: "This protocol prepares Gibson reactions in a strip-well tube."
+end
+
+#take
+#  master_mix = 1 "Gibson Aliquot"
+#end
+
+l=length(fragments)
+
+x= "step\n\tdescription: \"this is just a test\"\nend"
+
+x
+
+step
+  description: "Passed the test."
 end
 
