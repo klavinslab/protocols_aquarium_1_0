3894a4a2af480a3b0fa1ad3d1ea3bae7442bd722
tamimeur <tamimeur@gmail.com>
2014-02-07 17:29:05 -0800

    Update TA_gibson_new.pl

--- cloning/assembly_digest/TA_gibson_new.pl
+++ cloning/assembly_digest/TA_gibson_new.pl
@@ -11,7 +11,7 @@ step
   description: "This protocol prepares Gibson reactions in a strip-well tube."
 end
 
-#l=length(fragments)
+num_gibs=length(gibson_pieces)
 
 two_d_arr = [[1,1],[2,2]]
 
@@ -21,7 +21,57 @@ take
 end
 
 step
-  description: "Passed the test %{two_d_arr}"
+  description: "Grab a 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
+  check: "Label the right most well with the letters DY"
+  check: "Label the left most well with the letter A"
+  note: "These wells will be refered to as 1 - 12"
+end
+if num_gibs > 12
+  step
+    description: "Grab a second 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
+    check: "Label the right most well with the letters DY"
+    check: "Label the left most well with the letter B"
+    note: "These wells will be refered to as 13 - 24"
+  end
+end
+if num_gibs > 24
+  step
+    description: "Grab a third 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
+    check: "Label the right most well with the letters DY"
+    check: "Label the left most well with the letter C"
+    note: "These wells will be refered to as 25 - 36"
+  end
+end
+if num_gibs > 36
+  step
+    description: "Grab a fourth 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
+    check: "Label the right most well with the letters DY"
+    check: "Label the left most well with the letter D"
+    note: "These wells will be refered to as 37 - 48"
+  end
+end
+if num_gibs > 48
+  step
+    description: "Grab a fourth 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
+    check: "Label the right most well with the letters DY"
+    check: "Label the left most well with the letter E"
+    note: "These wells will be refered to as 49 - 60"
+  end
+end
+
+#step
+#  description: "Passed the test %{two_d_arr}"
+#end
+
+i = 0
+
+while i < num_gibs
+
+  step
+    description: "Prepare Gibson %{i}"
+    note: "Add 2 µL"
+  end
+  
 end
 
 release my_frag_arr
