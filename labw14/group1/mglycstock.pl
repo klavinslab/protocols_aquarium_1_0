ae51939b24040c387cb7c6ac6377289507bd89fe
lavania <chandrashekharla@gmail.com>
2014-02-08 16:02:02 -0800

    Update mglycstock.pl

--- labw14/group1/mglycstock.pl
+++ labw14/group1/mglycstock.pl
@@ -8,20 +8,26 @@ end
 
 take
   glycerol_bottle = 1 "50 percent Glycerol (sterile)"
-  glycerol_tube = 1 "Cryo tube"
   tube = item Transformed_E_coli_Strain_id
 end
 
+len1 = length(tube)
+take 
+
+   glycerol_tube = len1 "Cryo tube"
+end
+
 step
- description: "Put the overnight suspension culture tube into a holder"
+ description: "Put the overnight suspension culture tubes into a holder"
 end
 
 step
  description: "Mixing glycerol with cell suspension"
- note: "Pipette 900 uL of the glycerol stock into the Cryo tube. Then pipette 900 uL of the overnight
- suspension culture into the same Cryo tube"
- bullet: "Label the Cryo tube with your initials, date and year"
- bullet: "Vortex the Cryo tube for 15-30 seconds"
+ note: "Pipette 900 uL of the glycerol stock into each of the Cryo tubes. Then pipette 900 uL of the overnight
+ suspension culture into each of the same Cryo tubes"
+ bullet: "Label the Cryo tubes with your initials, date and year"
+ bullet: "Vortex the Cryo tubes for 15-30 seconds"
+ bullet: "Make sure you have done this for all the cyro tubes"
  warning: "Be as sterile as possible. Do not touch the inside of the glycerol bottle with either a tip or pipette."
 end
 
@@ -31,7 +37,7 @@ r2 = []
 while x < length(tube)
  produce
   r3 = 1 "Transformed E coli Glycerol Stock" from tube[x]
-  release glycerol_tube
+  
   note: "Place your Cryo tube in the -80 C refrigerator"
  end
  r2 = append(r2,r3[:id])
