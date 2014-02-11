0d24b09f18804855b87255dbde3ccb75bf14b9c2
alexbaryshev <baryshev@uw.edu>
2014-02-11 11:01:58 -0800

    Update baryshev_overnight_suspension.pl

--- labw14/group5/baryshev_overnight_suspension.pl
+++ labw14/group5/baryshev_overnight_suspension.pl
@@ -6,7 +6,7 @@ take
     plate = item Transformed_E_coli_Strain_id
 end
 
-num = length(plate)
+#num = length(plate)
 
 step
  description: "This protocol describes the process of overnight suspension"
