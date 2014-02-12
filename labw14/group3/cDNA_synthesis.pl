dacb79088691eab5e56f0884bbd3a4c88032f9f9
Joshua Borgman <joshborgman@comcast.net>
2014-02-12 13:31:42 -0800

    Update cDNA_synthesis.pl

--- labw14/group3/cDNA_synthesis.pl
+++ labw14/group3/cDNA_synthesis.pl
@@ -1,13 +1,11 @@
 argument
-  rxn_mix: sample, "Select te 5x iScript reaction mix"
-  rev_enzyme: sample, "Select the iScript reverse transcriptase"
   rna_id: sample, "The lysed RNA"
 end
 
 take
-  iscript_mix = item rxn_mix
-  rev_trans = item rev_enzyme
-  rna_yeast = rna_id
+  rxn_mix: sample, "Select te 5x iScript reaction mix"
+  rev_enzyme: sample, "Select the iScript reverse transcriptase"
+  rna_yeast = rna_id, "This is the isolated RNA"
 end
 
 step
@@ -16,28 +14,24 @@ end
 
 step
   description: "Label a 0.2 mL PCR tube. Write your initials on it."
-  note: "Prior to moving forward check that your iScript reaction mix has
-    generated any precipitation upon thawing. 
-  bullet: "If precipitation has occured, please mix througly to resuspend, 
-    before moving forward"
+  note: "Prior to moving forward check that your iScript reaction mix has \n generated any precipitation upon thawing." 
+  bullet: "If precipitation has occured, please mix througly to resuspend, \n before moving forward"
 end
 
 step 
   description: "Prepare Reaction"
   check:"Pipet 4 µL of 5x iScript reaction mix into the labeled PCR tube."
-  check: "Pipet 1 µL of iScript reverse transcriptase in to the labeled PCR tube"
-  check: "Pipet 1 µL of primer with id %{primer1} into the tube."# ended here
-  check: "Pipet 1 µL of primer with id %{primer2} into the tube."
-  check: "Pipet 10 µL of Phusion Master Mix with id %{enzyme_id} into the tube."
+  check: "Pipet 1 µL of iScript reverse transcriptase into the labeled PCR tube"
+  check: "Pipet (some amount) of Nuclease-free water into the labeled PCR tube"
+  check: "Pipet (some amount) of RNA template into the labeled PCR tube"
   check: "Use the tip to gently mix."
   note: "Be careful to pipette into the liquid, not the side of the tube."
 end
 
-release phusion_stock
 
 step
   description: "You will now be asked to use the lab thermal cycler"
-  note: "The thermal cycler will be shared by all groups."
+  note: "The thermal cycler will be shared by all groups (if others need it)."
   image: "thermal_cycler_off"
 end
 
@@ -47,17 +41,32 @@ end
 
 step
   description: "Choose program on the thermal cycler"
-  note: "Click Home then click Saved Protocol, choose LABW14 folder, choose COLONYPCR. Wait for all other groups to load in their samples."
+  note: "Click Home then click Saved Protocol, choose LABW14 folder, choose RT_React. "
+  note: "Thermal cycling routine should be programmed prior to use. If not, contact lab supervisor."
   image: "thermal_cycler_home"
 end
 
 step
-  description: "When all group samples are loaded, hit 'run' on the thermal cycler and select 20 µL setting."
-  note: "Coordinate with your fellow classmates to put all their samples before hit run."
+  description: "When all samples are loaded, hit 'run' on the thermal cycler and select 20 µL setting."
+  note: "Hit run. The incubation time in the thermal cycler will take approximately 40 minutes"
+  warning: "Hit next, immediately following the placement into the thermal cycler."
+  timer: { hours: 0, minutes: 40, seconds: 0}
   image: "thermal_cycler_select"
 end
 
+step
+  description: "*Optional Hold Stage*"
+  note: "This is an optimal point to hold your sample if you do not anticipate time to finish."
+  note: "If this is the case, do not remove the tube from the 4°C hold."
+end
 
-release [primer1_stock[0],primer2_stock[0],plasmid_stock[0]]
+produce
+  r = 1 "Yeast cDNA" of "target_strain"
+  note: "Keep the tube on the bench to use in the next protool"
+end
 
+log
+  return: {Yeast_cDNA_id: r[:id]}
+end
 
+release [rxn_mix[0],rev_enzyme[0],rna_yeast[0]]
