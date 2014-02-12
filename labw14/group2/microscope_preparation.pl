ac8a6a60c55ee733abf0dafa579d7df010ff0e63
hamsanchez <hamilton.sanch@gmail.com>
2014-02-12 00:06:29 -0800

    Update microscope_preparation.pl

--- labw14/group2/microscope_preparation.pl
+++ labw14/group2/microscope_preparation.pl
@@ -1,1 +1,10 @@
-argument
+step
+  description: "Prepare the microscope for use"
+  note: "Click next to begin this protocol"
+end
+
+step
+  description: "Turn on the microscope incubator"
+  bullet: "Head to the biohazard level 2 room, and turn on the incubator for the microscope"
+  note: "The microscope incubator is set to 37 degrees. This ensures cells maintain optimal temperature for growth."
+end
