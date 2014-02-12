941c45d2e493595a588680de84aed0449c2cabd9
pollardt <pollardt@uw.edu>
2014-02-12 13:03:45 -0800

    Update dendiDNA.pl

--- labw14/group1/dendiDNA.pl
+++ labw14/group1/dendiDNA.pl
@@ -51,13 +51,7 @@ step
   check: "Lable a new 1.5mL microcentrifuge tube with the date and your initials"
   check: "Add quantities to the new tube as desired using the example image."
   check: "Place the denatured and diluted DNA on ice"
-  if t == 20
-    image: "40pm"
-  else
-    image: "20pm"
-  end
-  #image: "http://bioturk.ee.washington.edu:3012/files/304"
-  #image: "http://bioturk.ee.washington.edu:3012/files/305"
+  image: "dendi_DNA_HT1_Ratios"
 end
 
 produce
