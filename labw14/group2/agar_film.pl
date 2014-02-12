5f99e838083479445b0e31f8b388afe673c02713
hamsanchez <hamilton.sanch@gmail.com>
2014-02-12 00:46:40 -0800

    Update agar_film.pl

--- labw14/group2/agar_film.pl
+++ labw14/group2/agar_film.pl
@@ -1,2 +1,58 @@
 argument
   id: sample ("Transformed E coli Strain"), "This is where our cells will come from"
+end
+
+step
+  desciption: "Prepare the agar film to use on the microscope"
+  note: "Click next to begin this procedure"
+end
+
+take
+  test_tube = 1 "14 mL Test Tube"
+  dish = 1 "Small Petri Dish"
+  #plate = 1 "M9 agar plate"
+  ons = item id
+end
+
+step
+  description: "Prepare the petri dish for the agar film"
+  bullet: "Using a 10 microL pipetter, pipette 2 microL of our M9 diluted cells into the center of the petri dish"
+  note: "it should just look like a small bead of liquid at the center of the petri dish."
+end
+step
+  description: "Cut the agar film"
+  bullet: "Take the lid off of the 14 mL test tube"
+  bullet: "Take the lid off of the M9 petri dish"
+  bullet: "Stick the opening of the test tube into the center of the agar to punch a hole in the agar"
+end
+
+step
+  description: "Remove the agar film"
+  warning: "The film is delicate, high precision is necessary."
+  bullet: "Using a spatula carefully remove the punched out agar film from the dish."
+end
+
+step
+  description: "Place the agar film in the petri dish"
+  warning: "The film is delicate, high precision is necessary."
+  bullet: "CAREFULLY place the agar film on top of our cells in the center of the dish"
+  bullet: "Use the spatula to flatten out any air bubbles"
+  note: "The agar will most likely stick to the spatula. You may use a FRESH 1 mL pipette tip to assist in placing the agar."
+end
+
+step
+  description: "Recap the petri dish"
+  note: "Replace the cap on the petri dish with our agar film, and prepare to use the microscope."
+end
+
+produce
+  r = "" from ons
+  location: "Bench"
+  note: "You will be taking this with you to the microscope lab"
+end
+
+log
+  return: {ons: r}
+end
+
+release [dish[0], test_tube[0]]
