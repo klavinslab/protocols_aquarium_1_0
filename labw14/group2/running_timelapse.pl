fd8833ae41af389cd5b0c35726cd7d1d634f7313
aquasonal <sonalj@uw.edu>
2014-02-12 15:11:18 -0800

    Update running_timelapse.pl

--- labw14/group2/running_timelapse.pl
+++ labw14/group2/running_timelapse.pl
@@ -1,1 +1,25 @@
-araldsjkfa;ljsdfj
+step
+  description: "Adjust the Nikon software settings for running timelapse"
+  note: "Click next to begin this protocol"
+end 
+
+step
+  description: " Time adjustments"
+  bullet: "Find the ND acquisition window and click on the Time tab"
+  bullet: "Phase: make sure the box for #1 is checked"
+  bullet: "Interval: set to 10 min"
+  bullet: "Duration: set to 8 hours"
+end
+
+step
+  description: "Test time-lapse"
+  bullet: "Click on '1 time loop'. This takes an image of all your cells only once."
+  bullet: "Find the LUTs window"
+  bullet: "Adjust the Ph GFP and GFP bandwidth according to the brightness level needed to make the cells visible"
+end
+
+step
+  description: "Run time-lapse"
+  bullet: "Click on 'play now' to begin the cycle"
+  bullet: "This protocol is now complete, and so is Day 2!" 
+end
