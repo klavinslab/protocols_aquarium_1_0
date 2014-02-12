c93ecd97ee44eb95e997985a5b269c973a151499
aquasonal <sonalj@uw.edu>
2014-02-12 13:24:14 -0800

    Update clean_up.pl

--- labw14/group2/clean_up.pl
+++ labw14/group2/clean_up.pl
@@ -1,1 +1,22 @@
-faklasd
+argument
+  id: sample(""), "Agar plate to be disposed"
+end
+
+step
+  description: "Go to the microscope room"
+  note: "Open the protocol in the microscope room and click next"
+end
+
+step
+  descrption: "Take out agar plate"
+  bullet: "Turn the big knob on the bottom left of the microscope in a clockwise direction."
+  bullet: "Open the micrscope incubator and loosen the screws." 
+  bullet: "Carefully take out the agar plate along with the ring."
+  bullet: "Take the ring out and discard the agar plate" 
+end
+
+step
+  description: "Turn off micrscope"
+  bullet: "Turn off everything labeled 2 - 6"
+  bullet: "Turn off the fluorescent light bulb (labeled 1)"
+end
