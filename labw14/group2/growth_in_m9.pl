65221a4217bd175f79e168fb10b7ba6537f296c8
hamsanchez <hamilton.sanch@gmail.com>
2014-02-11 22:48:03 -0800

    Update growth_in_m9.pl

--- labw14/group2/growth_in_m9.pl
+++ labw14/group2/growth_in_m9.pl
@@ -16,28 +16,30 @@ take
   pipette = "Serological Pipette"
 end
 
+step
+  description: "Pipette 2 microL into the 14 mL test tube"
+  warning: "Do not touch the pipette tip to the side of the 400 mL glucose flask!"
+  bullet: "Take the test tube rack from the bench to the media station"
+  bullet: "Attach the 5 mL serological pipette tip to the serological pipetter"
+  bullet: "Pipette 2mL of the M9 solution into the 14 mL test tube"
+end
 
 step
-  check:
-      "Dispose of 5 mL serological tip"
+  check: "Dispose of 5 mL serological tip"
 end
 
 release [pipette[0], M9[0]]
 
 take
-  description:
-      "These are located in B1.165 in the antibotics box"
+  description: "These are located in B1.165 in the antibotics box"
   amp = 1 "100X 1mL Ampicillin Aliquot"
   kan = 1 "200X 1mL Kanamycin Aliquot"
 end
 
 step
-  description:
-      "Pipette antibotics"
-  bullet:
-      "Pipette 20 microL of Ampicillin into the test tube"
-  bullet:
-      "Pipette 4 microL of Kanamycin into the test tube"
+  description: "Pipette antibotics"
+  bullet: "Pipette 20 microL of Ampicillin into the test tube"
+  bullet: "Pipette 4 microL of Kanamycin into the test tube"
 end
 
 release[amp[0], kan[0]]
@@ -47,10 +49,8 @@ take
 end
 
 step
-  description:
-      "Mix the overnight suspension culture into the M9 glucose solution"
-  note:
-      "Pipette 13 microL of the overnight suspension culture into the M9 solution"
+  description: "Mix the overnight suspension culture into the M9 glucose solution"
+  note: "Pipette 13 microL of the overnight suspension culture into the M9 solution"
 end
 
 produce
@@ -63,10 +63,8 @@ log
 end
 
 step
-  description:
-      "Store solution in incubator"
-  note:
-      "Place the solution in the 37 deg shaker incubator located at B13.425"
+  description: "Store solution in incubator"
+  note: "Place the solution in the 37 deg shaker incubator located at B13.425"
 end
 
 release[ons[0]]
