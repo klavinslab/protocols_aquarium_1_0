255f286ef51660b0b95181bd1cf7885f5bcb0ee9
aquasonal <sonalj@uw.edu>
2014-02-12 13:30:30 -0800

    Update clean_up.pl

--- labw14/group2/clean_up.pl
+++ labw14/group2/clean_up.pl
@@ -12,6 +12,7 @@ step
   bullet: "Turn the big knob on the bottom left of the microscope in a clockwise direction."
   bullet: "Open the micrscope incubator and loosen the screws." 
   bullet: "Carefully take out the agar plate along with the ring."
+  bullet: "Clean the lens of the microscope using a olympus lens cleaner and shut the incubator"
   bullet: "Take the ring out and discard the agar plate" 
 end
 
@@ -20,3 +21,12 @@ step
   bullet: "Turn off everything labeled 2 - 6"
   bullet: "Turn off the fluorescent light bulb (labeled 1)"
 end
+
+step
+  description: "Save your movie"
+  bullet: "Go to the computer and click through your images to check you results"
+  bullet: "Go to File -> Save As, and then save your movie as 'Repressilator_your-name_date' in D:/LABW14"
+  warning: "Make sure to not click anywhere else on the screen while the computer is saving the file."
+end
+
+
