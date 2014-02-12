e8e0d092113e3d0660a68381254d614f4050d0de
pollardt <pollardt@uw.edu>
2014-02-11 16:39:42 -0800

    Update denDNA.pl

--- labw14/group1/denDNA.pl
+++ labw14/group1/denDNA.pl
@@ -1,9 +1,9 @@
 step
-  description "This protocal provides the procedure to denature the extracted DNA library"
+  description: "This protocal provides the procedure to denature the extracted DNA library"
 end
 
 step
-  description "Select the approximate concentraton of your DNA library"
+  description: "Select the approximate concentraton of your DNA library"
   getdata
     C = string, ["4nM or greater", "approx 2nM"]
   end
