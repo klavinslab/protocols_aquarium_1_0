4abda0470aba58cba2b485e06faae8e8aa5105ac
hamsanchez <hamilton.sanch@gmail.com>
2014-02-12 13:10:43 -0800

    Update microscope.pl

--- labw14/group2/microscope.pl
+++ labw14/group2/microscope.pl
@@ -1,17 +1,9 @@
-argument
-  id: sample (""), "Agar plate to be examined"
-end
-
-take
-  agar_plate = item id
-end
-
 step
   description: "Go to the microscope room"
   note: "Go to the microscope room and resume the protocol from there. If already in the microscope room click next."
 end
 
-step
+step  
   description: "Setting up the microscope for use"
   note: "In the next few steps we will set up the microscope to track cells for our timelapse microscopy."
   warning: "THIS EQUIPMENT IS EXTREMELY EXPENSIVE! THE UTMOST CARE MUST BE USED WHEN HANDING THE MICROSCOPE"
@@ -20,30 +12,39 @@ end
 
 step
   description: "Turn on the fluorescent light bulb"
-  note: "This must be turned on first for it draws an extremely high current."
+  note: "Thsi is labeled with a 1 on it must be turned on first for it draws an extremely high current."
   warning: "FAILURE TO DO THIS FIRST MAY RESULT IN A BROKEN SYSTEM."
 end
 
 step
-  description: "Turn on the stage controller"
-  note: "This must be turned on second"
+  description: "Turn on the shutter controller"
+  note: "This is labeled with a 2 must be turned on second"
+end
+
+step
+  description: "Center the shutter controller"
+  bullet: "Flip the tuner switch up and down twice, and end it aligned in the center to reset the controller fully"
 end
 
 step
   description: "Turn on the microscope bright-light"
-  note: "This should be turned on third"
+  note: "This is labeled 3 and turned on third"
 end
 
 step
-  description: "Center the stage controller"
-  bullet: "Flip the switch up and down twice, and end it aligned in the center to reset the controller fully"
-end
-
+  description: "Turn on the microscope"
+  note: "This should be labeled number 4. The switch is a little difficult to find so you may need to feel around for it."
 step
   description: "Turn on the X-Y controller"
-  note: "This helps us pan through the surface to find cells at a fixed focal point z"
+  note: "This is labeld 4 and helps us pan through the surface to find cells at a fixed focal point z"
 
 step
   descrition: "Turn on the fine Z tunning controller"
-  note: "This adjusts our focal points when searching for cells"
+  note: "This is labeled 5 and adjusts our focal points when searching for cells"
 end
+
+step
+  description: "Microscope setup complete"
+  note: "Click next to procede to the next protocol"
+end
+
