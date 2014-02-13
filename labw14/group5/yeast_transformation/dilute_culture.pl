1797586b96cedf77067f609e6acdbe9d5370e6cf
alexbaryshev <baryshev@uw.edu>
2014-02-12 19:32:13 -0800

    Update dilute_culture.pl

--- labw14/group5/yeast_transformation/dilute_culture.pl
+++ labw14/group5/yeast_transformation/dilute_culture.pl
@@ -1,5 +1,5 @@
 argument
- x: sample, "Yeast_overnight_suspension_culture"
+ Yeast_overnight_suspension: sample array , "Yeast overnight suspension culture tube"
 end
 
 step
@@ -20,7 +20,7 @@ end
 step
  description: "Diluting cells in YPD"
  note: "Pour 25 mil of YPAD into flask. Then, pipette 500 uL of the yeast overnight suspension cultrue into the flask."
-  bullet: "Label the flask with your initials and date"
+ bullet: "Label the flask with your initials and date"
 end
 
 
