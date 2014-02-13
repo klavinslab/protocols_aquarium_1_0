7ae35e25258b6110c3cec39f24fc8db511efd8b1
pegahsz <pegahsz@uw.edu>
2014-02-13 14:41:29 -0800

    Create make_master_stock.pl

---
+++ labw14/group6/make_master_stock.pl
@@ -0,0 +1,12 @@
+argument
+    oligonucleotides: sample array, "Choose the oligonucleotides you want to make into a master stock"
+end
+
+produce
+  master_stock = 1 "Primer Pool"
+
+end
+
+log
+  return: { working_fragments: master_stock  }
+end
