ded1a3b751f36802010c809dd5b6bcfceb46e70c
hamsanchez <hamilton.sanch@gmail.com>
2014-02-12 20:04:21 -0800

    Create tracking_cells.pl

---
+++ labw14/group2/tracking_cells.pl
@@ -0,0 +1,70 @@
+step
+  description: "Track cells using the NIS-Elements software"
+  note: "Click next to begin this protocol"
+end
+
+step
+  description: "Setup your work station"
+  bullet: "Turn on the computer (if not already on)"
+  bullet: "Move the X-Y stage controller to the computer station"
+  bullet: "Press the black button on the remote focus controller to zero your position"
+end
+
+step
+  description: "Open the software"
+  note: "Open and load the Nikon NIS-Elements software on the desktop."
+end
+
+step
+  description: "Setup Nikon NIS-Elements UI"
+  bullet: "Click on the 'Device' menu"
+  bullet: "Click on the 'Manage device's' option"
+  bullet: "Click on the 'Nikon Ti' option in the pop-up menu"
+  bullet: "Press the 'Connect' button in the pop-up menu:
+  bullet: "Close the pop-up menu"
+  note: "If the device is already connected ignore this step"
+end
+
+step
+  description: "Setup Nikon NIS-Elements UI"
+  bullet: "Click on the 'View' menu"
+  bullet: "Click on the "Aquisition Controls' option
+  bullet: "Select both 'Cool Snaps ES Settings' and 'Ti Pad' options:
+end
+
+step
+  description: "Setup Nikon NIS-Elements UI"
+  bullet: "Click on the 'View' menu"
+  bullet: "Click on the 'Visualization Controls' option"
+  bullet: "Select both 'XY Overview' and 'LUTs' options"
+end
+
+step
+  description: "Merge the UI windows"
+  note: 
+press view menu
+visualization controls
+select both xy overview and LUTs
+
+adjust TI pad lamps to be medium contrast gradient
+
+press the turn probe on off button on the right side tool bar
+make it a small square
+
+press xy speed button once so that the light is completely green
+
+xy controller top knob controls the y bottom knob controls x
+
+search for cells using both xy remote and z focus control to find elongated single cells
+they will look like long black rods  when completely in focus.
+
+click on the xy position tab in nd acquisition and check the blank box under point name field to aquire the point
+
+offset z value by clicking on the value and pressing the off set z value which will off set by remote control value
+press black button to zero controller.
+
+find 9 other points leaving the offset as set on the first value
+this process may take approximately 30 minutes as finding proper cells for time laps is difficult.
+
+refocus all points 
+
