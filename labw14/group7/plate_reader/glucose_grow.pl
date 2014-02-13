20eddcafc134459e711388e6e4cf91d15df615c1
dstarkebaum <dasman@uw.edu>
2014-02-12 16:20:30 -0800

    Update glucose_grow.pl

--- labw14/group7/plate_reader/glucose_grow.pl
+++ labw14/group7/plate_reader/glucose_grow.pl
@@ -1,67 +1,64 @@
 argument
- Transformed_E_coli_Strain_tube_array: sample array, "An array of overnight suspensions of transformed E. Coli cells (in culture tubes)"
+ Transformed_E_coli_Strain_plate_array: sample array, "An array of agarose-streaked colonies to grow in 0.4 percent Glucose M9 media"
+#TODO: add an extra agument to account for the number of induction cycles to perform on plate reader
 end
 
 step
- description: "This protocol will prepare multiple cell cultures for long-term storage in glycerol stock."
+ description: "This protocol will grow multiple cell colonies in glucose."
 end
 
-n = length(Transformed_E_coli_Strain_tube_array)
+strains = length(Transformed_E_coli_Strain_plate_array)
+cycles = 4
+n = strains * cycles
+#TODO: cycles = 4 should be replaced with an arbitrary number of induction cycles passed as an argument
 
 take
-  cell_culture_tube_array = item Transformed_E_coli_Strain_tube_array
-  glycerol = 1 "50 percent Glycerol (sterile)"
-  sterile_tube = n "Cryo Tube"
-  note: "Be sure to use STERILE technique when removing cryo tubes from their container. This means:
-         shake out one of the sterile tubes into your gloved hand while holding the outside of the bag,
-         rather than putting your hand into the bag to take one out."
-end
-
-
-step
- description: "Add gylcerol to each cryo tube"
- check: "Pipette 900uL of the 50%% glycerol stock to each 1.5mL sterile tube"
- note: "Be sure to use STERILE technique while handling the gylcerol stock.  If you contaminate this stock
-        it wil ruin everyone else's experiments! This means:"
- bullet: "Loosen caps before loading the pipette tip"
- bullet: "Hold the cap in your gloved hand while extracting fluid with the micro-pipetter, rather than placing it on the lab bench."
- bullet: "Do not touch the inside of the tube with the micropipetter."
- bullet: "Do not re-use pipette tips."
+  plate_array = item Transformed_E_coli_Strain_plate_array
+  media_tube = 1 "50 mL Falcon Tube" #I could not find "50mL conical", so I used this instead
+  glucose = 1 "Minimal M9 Broth"
+  kan = 1 "200X 1 mL Kanamycin Aliquo"
+  amp = 1 "100X 1 mL Ampicillin Aliquot"
+  pipette = 1 "Serological Pipette"
+  tips = 2 "5 mL Serological Pipette Tips"
+  culture_tubes = m "14 mL Test Tube"
+ #TODO: Check all of these arguments with Leandra to confirm they are correct
 end
 
 step
- description: "Add cell culture to each cryo tube"
- note: "In the following steps, you will add cell culture to each cryo tube in turn, label it with its ID number, your initial, and todays date, then store it in the -80C freezer" 
+ description: "Prepare 40mL of M9 media with (1:200) ampicillin and (1:500) kanomyacin"
+ check: "Pipette 40mL of 0.4%% Glucose M9 media (%{glucose}) into the 50mL falcon tube (%{media_tube})"
+ check: "Pipette 200uL of Kanomyacin (%{kan}) into the 50mL falcon tube"
+ check: "Pipette 80uL of Ampicillin (%{amp}) into the 50mL falcon tube"
+ check: "Swish the falcon tube around a few times to mix"
 end
 
-i = 0
-r = []
-while i < n
- id = Transformed_E_coli_Strain_tube_array[i]
- step
-  description: "Add overnight suspension culture to the cryo tube"
-  check: "Pipette 900uL of cell suspension number %{id} to a new 1.5mL sterile tube (containing glycerol)"
-  check: "Vortex 1.5mL this sterile tube for 15s at speed 10 using the VWR analog vortex mixer."
- end
- 
- produce
-  s = 1 "transformed E. Coli in 50%% glycerol" from cell_culture_tube_array[i]
-  note: "Make sure to also add your initials and todays date (mm/dd/yyyy) in addition to the item ID number.
-        This glycerol/cell culture mixture will potentially be stored in the -80 C freezer for many months,
-        so be sure to label both the cap and the side of the tube for redundancy."
- end
- r = append(r,s[:id])
- i=i+1
+step
+ description: "Label the culture tubes"
+ check: "Label %{cycles} culture tubes with DL046"
+ check: "Labe1 %{cycles} culture tubes with DL104"
+ check: "Label %{cycles} culture tubes with DL147"
+ #TODO: replace DL064 etc with references from the sample array argument
 end
 
-#log
-#  return: {glycerol_stock_array_id: r}
-#end
+step
+ description: "Add 3mL media to each culture tube"
+ check: "Use electric serological pipette to add 3ml of you prepared (M9 with amp and kan) into each culture tube."
+end
 
-#step
-#  description: "the glycerol_stock_array_id is %{r}"
-#end
+step
+ description: "Add cells to each culture tube"
+ check: "Using a 100uL pipette tip, extract a single colony from plate DL046, and swish it around in one of the culture tubes marked DL046"
+ check: "Repeat this %{cycles - 1} more times with new pipette tips and new colonies, until you have 4 total culture tubes labeled DL046 each containing celss from the DL046 plate"
+ check: "Repeat the above two steps for each plate DL108 and DL 147, so that you end up with %{cycles} culture tubes containing each strain for a total of %{n} tubes" 
+#TODO: Repace the above code with something more modular, based on an arbitrary number of cycles and strains
+end
 
+step
+ desription: "place the tubes in the 37C shaker"
+ check: "Place the %{n} culture tubes in a tube rack and load them into the 37C shaker."
+ note: "This will incubate for 18hrs (overnight)"
+end
 
 #release [cell_culture_tube_array[0], glycerol[0]]
-release concat(cell_culture_tube_array, glycerol)
+release concat(plate_array, glucose, kan, amp, pipette)
+end
