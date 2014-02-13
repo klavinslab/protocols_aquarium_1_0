7d527d6dde8b3a368e16a66acb03389713b803fe
dstarkebaum <dasman@uw.edu>
2014-02-13 15:18:11 -0800

    Update glucose_grow.pl

--- labw14/group7/plate_reader/glucose_grow.pl
+++ labw14/group7/plate_reader/glucose_grow.pl
@@ -1,66 +1,56 @@
 argument
- plated_cells: sample("Transformed E coli Strains") array, "An array of agarose-streaked colonies to grow in 0.4 percent Glucose M9 media"
-#TODO: add an extra agument to account for the number of induction cycles to perform on plate reader
+  plated_cells: sample("Transformed E coli Strains") array, "An array of THREE agarose-streaked colonies to grow in glucose media"
+  glucose_media: sample, "The (glucose) media to grow the cells in"
+  #repeats: num, "The number of repeats of each cell culture to grow"
+  #We tried to make this a more generalized protocol, but the (lack of) capabilities in the language defeated us!
 end
 
 step
- description: "This protocol will grow multiple cell colonies in glucose."
+  description: "This protocol will grow multiple cell colonies in glucose."
 end
 
-strains = length(Transformed_E_coli_Strain_plate_array)
-regions = 4
-n = strains * regions
-#TODO: cycles = 4 should be replaced with an arbitrary number of induction cycles passed as an argument
+strains = length(plated_cells)
+n = strains * 4 #this could be "repeats"
+
 
 take
- media_tube = 1 "50 mL Falcon Tube" #I could not find "50mL conical", so I used this instead
- glucose = 1 "Minimal M9 Broth"
- kan = 1 "200X 1 mL Kanamycin Aliquot"
- amp = 1 "100X 1 mL Ampicillin Aliquot"
- pipette = 1 "Serological Pipette"
- tips = 2 "5 mL Serological Pipette Tips"
- culture_tubes = n "14 mL Test Tube"
- note: "Be sure to grab a tube rack to hold all of the culture tubes"
- #TODO: Check all of these arguments with Leandra to confirm they are correct
+  media = item glucose_media
+  pipette = 1 "Serological Pipette"
+  tip = 1 "5 mL Serological Pipette Tips"
+  culture_tubes = n "14 mL Test Tube"
+  note: "Be sure to grab a tube rack to hold all of the culture tubes"
 end
 
 step
- description: "Prepare 40mL of M9 media with (1:200) ampicillin and (1:500) kanomyacin"
- check: "Use the electric serological pipette to add 40mL of 0.4%% Glucose M9 media into the 50mL falcon tube"
- check: "Pipette 200uL of Kanomyacin into the 50mL falcon tube"
- check: "Pipette 80uL of Ampicillin into the 50mL falcon tube"
- check: "Swish the falcon tube around a few times to mix"
- check: "Dispose of your 5mL serological pipette tip in tip waste"
-end
-
-step
- description: "Label each of the culture tubes"
- check: "Label %{regions} culture tubes with DL046"
- check: "Label %{regions} culture tubes with DL104"
- check: "Label %{regions} culture tubes with DL147"
- #TODO: replace DL064 etc with references from the sample array argument... this may need a loop
+  description: "Label each of the culture tubes"
+  check: "Label 4 culture tubes with %{plated_cells[0][:id]} R1, %{plated_cells[0][:id]} R2... %{plated_cells[0][:id]} R4"
+  check: "Label 4 culture tubes with %{plated_cells[0][:id]} R1, %{plated_cells[0][:id]} R2... %{plated_cells[0][:id]} R4"
+  check: "Label 4 culture tubes with %{plated_cells[0][:id]} R1, %{plated_cells[0][:id]} R2... %{plated_cells[0][:id]} R4"
+  check: "Label 4 culture tubes with %{plated_cells[0][:id]} R1, %{plated_cells[0][:id]} R2... %{plated_cells[0][:id]} R4"
 end
 
 step
  description: "Add 3mL media to each culture tube"
- check: "Use the electric serological pipette to add 3ml of your M9 media (with amp and kan) into each culture tube."
+ check: "Use the electric serological pipette to add 3ml of media (ID %{media[:id]}) into each culture tube."
+ #I believe that %{media[:id]} should give the same thing as %{glucose_media}?
  check: "Dispose of your serological pipette tip in tip waste"
 end
 
-release [glucose[0], kan[0], amp[0], pipette[0]]
+release [pipette[0]]
 
 take
  plate_array = item plated_cells
 end
-
+i = 0
+for i<strains
 step
  description: "Add cells to each culture tube"
- check: "Using a 100uL pipette tip, extract a single colony from plate DL046, and swish it around in one of the culture tubes marked DL046"
- check: "Using a new 100uL pipette tip extract another single colony from plate DL046, and swirl it around in another culture tube labeled DL046. Repeat this until you have DL046 cells in all %{regions} of your tubes labeled DL046."
- check: "Repeat the above two steps for plates DL108 and DL147, so that you end up with %{strains} sets of %{regions} culture tubes loaded with cells, for a total of %{n} tubes" 
+ check: "Use a 100uL pipette tip (held in your hand) to extract a single colony from plate %{plated_cells[0][:id]}, and swish it around in the culture tube marked %{plated_cells[0][:id]} R1"
+ check: "Use a new 100uL pipette tip extract another single colony from the same plate, and swirl it around in the culture tube labeled %{plated_cells[0][:id]} R2"
+ check: "Repeat the above procedure for R3 and R4."
 #TODO: Repace the above code with something more modular, based on an arbitrary number of cycles and strains
 end
-
+#until you have DL046 cells in all %{regions} of your tubes labeled DL046.Repeat the above two steps for plates DL108 and DL147, so that you end up with %{strains} sets of %{regions} culture tubes loaded with cells, for a total of %{n} tubes" 
 step
  description: "Place the tubes in the 37C shaker"
  check: "Load the tube rack containing your %{n} cell culture tubes into the 37C shaker."
