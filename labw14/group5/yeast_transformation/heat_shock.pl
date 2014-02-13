73e46bfca5c4d2ef6fd73ea2d1db34b3b4c8aa16
alexbaryshev <baryshev@uw.edu>
2014-02-12 22:58:17 -0800

    Update heat_shock.pl

--- labw14/group5/yeast_transformation/heat_shock.pl
+++ labw14/group5/yeast_transformation/heat_shock.pl
@@ -1,3 +1,22 @@
 argument
+  yeast_plasmid_mixture: sample array, "Mixture to transform"
+end
 
+num = length(yeast_plasmid_mixture)
+
+step
+ description: "This protocol describes how to heat shock the yeast mixture"
+ warning: "You're going to shock heat %{num} yeast mixture tubes"
+end
+
+take
+  mixture_tube = item yeast_plasmid_mixture
+end
+
+ii = 0
+r = []
+
+while ii < length(Transformed_yeast_plate)
+
+  ii=ii+1
 end
