cc99029e501c3b5a765b1a3cc7041e917b0b7005
Erik909 <erik909@uw.edu>
2014-02-13 15:02:54 -0800

    Update prepare_media_2.pl

--- labw14/group7/plate_reader/prepare_media_2.pl
+++ labw14/group7/plate_reader/prepare_media_2.pl
@@ -8,6 +8,7 @@ argument
   #can we force the same lengths of array?  oherwise return error.
 end
 
+
 take
  media_tube = 1 "50 mL Falcon Tube"
  antibio1 = 1 antibio1
@@ -21,14 +22,17 @@ end
 
 ab_vol1 = volume/dilution1*1000
 ab_vol2 = volume/dilution2*1000
+media_name = media[:name]
+ab1_name = antibio1[:name]
+ab2_name = antibio2[:name]
 
 step
-  description: "Prepare %{volume} mL of %{media[:name]} media with the desired antibiotics"
+  description: "Prepare %{volume} mL of %{media_name} media with the desired antibiotics"
   check: "Attach the 25mL tip to the serological pipetter"
-  check: "Use the electric serological pipette to add  %{volume} mL of %{media[:name]} into the 50mL falcon tube"
+  check: "Use the electric serological pipette to add  %{volume} mL of %{media_name} into the 50mL falcon tube"
   check: "Dispose of your 25mL serological pipette tip in tip waste"
-  check: "Pipette %{ab_vol1} uL of %{antibio1[:name]} to the solution"
-  check: "Pipette %{ab_vol2} uL of %{antibio2[:name]} to the solution"
+  check: "Pipette %{ab_vol1} uL of %{ab1_name} to the solution"
+  check: "Pipette %{ab_vol2} uL of %{ab2_name} to the solution"
   check: "Vortex the solution for 15 seconds"
 end
 
