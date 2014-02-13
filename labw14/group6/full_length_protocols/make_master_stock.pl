427ead6f3ab969d9078f764672c9eba80a805377
pegahsz <pegahsz@uw.edu>
2014-02-13 14:27:12 -0800

    Update make_master_stock.pl

--- labw14/group6/full_length_protocols/make_master_stock.pl
+++ labw14/group6/full_length_protocols/make_master_stock.pl
@@ -10,7 +10,7 @@ n = length(oligonucleotides)
 
 take
   oligonuc = item oligonucleotides
-  molecular_g_h2o = 1 "Molecular Grade Water"
+  molecular_g_h2o = 1 "Molecular Biology Grade Water"
 end
 
 step
