4611b6807b2db9965ac00895d0da7ec1d774e802
Erik909 <erik909@uw.edu>
2014-02-13 14:59:49 -0800

    Update prepare_media_2.pl

--- labw14/group7/plate_reader/prepare_media_2.pl
+++ labw14/group7/plate_reader/prepare_media_2.pl
@@ -23,12 +23,12 @@ ab_vol1 = volume/dilution1*1000
 ab_vol2 = volume/dilution2*1000
 
 step
-  description: "Prepare %{volume} mL of %{media} media with the desired antibiotics"
+  description: "Prepare %{volume} mL of %{media[:name]} media with the desired antibiotics"
   check: "Attach the 25mL tip to the serological pipetter"
-  check: "Use the electric serological pipette to add  %{volume} mL of %{media} into the 50mL falcon tube"
+  check: "Use the electric serological pipette to add  %{volume} mL of %{media[:name]} into the 50mL falcon tube"
   check: "Dispose of your 25mL serological pipette tip in tip waste"
-  check: "Pipette %{ab_vol1} uL of %{antibio1} to the solution"
-  check: "Pipette %{ab_vol2} uL of %{antibio2} to the solution"
+  check: "Pipette %{ab_vol1} uL of %{antibio1[:name]} to the solution"
+  check: "Pipette %{ab_vol2} uL of %{antibio2[:name]} to the solution"
   check: "Vortex the solution for 15 seconds"
 end
 
