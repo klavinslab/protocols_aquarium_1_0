637c98506192a11d9f5b7b1fbef42e7422fc682c
alexbaryshev <baryshev@uw.edu>
2014-02-12 16:56:00 -0800

    Update start_overnight.pl

--- labw14/group5/yeast_transformation/start_overnight.pl
+++ labw14/group5/yeast_transformation/start_overnight.pl
@@ -1,7 +1,21 @@
 argument
+  Transformed_yeast_plate: sample, "A plate with yeast colonies"
+end
+
+take
+  f
+end
+
+ii = 0
+
+while ii < length(Transformed_yeast_plate)
+
+  produce
+      y = 1 "Overnight suspension culture" from plate[ii]
+  end
 
 end
 
-produce
-
-
+log
+  return: { yeast_overnight_suspension: y }
+end
