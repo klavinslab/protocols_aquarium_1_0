c3f9df9bf67a3a892331ea3776c34de28036a75b
pegahsz <pegahsz@uw.edu>
2014-02-12 18:01:20 -0800

    Update make_master_stock.pl

--- labw14/group6/make_master_stock.pl
+++ labw14/group6/make_master_stock.pl
@@ -1,3 +1,5 @@
+# This protocol assumes six total primers in the oligo mix
+
 argument
   oligonucleotides: sample array, "Select the oligonucleotides you want to make into a master stock"
 end
@@ -9,20 +11,25 @@ n = length(oligonucleotides)
 take
   oligonuc = item oligonucleotides
   molecular_g_h2o = "Molecular Grade Water"
-  
+  tube = 1 "1.5 mL Eppendorf Tube"
 end
 
 step
-  description: 
-    "This protocol describes how to create a master stock from desired oligonucleotides"
+  description: "This protocol describes how to create a master stock from desired oligonucleotides"
 end
 
 step
-  description: ""
-  note: ""
+  description: "Label the 1.5 mL Eppendorf tube with your initials and date"
+  note: "place the tube in a test tube rack"
 end
 
+step
+  description: "Pipette 480 μL of Molecular grade water into the tube"
+end
 
+step
+  description: "Add 10 μL of each primer stock into each tube"
+end
 
 produce
   master_stock = 1 "Primer Pool"
