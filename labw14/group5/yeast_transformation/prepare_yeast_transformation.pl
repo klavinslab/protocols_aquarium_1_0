e8c33349c164755d75bd0b707195c5ac3ef846ab
alexbaryshev <baryshev@uw.edu>
2014-02-12 22:12:05 -0800

    Update prepare_yeast_transformation.pl

--- labw14/group5/yeast_transformation/prepare_yeast_transformation.pl
+++ labw14/group5/yeast_transformation/prepare_yeast_transformation.pl
@@ -1,7 +1,35 @@
 argument
-
-end 
-
-produce
-
+  yeast_aliquot: sample array, "Diluted yeast culture"
+  fragment: sample, "Digested plasmid"
 end
+
+num = length(yeast_aliquot)
+
+step
+ description: "This protocol describes how to prepare yeast trasformation mix"
+ warning: "You're going to make %{num} overnight suspension tubes"
+end
+
+take
+  yeast_tube = item yeast_aliquot
+  digested_plasmid = item fragment
+end
+
+ii = 0
+r = []
+
+while ii < length(Transformed_yeast_plate)
+
+  produce
+      y = 1 "???????????????"
+  end
+  
+  r = append(r,y[:id])
+  ii=ii+1
+end
+
+log
+  return: { yeast_plasmid_mixture: r }
+end
+
+release concat(yeast_tube, digested_plasmid,)
