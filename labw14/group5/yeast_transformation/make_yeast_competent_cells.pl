07dbfb174ae6704d2f684a8346c2c5f3948fe86b
alexbaryshev <baryshev@uw.edu>
2014-02-12 22:04:33 -0800

    Update make_yeast_competent_cells.pl

--- labw14/group5/yeast_transformation/make_yeast_competent_cells.pl
+++ labw14/group5/yeast_transformation/make_yeast_competent_cells.pl
@@ -1,5 +1,34 @@
 argument
-
+  yeast_250ml_flask: sample array, "Diluted yeast culture"
 end
 
-produce
+num = length(yeast_250ml_flask)
+
+step
+ description: "This protocol describes how to make competent yeast cells"
+ warning: "You're going to make %{num} overnight suspension tubes"
+end
+
+take
+  falcon_tube = num "50 mL Falcon Tube"
+  flask = item yeast_250ml_flask
+end
+
+ii = 0
+r = []
+
+while ii < length(Transformed_yeast_plate)
+
+  produce
+      y = 1 "Yeast aliquot"
+  end
+  
+  r = append(r,y[:id])
+  ii=ii+1
+end
+
+log
+  return: { yeast_aliquot: r }
+end
+
+release concat(falcon_tube, flask)
