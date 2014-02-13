1ea0a2286a32e24a8044e0e192e9cfb7f2d7a31d
alexbaryshev <baryshev@uw.edu>
2014-02-13 09:51:11 -0800

    Update start_overnight.pl

--- labw14/group5/yeast_transformation/start_overnight.pl
+++ labw14/group5/yeast_transformation/start_overnight.pl
@@ -1,8 +1,8 @@
 argument
-  Transformed_yeast_plate: sample array, "A plate with yeast colonies"
+  transformed_yeast_plate: sample array, "A plate with yeast colonies"
 end
 
-num = length(Transformed_yeast_plate)
+num = length(transformed_yeast_plate)
 
 step
  description: "This protocol describes how to make an overnight yeast suspension"
@@ -10,16 +10,16 @@ step
 end
 
 take
-  test_tube = num "14 mL Test Tube"
-  plate = item Transformed_yeast_plate
+  test_tube    = num "14 mL Test Tube"
   media_bottle = 1 "800 mL YPAD liquid (sterile)"
-  falcon_tube = 1 "50 mL Falcon Tube"
+  falcon_tube  = 1 "50 mL Falcon Tube"
+  plate        = item transformed_yeast_plate
 end
 
 ii = 0
 r = []
 
-while ii < length(Transformed_yeast_plate)
+while ii < length(transformed_yeast_plate)
 
 #  produce
 #      y = 1 "Overnight suspension culture" from plate[ii]
@@ -31,6 +31,8 @@ while ii < length(Transformed_yeast_plate)
     inuse:0
   end
   
+  release plate[ii]
+  
   r = append(r,test_tube[ii][:id])
   ii=ii+1
 end
@@ -39,4 +41,4 @@ log
   return: { yeast_overnight_suspension: r }
 end
 
-release concat(falcon_tube, plate, media_bottle)
+release concat(falcon_tube, media_bottle)
