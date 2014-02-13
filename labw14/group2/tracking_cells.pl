cae5036834ed0371df501a0ef2f78b59c8876872
hamsanchez <hamilton.sanch@gmail.com>
2014-02-12 21:25:08 -0800

    Update tracking_cells.pl

--- labw14/group2/tracking_cells.pl
+++ labw14/group2/tracking_cells.pl
@@ -28,43 +28,112 @@ end
 step
   description: "Setup Nikon NIS-Elements UI"
   bullet: "Click on the 'View' menu"
-  bullet: "Click on the "Aquisition Controls' option
-  bullet: "Select both 'Cool Snaps ES Settings' and 'Ti Pad' options:
+  bullet: "Hover over the 'Aquisition Controls' sub menu"
+  bullet: "Select both 'Cool Snaps ES Settings' and 'Ti Pad' options"
 end
 
 step
   description: "Setup Nikon NIS-Elements UI"
   bullet: "Click on the 'View' menu"
-  bullet: "Click on the 'Visualization Controls' option"
+  bullet: "Hover over the 'Visualization Controls' option"
   bullet: "Select both 'XY Overview' and 'LUTs' options"
 end
 
 step
   description: "Merge the UI windows"
-  note: 
-press view menu
-visualization controls
-select both xy overview and LUTs
+  note: "Click and drag the UI windows into each other to merge them as tabs"
+  note: "The program is a little finiky so when dragging the pop-ups drag them to align with the tabs of another window before dropping"
+end
 
-adjust TI pad lamps to be medium contrast gradient
+step
+  description: "Open the ND Aquisition window"
+  bullet: "Click on the 'View' menu"
+  bullet: "Hover over the 'Acquisition Controls' sub menu"
+  bullet: "Click on the 'ND Aquisition' option"
+  note: "Don't merge this window with the others. It will make your task more difficult."
+end
 
-press the turn probe on off button on the right side tool bar
-make it a small square
+step
+  description: "Turn on the camera"
+  note: "Press the play button on the tool bar under the menu to turn on the camera"
+end
 
-press xy speed button once so that the light is completely green
+step
+  description: "Adjust the lighting"
+  bullet: "Select the 'TI Pad' tab"
+  bullet: "Click on the red minus sign button to turn it green"
+  bullet: "Slide the marker next to the green bar to adjust the light allowed by the lens"
+  note: "The light should be in a grey ish contrast where you can see texture on the screen"
+end
 
-xy controller top knob controls the y bottom knob controls x
+step
+  description: "Adjust the LUTs"
+  bullet: "Select the 'LUTs' tab"
+  bullet: "Adjust the left and right cursors on the LUTs tab to encompass the peaks"
+  note: "This must be played by ear, sometimes encompassing the peaks makes it too bright so add some dark regions into the range to improve contrast"
+end
 
-search for cells using both xy remote and z focus control to find elongated single cells
-they will look like long black rods  when completely in focus.
+step
+  description: "Turn on the probe"
+  bullet: "Navigate to the right side of the screen and locate the tool bar"
+  bullet: "Click on the top most option of the tool bar to open the probe box"
+  bullet: "Click and drag on the black edges of the box to create a 2x2 inch box at the center of the screen"
+end
 
-click on the xy position tab in nd acquisition and check the blank box under point name field to aquire the point
+step
+  description: "Adjust the XY controller"
+  bullet: "Locate the X-Y stage controller next to the computer"
+  bullet: "Press the X-Y speed (top-right) button and ensure that the light is fully green"
+  bullet: "The top dial adjusts the Y axis and the bottom dial adjusts the X axis"
+  note: "We will deal with this in the next few slides"
+end
 
-offset z value by clicking on the value and pressing the off set z value which will off set by remote control value
-press black button to zero controller.
+step
+  description: "Start looking for cells"
+  bullet: "Using the remote focus controller to adjust the focus on the screen"
+  bullet: "Look for white splotches which turn black when in focus"
+  bullet: "We want to look for black pill shaped rods, these are our cells"
+  bullet: "When a cell is found on a focal point we will look for cells on the same XY plane"
+  note: "This process takes approximately 30 minutes. If no cells are found in the region adjust the xy controller"
+        "and start focusing in another region. Most cells will be found in similar focal planes"
+  note: "Once any cell is found click next"
+end
 
-find 9 other points leaving the offset as set on the first value
-this process may take approximately 30 minutes as finding proper cells for time laps is difficult.
+step
+  description: "Selecting ideal cells"
+  bullet: "Select the 'X-Y overview' tab; this will help you keep track of our selected cells"
+  bullet: "We want to find elongated black rods, short rods are probably dead cells"
+  bullet: "Click on the 'X-Y position' tab in the ND aquisition window"
+  bullet: "When a desirable cell is found center the black box probe around it"
+  bullet: "Click the blank check box in the 'X-Y position' tab in th eND aquisition window"
+  note: "This selects the current X-Y position and stores it. It will also been seen on your X-Y overview map"
+end
 
-refocus all points 
+step
+  description: "Set the Z-offset"
+  bullet: "Click the z-position of our cell in the 'X-Y position' tab"
+  bullet: "Click on the 'Offset z-position' button"
+  bullet: "Press the black button on the remote focus controller to zero the z-axis"
+  note: "Offsetting the z-position will set the value to the current controller value"
+end
+
+step
+  description: "Continue selection cells"
+  bullet: "Select 9 to 11 more ideal cells"
+  bullet: "Do not offset the z-position anymore it is already done"
+  note: "Cells in vertically aligned pairs are freshly divided and are great to select"
+  note: "The best cells will have plenty of space around them, too many cells will make the timelapse moot."
+end
+
+step
+  description: "Refocus all points"
+  bullet: "Select an X-Y point in the ND Aquisition window"
+  bullet: "Toggle the focus controller to refocus the cell if it unfocused"
+  bullet: "Repeat for each X-Y position selected"
+end
+
+step
+  description: "Run Timelapse"
+  bullet: "Proceed to next protocol to run timelapse"
+end
 
