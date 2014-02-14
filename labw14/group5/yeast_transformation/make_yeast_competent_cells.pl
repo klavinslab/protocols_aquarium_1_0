0b9ab933b6c5cc28369934c1ed54cc8ac226f5c9
alexbaryshev <baryshev@uw.edu>
2014-02-14 11:36:35 -0800

    Update make_yeast_competent_cells.pl

--- labw14/group5/yeast_transformation/make_yeast_competent_cells.pl
+++ labw14/group5/yeast_transformation/make_yeast_competent_cells.pl
@@ -8,13 +8,6 @@ step
   description: "This protocol describes how to make yeast competent cell aliquot"
 end
 
-step
-  description: "You can make up to 6 aliquot tubes out of one 25ml yeast flask"
-  getdata
-    aliquot_number: number, "Desired number of aliquot tubes to make out of one 25ml flask"
-  end
-end
-
 take
   falcon_tube = num "50 mL Falcon Tube"
   flask = item yeast_250ml_flask
@@ -25,8 +18,18 @@ r = []
 
 while ii < length(yeast_250ml_flask)
 
+  step
+    description: "You can make up to 6 aliquot tubes out of one 25ml yeast flask"
+    getdata
+      aliquot_number: number, "Desired number of aliquot tubes to make out of one 25ml flask"
+    end
+  end
+
   produce
       y = 1 "Yeast Competent Aliquot" from flask[ii]
+      release flask[ii]
+      note: "Write the above id number on the aliquot tube's side. Place on the bench."
+      location:"Bench"      
   end
   
   r = append(r,y[:id])
