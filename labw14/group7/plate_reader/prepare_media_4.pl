af7375444b719939a7c5aff834e9a86be3aacda1
dstarkebaum <dasman@uw.edu>
2014-02-13 23:51:06 -0800

    Create prepare_media_4.pl

---
+++ labw14/group7/plate_reader/prepare_media_4.pl
@@ -0,0 +1,90 @@
+#By David Starkebaum and Erik Josberger, 2014/02/13
+argument
+  media_name: object, "Please selct the glucose media which you would add antibiotics to"
+  antibios: string array, "Please choose the antiobiotics you would like to add. Enter 'amp' for '100X 1 mL Ampicillin Aliquot', 'kan' for '200X 1 mL Kanamycin Aliquot', or 'chlor' for '1000X 1 mL Chloramphenicol Aliquot'"
+  dilutions: number array, "Please choose the dilution factors for each antibiotic selected above, where the ratio of antibiotic:media is 1:x"
+  total_volume: number, "The desired total volume in mL (<50mL)"
+  #can we force the same lengths of array? oherwise return error.
+end
+
+#if length(antibios) != length(dilutions)
+  #make some error and abort
+#end
+
+take
+ media_tube = 1 "50 mL Falcon Tube"
+ media = 1 media_name
+ #media[:id] == media_base
+ pipette = 1 "Serological Pipette"
+ tip = 1 "25 mL Serological Pipette Tips"
+end
+
+#media_name == media[0][:name]
+step
+  description: "Add media to the falcon tube"
+  check: "Attach the 25mL tip to the serological pipetter"
+  check: "Use the electric serological pipette to add %{total_volume} mL of '%{media_name}' into the 50mL falcon tube"
+  check: "Dispose of your 25mL serological pipette tip in tip waste"
+end
+
+release concat(media, pipette)
+
+i=0
+antibio_volumes = []#the volumes of antibiotic solutions in uL
+antibio_objects = []#the object references (an array of json tables) of each antibiotic
+while i<length(antibios)
+  v = total_volume*1000/dilutions[i]
+  antibio_volumes = append(antibio_volumes, v)
+  antibio_abr = antibios[i]
+  if antibio_abr == "amp"
+    take
+      a = 1 "100X 1 mL Ampicillin Aliquot"
+    end
+  end #I wanted to use elseif here but I got a parsing error: "Expected '=' at 'antibio_abr'"
+  if antibio_abr == "kan"
+    take
+      a = 1 "200X 1 mL Kanamycin Aliquot"
+    end
+  end
+  if antibio_abr == "chlor"
+    take
+      a = 1 "1000X 1 mL Chloramphenicol Aliquot"
+    end
+  #else #I cannot use the else here, because all of the elseifs did not work. As is, this would only give an error if it was not 'chlor'
+    #someone entered the wrong choice
+  end
+  antibio_objects = append(antibio_objects, a)
+  i=i+1
+end
+
+
+j=0
+while j<length(antibios)
+  antibio_abr = antibios[i]
+  antibio_hash = antibio_objects[j]
+  antibio_name = antibio_hash[:name]
+  v = antibio_volumes[j]
+  step
+    description: "Add %{antibio_abr} to the solution"
+    check: "Pipette %{antibio_volume} uL of '%{antibio_name}' to the 50mL falcon tube"
+  end
+  j=j+1
+end
+
+step
+  description: "Vortex the solution"
+  check: "Press the 50mL Falcon tube to the vortexer for 15 seconds"
+end
+
+
+#produce
+# m = 1 "Prepared Media" from media_tube
+# location: "Bench"
+#end
+
+release antibio_objects
+
+#I am not sure what to put in here yet... come back to this later. It would be great to be able to generate a label bu concatenating the antibio_abr[i] and media strings together
+#log
+# return: {prepared_media: m}
+#end
