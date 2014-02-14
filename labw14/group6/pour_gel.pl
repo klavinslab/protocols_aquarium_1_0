16d2c36b4668b4daa1f966bf21bf5d5f6e018535
pegahsz <pegahsz@uw.edu>
2014-02-14 14:14:17 -0800

    Create pour_gel.pl

---
+++ labw14/group6/pour_gel.pl
@@ -0,0 +1,11 @@
+argument
+  percentage: number, "Enter the percentage gel to make (e.g. 1 = 1%)"
+  comb_n: number, "Enter the number of combs to use (1 or 2)."
+end
+
+produce
+  gel = 1 "50 mL 1 Percent Agarose Gel in Gel Box"
+  location: "A7.325"
+  note: "Write the id on a piece of lab tape. Add your initials as well."
+  release gel_box
+end
