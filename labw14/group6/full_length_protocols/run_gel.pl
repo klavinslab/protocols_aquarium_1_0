921dfb60bf308eb7a639bd70c2d2c43fe0805610
pegahsz <pegahsz@uw.edu>
2014-02-14 12:04:04 -0800

    Update run_gel.pl

--- labw14/group6/full_length_protocols/run_gel.pl
+++ labw14/group6/full_length_protocols/run_gel.pl
@@ -48,7 +48,7 @@ end
 dye_volume = fragment_volume / 5.0
 step
   description: "Add loading dye to the PCR fragment"
-  note: "Using a 10 µL or 100 µL pipetter, add %{dye_volume} µL of loading dye to the PCR results with id %{colony}."
+  note: "Using a 10 µL or 100 µL pipetter, add %{dye_volume} µL of loading dye to the PCR results with id %{assemblies}."
   image: "gel_add_loading_dye"
 end
 
