eee10604aacdd292b4b40f8407c1b011cf04480f
pegahsz <pegahsz@uw.edu>
2014-02-14 12:32:19 -0800

    Update gel_purify.pl

--- labw14/group6/full_length_protocols/gel_purify.pl
+++ labw14/group6/full_length_protocols/gel_purify.pl
@@ -1,20 +1,24 @@
 argument
-  gel: sample, "Choose the Gel Slice you need to purify."
+  gel: sample array, "Choose the Gel Slices you need to purify."
 end
 
 take
-  gel_slice = item gel
+  gel_slices = item gel
 end
 
+T1 = gel[0]
+T2= gel[1]
+
 step
-  description: "Weigh the gel slice on a scale at A5.300"
+  description: "Weigh the gel sliec with id %{T1} on a scale at A5.300"
   note: "Use an empty 1.5 mL tube to zero the scale first, then put the gel slice tube on the scale."
   getdata
-    gel_weight: number, "Enter the gel slice weight shown on the scale in mg. If it shows 0.134 on scale, you enter 134 below."
+    gel_weight1: number, "Enter the gel slice weight shown on the scale in mg. If it shows 0.134 on scale, you enter 134 below."
   end
 end
 
-while gel_weight > 200 
+
+while gel_weight1 > 200 
   step
     description: "Go back to Gel room to trim you gel slice"
     note: "Wait for the gel slice to thaw, put on the transiluminator, trim the non DNA part of your gel."
@@ -28,78 +32,103 @@ while gel_weight > 200
 end
 end
 
+step
+  description: "Weigh the gel sliec with id %{T2} on a scale at A5.300"
+  note: "Use an empty 1.5 mL tube to zero the scale first, then put the gel slice tube on the scale."
+  getdata
+    gel_weight2: number, "Enter the gel slice weight shown on the scale in mg. If it shows 0.134 on scale, you enter 134 below."
+  end
+end
 
-QG_volume = 3 * gel_weight
+while gel_weight2 > 200 
+  step
+    description: "Go back to Gel room to trim you gel slice"
+    note: "Wait for the gel slice to thaw, put on the transiluminator, trim the non DNA part of your gel."
+  end
+  step
+    description: "Weigh the gel slice on a scale at A5.300"
+    note: "Use an empty 1.5 mL tube to zero the scale first, then put the gel slice tube on the scale."
+    getdata
+      gel_weight: number, "Enter the gel slice weight shown on the scale in mg. If it shows 0.134 on scale, you enter 134 below."
+    end
+end
+end
 
-QG_volume_plus = QG_volume + 100
+QG_volume1 = 3 * gel_weight1
+QG_volume2 = 3 * gel_weight2
+
+QG_volume_plus = QG_volume1 + 100
+QG_volume_plus = QG_volume2 + 100
 
 step
-  description: "Add %{QG_volume} µL buffer QG into the gel slice tube with id %{gel}"
+  description: "Purification"
+  note: "Add %{QG_volume1} µL buffer QG into the gel slice tube with id %{T1}"
+  note: "Add %{QG_volume2} µL buffer QG into the gel slice tube with id %{T2}"
 end
 
 step
-  description: "Incubate the tube in a 50 C heat block at B3.340 for 10 minutes."
+  description: "Incubate the tubes in a 50 C heat block at B3.340 for 10 minutes."
   note: "Vortex after 5 minutes of incubation and put back in the 50 C until 10 minutes totoal time reached"
 end
 
 step
-  description: "Take out the tube from 50 C heat block"
-  note: "Place it on bench in a tube rack."
+  description: "Take out the tubes from 50 C heat block"
+  note: "Place them on bench in a tube rack."
 end
 
 step
-  description: "Label a pink QIAquick Spin column"
+  description: "Label two pink QIAquick Spin columns"
   note: "Write you initials on it."
 end
 
 step
-  description: "Add the tube content to the labeled QIAquick Spin column"
-  note: "Tube content volume shoud be around %{QG_volume_plus} µL."
+  description: "Add each tube content to seperate labeled QIAquick Spin columns"
+  note: "Tube content volume shoud be around %{QG_volume_plus1} µL."
 end
 
 step
   description: "Centrifuge at top speed (Make sure to balance in the centrifuge!)"
-  bullet: "Place the tube into centrifuge at B14.320, balance with another team's column."
+  bullet: "Place the tubes into centrifuge at B14.320."
   bullet: "Select 17,000 g and 1 minutes, press start."
 end
 
 step
-  description: "Take the column out of centrifuge and empty collection tube"
-  note: "Empty the waste in collection tube into a liquid waster collector on bench, put the collection tube back on afterwards."
+  description: "Take the columns out of centrifuge and empty collection tubes"
+  note: "Empty the waste in collection tubes into a liquid waster collector on bench, put the collection tubes back on afterwards."
 end
 
 step
-  description: "Add 750 µL PE buffer to column and wait 3 minutes"
+  description: "Add 750 µL PE buffer to columns and wait 3 minutes"
 end
 
 step
   description: "Centrifuge at top speed (Make sure to balance in the centrifuge!)"
-  bullet: "Place the tube into centrifuge, balance with another team's column."
+  bullet: "Place the tubes into centrifuge."
   bullet: "Select 17,000 g and 1 minutes, press start."
 end
 
 step
-  description: "Take the column out of centrifuge and empty collection tube"
-  note: "Empty the waste in collection tube into a liquid waster collector on bench, put the collection tube back on afterwards."
+  description: "Take the columns out of centrifuge and empty collection tubes"
+  note: "Empty the waste in collection tubes into a liquid waster collector on bench, put the collection tubes back on afterwards."
 end
 
 
 step
   description: "Remove the residual PE buffer by centrifuge"
-  bullet: "Place the tube into centrifuge, balance with another team's column."
+  bullet: "Place the tubes into centrifuge."
   bullet: "Select 17,000 g and 1 minutes, press start."
   warning: "Make sure to balance in the centrifuge!"
 end
 
 step
-  description: "Take the column out of centrifuge and empty collection tube"
-  note: "Empty the waste in collection tube into a liquid waster collector on bench, put the collection tube back on afterwards."
+  description: "Take the columns out of centrifuge and empty collection tubes"
+  note: "Empty the waste in collection tubes into a liquid waster collector on bench, put the collection tubes back on afterwards."
 end
 
 step
-  description: "Label a 1.5 mL tube and place column into it"
-  bullet: "Write you date and initials on the side of the tube."
-  bullet: "Place the column into the labeled 1.5 mL tube."
+  description: "Label two 1.5 mL tubes and place columns into it"
+  bullet: "Write you date and initials on the side of the tubes."
+  bullet: "Place the columns into the labeled 1.5 mL tubes."
 end
 
 step
@@ -109,17 +138,17 @@ end
 
 step
   description: "Centrifuge at top speed"
-  bullet: "Place labeled tube with the column into centrifuge, balance with another team's column."
+  bullet: "Place labeled tubes with the columns into centrifuge."
   bullet: "Select 17,000 g and 1 minutes, press start."
 end
 
 step
-  description: "Dicard the column and place the tube on the bench"
-  bullet: "Column can go to the tip waster collector."
+  description: "Dicard the columns and place the tubes on the bench"
+  bullet: "Columns can go to the tip waster collector."
 end
     
 produce
-  r = 1 "Fragment Stock" of "fLAB1"
+  r = 1 "Fragment Stock" 
   location: "Bench"
   release gel_slice
 end
