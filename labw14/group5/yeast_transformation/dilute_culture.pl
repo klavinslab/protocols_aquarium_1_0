2b860e38e7d9116653c388a3c93bd616b2c89dc3
alexbaryshev <baryshev@uw.edu>
2014-02-14 10:38:47 -0800

    Update dilute_culture.pl

--- labw14/group5/yeast_transformation/dilute_culture.pl
+++ labw14/group5/yeast_transformation/dilute_culture.pl
@@ -22,20 +22,26 @@ step
      and put the tube(s) into a tube holder on your bench."
 end
 
-step
- description: "Diluting cells in YPD"
- note: "Pour 25 mil of YPAD into flask. Then, pipette 500 μL of the yeast overnight suspension cultrue into the flask."
- bullet: "Label the flask with your initials and date."
-end
-
-ii = 0
-r  = []
+ii  = 0
+ind = 0
+r   = []
 
 while ii < length(yeast_overnight_suspension)
+
+  ind = ind + 1
+
+  step
+    description: "Diluting cells in YPD"
+    bullet: "Using %{serological_pipette} serological pipette pour 25 mil of YPAD into the flask N%{ind}."
+    bullet: "Then, using 1000 μL pipette pour 500 μL of the yeast overnight suspension cultrue into the flask."
+    bullet: "Label the flask with your initials and date."
+  end
 
   produce
       y = 1 "Yeast Overnight Suspension" from yeast_suspension_tube[ii]
       release flask[ii]
+      note: "Write the above id number on the flask's side. Place the flask in the 30 C Shaker Incubator."
+      location:"B13.125"
   end
   
   r = append(r,y[:id])
