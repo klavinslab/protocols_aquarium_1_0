9dc2040b7a988d6b4e2fdc6e3e82ca9dc459b7e0
tamimeur <tamimeur@gmail.com>
2014-02-13 17:39:33 -0800

    Update TA_gibson_new.pl

--- cloning/assembly_digest/TA_gibson_new.pl
+++ cloning/assembly_digest/TA_gibson_new.pl
@@ -7,19 +7,18 @@ end
 information "Scarlessly assemble DNA using fragments with terminal homologies."
 
 include "cloning/assembly_digest/TA_gibson_input.pl"
-  test_input=fragment_input
-  test_conc=conc_input
+  fragments=fragment_input
+  concentrations=conc_input
 end
 
 step
   description: "This protocol prepares Gibson reactions in a strip-well tube."
 end
 
-num_gibs=length(gibson_pieces)
+num_gibs=length(concentrations)
 
-#two_d_arr = [[1,1],[2,2]]
 
-#test_input = [["F1","F2"],["F3","F4"]]
+#test_input = [F1,F2,F3,F4]
 #test_conc = [[2,2],[2,2]]
 
 take
@@ -27,18 +26,15 @@ take
   master_mix = 1 "Gibson Aliquot"
 end
 
-x = length(test_input)
+x = length(fragments)
+
+#step
+#  description: "The first element in the array of hashes is %{fragments} the length of the thing is %{x}"
+#end
 
 step
-  description: "The first element in the array of hashes is %{test_input} the length of the thing is %{x}"
-end
-
-y = test_input[0][0]
-z = test_conc[0][0]
-
-step
-  description: "BTW test_input[0][0] = %{z} uL of %{y} Grab a 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
-  check: "Label the right most well with the letters DY"
+  description: "Grab a 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
+  check: "Label the right most well with the letters TA"
   check: "Label the left most well with the letter A"
   note: "These wells will be refered to as 1 - 12"
 end
@@ -46,7 +42,7 @@ end
 if num_gibs > 12
   step
     description: "Grab a second 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
-    check: "Label the right most well with the letters DY"
+    check: "Label the right most well with the letters TA"
     check: "Label the left most well with the letter B"
     note: "These wells will be refered to as 13 - 24"
   end
@@ -54,7 +50,7 @@ end
 if num_gibs > 24
   step
     description: "Grab a third 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
-    check: "Label the right most well with the letters DY"
+    check: "Label the right most well with the letters TA"
     check: "Label the left most well with the letter C"
     note: "These wells will be refered to as 25 - 36"
   end
@@ -62,7 +58,7 @@ end
 if num_gibs > 36
   step
     description: "Grab a fourth 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
-    check: "Label the right most well with the letters DY"
+    check: "Label the right most well with the letters TA"
     check: "Label the left most well with the letter D"
     note: "These wells will be refered to as 37 - 48"
   end
@@ -70,7 +66,7 @@ end
 if num_gibs > 48
   step
     description: "Grab a fourth 12 strip-well PCR tube and cap, and rest it in a green PCR tube rack. With the numbers FACING YOU, do the following:"
-    check: "Label the right most well with the letters DY"
+    check: "Label the right most well with the letters TA"
     check: "Label the left most well with the letter E"
     note: "These wells will be refered to as 49 - 60"
   end
