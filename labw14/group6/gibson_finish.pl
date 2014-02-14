cc4233cd2c5afb3bb6e67d33c67f1f1ad532e64f
pegahsz <pegahsz@uw.edu>
2014-02-14 14:12:18 -0800

    Create gibson_finish.pl

---
+++ labw14/group6/gibson_finish.pl
@@ -0,0 +1,8 @@
+step
+  description: "Pick up the tube you incubated on the 50 C heat block"
+  note: "Place in the M20 box slot specified in next page."
+end
+
+produce
+  r = 1 "Gibson Reaction Result"
+end
