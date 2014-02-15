7f69fc4db4ffbea46336520336c5d264cb484530
alexbaryshev <baryshev@uw.edu>
2014-02-14 16:54:47 -0800

    Update start_overnight.pl

--- labw14/group5/yeast_transformation/start_overnight.pl
+++ labw14/group5/yeast_transformation/start_overnight.pl
@@ -6,7 +6,7 @@ num = length(transformed_yeast_plate)
 
 step
  description: "This protocol describes how to make an overnight yeast suspension"
- check: "%{transformed_yeast_plate}"
+ check: "%{transformed_yeast_plate[0]}"
  #warning: "You're going to make %{num} overnight suspension tubes"
 end
 
