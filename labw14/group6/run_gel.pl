30446b5dd579ada08adde83358c492fc6e01571e
pegahsz <pegahsz@uw.edu>
2014-02-16 12:36:52 -0800

    Update run_gel.pl

--- labw14/group6/run_gel.pl
+++ labw14/group6/run_gel.pl
@@ -4,3 +4,15 @@ argument
   fragment_volume: number, "The volume of PCR fragment to load in µL."
 end
 
+step
+    description: "Testing step"
+end
+
+produce
+  s = 1 "Fragment Gel Lanes"
+
+end
+
+log
+  return: { Fragment_Gel_Lanes_id: s[:id]}
+end
