302dadb6267f0fb15dc57a3f8f7e7388947418e4
pegahsz <pegahsz@uw.edu>
2014-02-16 13:15:06 -0800

    Delete gibson_finish.pl

--- labw14/group6/full_length_protocols/gibson_finish.pl
+++
@@ -1,8 +0,0 @@
-step
-  description: "Pick up the tube you incubated on the 50 C heat block"
-  note: "Place in the M20 box slot specified in next page."
-end
-
-produce
-  r = 1 "Gibson Reaction Result"
-end
