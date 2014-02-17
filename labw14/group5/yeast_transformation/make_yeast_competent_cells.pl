e93d9bc1d8b9a99025daec889a165c138f2ae35a
alexbaryshev <baryshev@uw.edu>
2014-02-16 16:31:45 -0800

    Update make_yeast_competent_cells.pl

--- labw14/group5/yeast_transformation/make_yeast_competent_cells.pl
+++ labw14/group5/yeast_transformation/make_yeast_competent_cells.pl
@@ -6,6 +6,14 @@ num = length(yeast_250ml_flask)
 
 step
   description: "This protocol describes how to make yeast competent cell aliquot"
+  warning: "It is possible to make up to 6 aliquots out of one yeast suspension culture flask. 
+        You will be asked on your desired number of aliquots later."
+end
+
+take
+  description: "Get the following items"
+  falcon_tube = num "50 mL Falcon Tube"
+  flask = item yeast_250ml_flask
 end
 
 ii = 0
@@ -13,19 +21,14 @@ r = []
 
 while ii < length(yeast_250ml_flask)
 
-  step
-    description: "You can make up to 6 aliquot tubes out of one 25ml yeast flask"
-    getdata
-      aliquot_number: number, "Desired number of aliquot tubes to make out of one 25ml flask"
-    end
-  end
+#  step
+#    description: "You can make up to 6 aliquot tubes out of one 25ml yeast flask"
+#    getdata
+#      aliquot_number: number, "Desired number of aliquot tubes to make out of one 25ml flask"
+#    end
+#  end
 
-  take
-    description: "Get the following items"
-    falcon_tube = num "50 mL Falcon Tube"
-    flask = item yeast_250ml_flask
-  end
-  
+
   step
     description: "Harvest cells by centrifugation"
   end
