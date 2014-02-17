ef93264f4025a832ebce394fcd8a68e6ad6740f2
Nathanrollins <nrollins@uw.edu>
2014-02-16 17:53:13 -0800

    Update run_gel.pl

--- labw14/group6/run_gel.pl
+++ labw14/group6/run_gel.pl
@@ -1,5 +1,6 @@
 argument
-  assemblies: sample array, " Select the PCR Results."
+  assemblies1: sample array, " Select the first PCR Results."
+  assemblies2: sample array, " Select the second PCR Results."
   ladder_one: sample
   fragment_volume: number, "The volume of PCR fragment to load in µL."
 end
