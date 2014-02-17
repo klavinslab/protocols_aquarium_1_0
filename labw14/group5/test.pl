48bcb222c74e6f3d76a6c7b5d958fefe69726a1f
alexbaryshev <baryshev@uw.edu>
2014-02-17 15:57:38 -0800

    Update test.pl

--- labw14/group5/test.pl
+++ labw14/group5/test.pl
@@ -1,31 +1,8 @@
-vol_data = []
+m = 51
 
-n  = 1
-ii = 0
-
-while ii < n
-  step
-    description: "Type number"
-    getdata
-      num: number, "Number"
-    end
-  end
-  
-  vol_data = append(vol_data,num)
-
-  nu = vol_data[0]
-
-  step
-    description: "Number: %{nu}"
-  end
-  
-  ii = ii + 1
-end
-
-nu = vol_data[0]
+n = 50
 
 step
-  description: "Final values"
-  note: "%{vol_data}"
-  note: "%{nu}"
+  description: "%{57*4%50}"
 end
+
