f2361cc9c95c949497b3149388817ffd1b8c8c6f
alexbaryshev <baryshev@uw.edu>
2014-02-18 12:27:48 -0800

    Update pmei_digest.pl

--- labw14/group5/yeast_transformation/pmei_digest.pl
+++ labw14/group5/yeast_transformation/pmei_digest.pl
@@ -1,12 +1,15 @@
-#argument
-#   fragment: sample, "The purified fragment that needs to be digested"
-#end
+argument
+   fragment_id: sample, "The purified fragment that needs to be digested"
+   neb_id: sample, "NEB 4 buffer"
+   bsa_id: sample, "BSA buffer"
+   pmei_id: sample, "PmeI enzyme"
+end
 
 take
 #  y = item fragment
-   neb4     = item 668 # 1 "NEB Buffer 4"
-   bsa      = item 798 # BSA Buffer
-   fragment = item 23942 #1543 # DNA plasmid to digest
+   neb4     = item neb_id
+   bsa      = item bsa_id
+   fragment = item fragment_id
 end
 
 step
@@ -17,17 +20,17 @@ end
 take
    ice_block = 1 "Styrofoam Ice Block"
    alum_rack = 1 "Aluminum Tube Rack"
-   pmei      = item 687 # PmeI Enzyme
+   pmei      = item pmei_id
 end
 
 step 
   description: "Prepare PmeI digestion reaction"
   check: "Take a 1.5mL tube. Write your name and today's date on the tube's side."
   check: "Pipet 42.3 µL molecular grade water into the 1.5mL tube."
-  check: "Pipet 5 µL NEB4 buffer with id %{neb4} into the tube."
-  check: "Pipet 0.5 µL BSA buffer with id %{bsa} into the tube."
-  check: "Pipet 1.0 µL DNA plasmid stock with id %{fragment} into the tube."
-  check: "Pipet 0.3 µL PmeI enzyme with id %{pmei} into the tube."
+  check: "Pipet 5 µL NEB4 buffer with id %{neb_id} into the tube."
+  check: "Pipet 0.5 µL BSA buffer with id %{bsa_id} into the tube."
+  check: "Pipet 1.0 µL DNA plasmid stock with id %{fragment_id} into the tube."
+  check: "Pipet 0.3 µL PmeI enzyme with id %{pmei_id} into the tube."
   note: "Be careful to pipet into the liquid, not the side of the tube."
 end
 
@@ -47,4 +50,5 @@ end
 #  inuse:0
 #end
 
-release [neb4[0],bsa[0],pmei[0], ice_block[0], alum_rack[0], fragment[0]]
+release [neb4[0],bsa[0],pmei[0]]
+release [ice_block[0], alum_rack[0], fragment[0]]
