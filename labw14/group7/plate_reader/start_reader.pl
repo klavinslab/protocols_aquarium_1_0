73f6e372a1e5c6a97b0ab16ceacfffc435cea077
Erik909 <erik909@uw.edu>
2014-02-18 13:14:54 -0800

    Create start_reader.pl

---
+++ labw14/group7/plate_reader/start_reader.pl
@@ -0,0 +1,19 @@
+step
+  description: "Wipe down the plate lid with defogger solution"
+  note: "This prevents condensation from disrupting the measurements"
+  check: "Use a Kimwipe to wipe the inside of the lid"
+  #TODO: take/return defog solution, or at least give location
+end
+
+step
+  description: "Seal the plate"
+  note: "A small foil shim is needed to keep the lid from shaking"
+  #TODO: include picture
+  check: "Seal the plate"
+end
+
+step
+  description: "Insert the plate and start the software"
+  check: "Start the software"
+  #TODO: add details on start sequence, template, etc.
+end
