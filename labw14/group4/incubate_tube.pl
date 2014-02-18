2a9ac3b3c2217996fb1323761e0da1cbd49449f8
vchavali <vchavali@users.noreply.github.com>
2014-02-18 12:36:28 -0800

    Create incubate_tube.pl

---
+++ labw14/group4/incubate_tube.pl
@@ -0,0 +1,27 @@
+# Modified from existing protocol
+argument
+  e_coli_strain_id: sample, "The tube to be incubated."
+  temp: number, "The temperature (in °C) at which to incubate the tube."
+end
+
+take
+  y = item e_coli_strain_id
+end
+
+step
+  description: "Put the tubes in a small green tube holder."
+        image: "green_tube_holder"
+end
+
+step
+  description: "Place in the %{temp}°C incubator, location is A1.110."
+        #image: "put_green_tube_holder_to_incubator"
+end
+
+foreach y_i in y
+	modify
+	  y_i
+	  location:"A1.110"
+	  inuse:0
+	end
+end
