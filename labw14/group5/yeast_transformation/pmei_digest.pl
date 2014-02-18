48c4922230f9b61dbf4a56e53c2f901da369c900
alexbaryshev <baryshev@uw.edu>
2014-02-18 12:00:04 -0800

    Update pmei_digest.pl

--- labw14/group5/yeast_transformation/pmei_digest.pl
+++ labw14/group5/yeast_transformation/pmei_digest.pl
@@ -1,17 +1,12 @@
 #argument
 #   fragment: sample, "The purified fragment that needs to be digested"
-#  neb: sample, "Choose NEB4 buffer"
-#  bsa: sample, "Choose BSA buffer"
-#  dpn: sample, "Choose DpnI enzyme"
 #end
 
 take
 #  y = item fragment
-#  neb4 = item neb
-#  bsa_buffer = item bsa
-   neb4 = item 668  #"NEB Buffer 4"
-   bsa  = 1 "BSA"
-#   pmei =  "Enzyme: PmeI"
+   neb4     = item 668 # NEB Buffer 4 
+   bsa      = item 798 # BSA Buffer
+   fragment = item 1543 # DNA plasmid to digest
 end
 
 step
@@ -22,14 +17,16 @@ end
 take
    ice_block = 1 "Styrofoam Ice Block"
    alum_rack = 1 "Aluminum Tube Rack"
-   pmei      = 1 "PmeI"
+   pmei      = item 687 # PmeI Enzyme
 end
 
 step 
   description: "Prepare PmeI digestion reaction"
-#  check: "Pipet 42.3 µL molecular grade water into fragment tube with id %{fragment}."
+  check "Take a 1.5mL tube. Write your name and today's date on the tube's side."
+  check: "Pipet 42.3 µL molecular grade water into the 1.5mL tube."
   check: "Pipet 5 µL NEB4 buffer with id %{neb4} into the tube."
   check: "Pipet 0.5 µL BSA buffer with id %{bsa} into the tube."
+  check: "Pipet 1.0 µL DNA plasmid stock with id %{fragment} into the tube."
   check: "Pipet 0.3 µL PmeI enzyme with id %{pmei} into the tube."
   note: "Be careful to pipet into the liquid, not the side of the tube."
 end
