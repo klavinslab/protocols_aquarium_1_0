a5b2a151d3ce0f9d203392d96d9cfe90944e3ddb
Erik909 <erik909@uw.edu>
2014-02-17 17:32:02 -0800

    Update load_plate.pl

--- labw14/group7/plate_reader/load_plate.pl
+++ labw14/group7/plate_reader/load_plate.pl
@@ -1,12 +1,12 @@
 argument
-  glucose_media
-  ce
+  #glucose_media?
+  cells_to_assay_in: sample array, "The strains to assay (4 copies of each strain)"
 end
 
 take
-  glucose_media: 
-  MG_water:
-  cells
+  DI_water: 1 "DI Water, Sterile"
+  cells: cells_to_assay_in
+  #glucose media
 end
 
 step
@@ -16,27 +16,29 @@ end
 step
   description: "Load the outermost edges of the plate with water"
   note: "This helps prevent evaporation of the samples, which will be in the middle of the plate"
-  check: "pipette 200uL of DI water into the outer edges"
+  check: "Pipette 200uL of DI water into the outer edges"
   #todo: include a picture of this
 end
 
 step
   description: "Load the middle area of the plate with the glucose medium"
-  check: "pipette 200uL of glucose media into each open well"
-  #todo: generalize this step
+  check: "Pipette 200uL of glucose media (prepared earlier) into each open well"
+  #TODO: include a picture
 end
 
+cell0 = cells[0]
+cell1 = cells[1]
+cell2 = cells[2]
 step
   description: "Load the cells into the wells"
   check: "Load the cells according to the chart"
+  note: "%{cell0}: R1 => A; R2 => B; R3 => C; R4 => D"
+  note: "%{cell1}: R1 => E; R2 => F; R3 => G; R4 => H"
+  note: "%{cell2}: R1 => I; R2 => J; R3 => K; R4 => L"
   #todo: include chart
   #todo: identify which cells correspond to which type.  Generalize.
 end
 
-produce
- a = 1 "loaded plate" from cells[]
- Location: "Bench"
-end
 
 #return statement
 
