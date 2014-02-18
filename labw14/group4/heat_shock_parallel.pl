7bd6bfa588fcdcde4780b4740f952a432227e4c0
PatrickMa <patrick.is@not.here>
2014-02-17 23:06:32 -0800

    working parallel version + updated variables
    heat_shock_parallel.pl can run n samples at once!
    ASSUMES THAT THE HEAT BATH IS INFINITE SIZE! this needs to be
    updated to automatically instruct the user to run batches.

--- labw14/group4/heat_shock_parallel.pl
+++ labw14/group4/heat_shock_parallel.pl
@@ -1,4 +1,8 @@
-information "Heat shock temperature sensitive cells."
+# Author: Patrick Ma
+# 2/17/2014
+#
+# A modified version of the heat_shock protocol for making multiple samples of
+# electrocompetent cells simultaneously
 
 # Precondition: The input array has no duplicates 
 argument
@@ -7,26 +11,25 @@ end
 
 num_samples = length(cells_flask_array)
 
-# I need a more efficient way of doing this. CAn we dynamically access and populate a take (or any other block?
-	take
-foreach sample in cells_flask_array
-		cells = item sample
-	#  tube = 1 "50 mL Falcon Tube"
-	end
+information "Heat shock temperature sensitive cells. You will perform this protocol on %{num_samples} samples"
 
-	cells = cells[0]
+take
+	taken_cells = item cells_flask_array
 end
 
 step
-  description: "Label %{num_samples} 50 mL Falcon tube(s)"
-  note: "Write your initials and date on the cap of each tube."
+  description: "Label (%{num_samples}) 50 mL Falcon tube(s)"
+  note: "Write your initials, date, and sample number on the cap of each tube."
 end
 
 step
-  description: "Transfer the cells to the labeled 50 mL Falcon Tube by pouring"
-  note: "Pour the log phase cells from the shaker flask into the 50 mL tube. The volume should be approximately 18 mL."
+  description: "Transfer each sample of log phase cells into a 50 mL Falcon Tube"
+  note: "Pour the entire volume of the shaker flask into the 50 mL tube. The volume should be approximately 18 mL."
 end
 
+# NOTE: must break up the cells into batches that will fit inside the heat bath
+
+bath_size = 4	# the number of falcon tubes that will fit in the bath
 step
   description: "Locate the 42 C heat bath at B13.105"
   note: "The next steps are time sensitive so you must know beforehand where the bath is."
@@ -60,13 +63,31 @@ step
   timer: { hours: 0, minutes: 8, seconds: 0 }
 end
 
-produce
-  hot_cells = 1 "Heat shocked tube" from cells
-  note: "Make sure to keep the cells in the ice bath for the next step."
-  location: "Bench"
-  release cells
+#
+# Produce each new heat treated sample
+ii = 0
+heat_treated_cells = []
+
+while ii < num_samples
+	id = taken_cells[ii][:id]
+
+	produce
+  	hot_cells = 1 "Heat shocked tube" from taken_cells[ii]
+
+		note: "This new sample is produced from Sample %{id}. Keep these cells on ice for the next step"
+		data
+			from: id	# for sample tracking purposes
+		end
+  	location: "Bench"
+		release taken_cells[ii]
+	end
+	heat_treated_cells = append(heat_treated_cells, hot_cells[:id])
+	ii = ii + 1
 end
 
+
+# output the id numbers of the treated cells for the next protocol
 log
-  return: { cell_id: hot_cells[:id]}
+  shocked_cells : heat_treated_cells 
 end
+
