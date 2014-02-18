0390ac47808a2cb7a9bfbf42233f0f8af350389d
vchavali <vchavali@users.noreply.github.com>
2014-02-18 12:32:10 -0800

    Create finish_log_phase.pl

---
+++ labw14/group4/finish_log_phase.pl
@@ -0,0 +1,57 @@
+##
+## Adapted from existing protocol
+##
+ 
+argument
+  log_cells: sample array, "The sample that you want to spec its concentration"
+end
+
+OD_tol = ##
+
+take
+  y = item log_cells
+  lb = 1 "50 mL LB liquid aliquot (sterile)"
+end
+
+information "Measure DNA concentration using Nanodrop spectrophotometer."
+
+step
+  description: "Take the cells and lb tothe Nanodrop at location B3.405"
+end
+
+ODs = []
+foreach c in log_cells
+	step
+	  description: "Blank the Nanodrop"
+	  check: "Wipe both lower (sensor) pedestal and the upper (lid) pedestal with Kimwipe."
+	  check: "Add 2 μL of LB Buffer." # Should we blank with LB or EB?
+	  check: "Close the pedestal."
+	  check: "Click Blank in the software."
+	end
+
+	## What settings should we set the nanodrop to?
+
+	step
+	  description: "Measure your sample"
+	  check: "Open the pedestal"
+	  check: "Wipe both lower (sensor) pedestal and the upper (lid) pedestal with Kimwipe."
+	  check: "Add 2 μL of your sample with id %{c} on the lower pedestal."
+	  check: "Close the pedestal."
+	  check: "Click Measure in the software."
+	end
+
+	step
+	  description: "Record the optical density measurement (OD) shown on the nanodrop computer"
+	  getdata
+		OD: number,"Write down the OD as shown on the computer" #Better disc. of where to find it?
+	  end
+	end
+	ODs = append(ODs, OD)
+	step
+	  description: "Clean the Nanodrop"
+	  check: "Open the pedestal"
+	  check: "Wipe both lower (sensor) pedestal and the upper (lid) pedestal with Kimwipe."
+	end
+end
+release y
+log { return {ODs: ODs} }
