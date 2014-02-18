3550e623ed34c0574756de6e722059329bc4d0d5
pegahsz <pegahsz@uw.edu>
2014-02-18 12:17:45 -0800

    Update run_gel.pl

--- labw14/group6/run_gel.pl
+++ labw14/group6/run_gel.pl
@@ -1,6 +1,6 @@
 argument
-  assemblies1: sample array, " Select the first PCR Results."
-  assemblies2: sample array, " Select the second PCR Results."
+  assembly1: sample, " Select the first PCR Result."
+  assembly2: sample, " Select the second PCR Result."
   ladder_one: sample
   fragment_volume: number, "The volume of PCR fragment to load in µL."
 end
