cbbb4b93c25f156c605833a009b38b322fc973a0
vchavali <vchavali@users.noreply.github.com>
2014-02-18 17:07:07 -0800

    Update pcr_prepare.pl

--- labw14/group4/pcr_prepare.pl
+++ labw14/group4/pcr_prepare.pl
@@ -1,60 +1,84 @@
 argument
-  primer_f: sample array, "forward primer list"
-  primer_r: sample array, "reverse primer list"
-  plasmid_id: sample, "The plasmid stock"
+  pfwd: sample("Primer"), "The forward primer"
+  prev1: sample, "Flanking reverse primer for knockout test group"
+  prev2: sample, "thyA reverse primer for control group"
+  #templates: sample array, "The templates you just made" #just assume you have it
   enzyme_id: sample, "The Phsion HF Master Mix stock"
+  sample_count: int, "Number of boiled cell samples"
 end
 
-num_samples = length(primer_f)
-
-
 take
-  plasmid_stock = item plasmid_id
+  fwd = item pfwd
+  rev1 = item prev1
+  rev2 = item prev2
+  #genomicdna = item templates
   phusion_stock = item enzyme_id
 end
 
+pcr_count = sample_count*2
+step
+  description: "Take %{pcr_count} 0.2 mL PCR tubes from PCR strip. Write your initials on it. Label 1-%{pcr_count} from left to right."
+end
 
-primer_f_items = []
-primer_r_items = []
+step
+  description: "Prepare reaction"
+  check: "Pipet 5 µL molecular grade water into each newly labeled PCR tube."
+end
 
-ii=0
-while ii<length(primer_f)
-  take
-    p1 = item primer_f[ii]
-    p2 = item primer_r[ii]
+step
+  description: "Spin down the boiled cell 1-%{sample_count} in the microcentrifuge on your bench for 1 minute"
+  note: "The boiled cell 1-%{sample_count} are the PCR tubes you took from thermal cylcler from your previous protocol."
+  warning: "Be extremely careful not to distrube the spun tubes.  They won't look any different but even the slightest tap can be detrimental."
+end
+
+boiled_tube = 0
+pcr_tube = 0
+while boiled_tube < sample_count
+  boiled_tube = boiled_tube + 1
+  pcr_tube = pcr_tube+1
+  
+  step 
+    description: "Prepare reaction for tube %{pcr_tube} -- boiled cell %{boiled_tube} knockout test group"
+    check: "Pipet 3 µL of boiled cell %{boiled_tube} into tube %{pcr_tube}."
+    check: "Pipet 1 µL of primer with id %{pfwd} into tube %{pcr_tube}."
+    check: "Pipet 1 µL of primer with id %{prev1} into tube %{pcr_tube}."
+    check: "Use the tip to gently mix."
+    note: "Be careful to pipette into the liquid, not the side of the tube. Always use a new tip."
+    warning: "Be extremely careful not to distrube the boiled cell tubes. They won't look any different but even the slightest tap can be detrimental."
   end
-  primer_f_items = append(primer_f_items,p1[0])
-  primer_r_items = append(primer_r_items,p2[0])
-  ii = ii+1
-end
-
-
-step
-  description: "This protocol prepares the PCR mix in
-    PCR tube and starts it in thermal cycler."
-end
-
-step
-  description: "Put everything in each tube. "
-  check: "Pipet 7.5 µL molecular grade water into the labeled PCR tube."
-  check: "Pipet 0.5 µL of plasmid into the tube."
-  check: "Pipet 1 µL of primer fwd into the tube."
-  check: "Pipet 1 µL of primer rev into the tube."
-  check: "Pipet 10 µL of Phusion Master Mix with id %{enzyme_id} into the tube."
+  
+  pcr_tube = pcr_tube+1
+  step 
+    description: "Prepare reaction for tube %{pcr_tube} -- boiled cell %{boiled_tube} control group"
+    check: "Pipet 3 µL of boiled cell %{boiled_tube} into tube %{pcr_tube}."
+    check: "Pipet 1 µL of primer with id %{pfwd} into tube %{pcr_tube}."
+    check: "Pipet 1 µL of primer with id %{prev2} into tube %{pcr_tube}."
+    check: "Use the tip to gently mix."
+    note: "Be careful to pipette into the liquid, not the side of the tube. Always use a new tip."
+    warning: "Be extremely careful not to distrube the boiled cell tubes. They won't look any different but even the slightest tap can be detrimental."
+  end
+  
 end
 
 step
-  description: "Place the tube into thermal cycler T2 at B3.335"
+  description: "Prepare reaction"
+  check: "Pipet 10 µL of Phusion HF Master Mix with id %{enzyme_id} into each PCR tube."
+  note: "Use the tip to gently mix after each pipette."
+end
+
+release phusion_stock
+
+step
+  description: "Place the tube strip into thermal cycler T2 at B3.335"
 end
 
 step
-  description: "Choose program on the thermal cycler and run Tm=63"
-  note: ""
+  description: "Choose program on the thermal cycler"
+  note: "Click Home then click Saved Protocol, choose LABW14 folder, choose COLONYPCR. Hit 'run' to start"
   image: "thermal_cycler_home"
 end
 
-release primer_f_items
-release primer_r_items
-release [plasmid_stock[0],phusion_stock[0]]
+release fwd
+release revs
 
 
