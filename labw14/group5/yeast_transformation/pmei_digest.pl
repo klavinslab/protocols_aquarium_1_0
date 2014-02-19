7de08143a31f306b7a8fd8b98270388915539257
alexbaryshev <baryshev@uw.edu>
2014-02-19 11:14:49 -0800

    Update pmei_digest.pl

--- labw14/group5/yeast_transformation/pmei_digest.pl
+++ labw14/group5/yeast_transformation/pmei_digest.pl
@@ -1,3 +1,5 @@
+information "This protocol describes how to digest your plasmid with PmeI enzyme."
+
 argument
    fragment_id: sample, "The purified fragment that needs to be digested"
    neb_id: sample, "NEB 4 buffer"
@@ -13,7 +15,7 @@ take
 end
 
 step
-  description: "Use a -20 C blue ice block tube holder to grab the enzyme"
+  description: "In the next step you will use a -20 C blue ice block tube holder to grab the enzyme"
   note: "The enzyme are temperature sensitive."
 end
 
@@ -26,11 +28,11 @@ end
 step 
   description: "Prepare PmeI digestion reaction"
   check: "Take a 1.5mL tube. Write your name and today's date on the tube's side."
-  check: "Pipet 42.3 µL molecular grade water into the 1.5mL tube."
-  check: "Pipet 5 µL NEB4 buffer with id %{neb_id} into the tube."
-  check: "Pipet 0.5 µL BSA buffer with id %{bsa_id} into the tube."
-  check: "Pipet 1.0 µL DNA plasmid stock with id %{fragment_id} into the tube."
-  check: "Pipet 0.3 µL PmeI enzyme with id %{pmei_id} into the tube."
+  check: "Pipet 42.3 µL of molecular grade water into the 1.5mL tube."
+  check: "Pipet 5 µL of NEB4 buffer with id %{neb_id} into the tube."
+  check: "Pipet 0.5 µL of BSA buffer with id %{bsa_id} into the tube."
+  check: "Pipet 1.0 µL of DNA plasmid stock with id %{fragment_id} into the tube."
+  check: "Pipet 0.3 µL of PmeI enzyme with id %{pmei_id} into the tube."
   note: "Be careful to pipet into the liquid, not the side of the tube."
 end
 
