f821408702d5bf4d6cab015b7aab8ae4dacbf465
hamsanchez <hamilton.sanch@gmail.com>
2014-02-19 14:50:49 -0800

    Update microscope_preparation.pl

--- labw14/group2/microscope_preparation.pl
+++ labw14/group2/microscope_preparation.pl
@@ -58,6 +58,7 @@ end
 step
   description: "Turn on the microscope light" 
   note: "This is labeled 5 and produces the light from the mechanism above the incubator"
+  note: "The button is on the left side"
   image: "micrcope light label 5"
 end
 
