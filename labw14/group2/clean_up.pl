d69e8ab278bedc457c4f21ef7a508f8e5991de4d
hamsanchez <hamilton.sanch@gmail.com>
2014-02-19 12:56:00 -0800

    Update clean_up.pl

--- labw14/group2/clean_up.pl
+++ labw14/group2/clean_up.pl
@@ -9,24 +9,35 @@ end
 
 step
   description: "Take out agar plate"
-  bullet: "Turn the big knob on the bottom left of the microscope in a clockwise direction."
-  bullet: "Open the micrscope incubator and loosen the screws." 
+  bullet: "Turn the big knob on the bottom right of the microscope in a clockwise direction."
+  bullet: "Open the micrscope incubator and loosen the screws."
   bullet: "Carefully take out the agar plate along with the ring."
   bullet: "Clean the lens of the microscope using an Olympus lens cleaner and shut the incubator"
   bullet: "Take the ring out and discard the agar plate" 
 end
 
 step
+  description: "Dismantle the ring"
+  bullet: "Take apart the dish from the ring"
+  bullet: "Return ring to microscope base"
+
+step
   description: "Turn off micrscope"
-  bullet: "Turn off everything labeled 2 - 6"
+  bullet: "Turn off everything labeled 7 - 2 in that order"
+  bullet: "Turn off the incubator labeled 0"
   bullet: "Turn off the fluorescent light bulb (labeled 1)"
 end
 
 step
   description: "Save your movie"
-  bullet: "Go to the computer and click through your images to check you results"
+  bullet: "On the image screen click on the blue bar near the bottom labeled M"
+  bullet: "These are your individual snap shots of each different cell"
+  bullet: "On the top bar labled T move your mouse to the right and press the play button to cycle through the time images"
+  bullet: "Find a cell divided properly and one that shows oscillations"
   bullet: "Go to File -> Save As, and then save your movie as 'Repressilator_your-name_date' in D:/LABW14"
   warning: "Make sure to not click anywhere else on the screen while the computer is saving the file."
+  warning: "Make sure the cell you want saved is selected on the M bar"
 end
 
+release [id[0]]
 
