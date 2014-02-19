ccc8e9351754007c1818510ae17e04a17e6fd015
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-02-19 13:54:52 -0800

    Update test_function.pl

--- labw14/test/test_function.pl
+++ labw14/test/test_function.pl
@@ -1,3 +1,26 @@
+argument
+  tube_id: sample, "The small tube to be incubated."
+#  e_coli_strain_id_2: sample, "The second plate to be incubated"
+  temp: number, "The temperature (in °C) at which to incubate the plate."
+end
+
+take
+  y = item tube_id
+end
+
+function incubate_small_tube(tube_id, temp)
+  step
+    description: "Put the tube with id %{tube_id} in a small green tube holder."
+	  image: "green_tube_holder"
+  end
+  step
+    description: "Place in the %{temp}°C incubator, location is B14.310."
+	  image: "put_green_tube_holder_to_incubator"
+  end
+end
+
+
+
 function cleanup(msg1, msg2)
   step
     description: "Clean up your workstation"
