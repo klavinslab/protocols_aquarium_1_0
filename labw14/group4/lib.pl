e7cacb686a3ac7308976ed574179aca634b3216e
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-02-19 14:27:19 -0800

    Update lib.pl

--- labw14/group4/lib.pl
+++ labw14/group4/lib.pl
@@ -1,6 +1,6 @@
 # library of functions
 
-# takes in an original sample id and a sample array and returns the id of the first descendent sample
+# takes in an original sample id and a sample array of hashes and returns the first descendent sample as a hash
 function find_original( o_id, sample_array)
   foreach elem in sample_array
     local orig_id = elem[:original_id]
