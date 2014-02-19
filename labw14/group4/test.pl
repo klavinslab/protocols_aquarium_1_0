8b82ef0ec6911d62718ccd3da5fc988d82de8da2
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-02-19 13:23:53 -0800

    Update test.pl

--- labw14/group4/test.pl
+++ labw14/group4/test.pl
@@ -1,17 +1,8 @@
 # test bed for trying ideas
 
-# can dynamically extend hashs?
-myhash = {x: "hi"}
-x = myhash[:x]
-step 
-  description:" lets test a hash"
-  note: "the has is : %{x}"
-end
-  
-#myhash[y] = "bye"
-
-step 
-  description:" lets test a hash"
-  note: "the hash is : %{myhash}"
+A = [ 1, 2, 3 ]
+foreach a in A
+  step
+    description: "In the loop, a = %{a}"
   end
-  
+end
