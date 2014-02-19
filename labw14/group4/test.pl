7ced0dc5800bdfcf5582bbea9b527436d5515ef3
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-02-19 14:10:02 -0800

    Update test.pl

--- labw14/group4/test.pl
+++ labw14/group4/test.pl
@@ -5,3 +5,9 @@ a = [{id: 1, original_id: "a"}, {id: 2, orginal_id: "b"}, {id: 3, original_id: "
 step
   description: "%{a}"
 end
+
+s = find_original("a", a)
+
+step
+  description: "sample %{s} came from the original sample "a""
+end
