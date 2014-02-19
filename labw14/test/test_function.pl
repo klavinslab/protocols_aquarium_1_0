f9f532b2aee2cd7e98fbb3abfd55b242acc59604
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-02-19 13:37:11 -0800

    Update test_function.pl

--- labw14/test/test_function.pl
+++ labw14/test/test_function.pl
@@ -1,8 +1,12 @@
-function cleanup(msg)
+function cleanup(msg1, msg2)
   step
     description: "Clean up your workstation"
     note: "%{msg}"
   end
+  step
+    description: "Replace empty tip boxes with new tip boxes."
+    note: "%{msg2}"
+  end
 end
 
-cleanup("Leave it cleaner than you found it")
+cleanup("Leave it cleaner than you found it", "Put empty boxes on bench where it says empty boxes")
