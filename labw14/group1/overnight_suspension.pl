a4feecea4c4a8e18cb2b45edf3fce5e26026e19e
pollardt <pollardt@uw.edu>
2014-02-18 17:36:35 -0800

    Update overnight_suspension.pl

--- labw14/group1/overnight_suspension.pl
+++ labw14/group1/overnight_suspension.pl
@@ -9,7 +9,7 @@ end
 take
   falcon_tube = 1 "50 mL LB liquid aliquot (sterile)"
   test_tube = 1 "14 mL Test Tube"
-  plate = 1 Transformed_E_coli_Strain_id
+  plate = sample Transformed_E_coli_Strain_id
 end
 
 step
