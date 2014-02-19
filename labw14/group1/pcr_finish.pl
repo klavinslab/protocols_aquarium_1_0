cbe8173a882016a45cc5befeae5b4ff3503acd6a
lavania <chandrashekharla@gmail.com>
2014-02-18 16:05:55 -0800

    Create pcr_finish.pl
    Created Initial Version

---
+++ labw14/group1/pcr_finish.pl
@@ -0,0 +1,18 @@
+argument
+  names: string array, "list of PCR product names"
+end
+
+step
+  description: "Go to the thermal cycler to pick up the PCR tubes you put in the previous pcr_prepare protocol."
+  note: "Put them on you bench after you picked up the PCR tube"
+end
+
+ii= 0
+while ii<length(names)
+  produce
+    1 "PCR Result" of names[ii]
+    note: "Keep the tube on the bench to use in the next protocol.  (no need to edit the location below)"
+    location: "Bench"
+  end
+  ii = ii+1
+end
