37249f8617579abf015eaeb0a06c7f4f225c4005
alexbaryshev <baryshev@uw.edu>
2014-02-19 11:39:54 -0800

    Update make_yeast_competent_cells.pl

--- labw14/group5/yeast_transformation/make_yeast_competent_cells.pl
+++ labw14/group5/yeast_transformation/make_yeast_competent_cells.pl
@@ -5,7 +5,7 @@ end
 num = length(yeast_250ml_flask)
 
 step
-  description: "This protocol describes how to make yeast competent cell aliquot"
+  description: "This protocol describes how to make yeast competent cell aliquots"
   warning: "It is possible to make up to 6 aliquots out of one yeast suspension culture flask. 
         You will be asked on your desired number of aliquots later."
 end
@@ -15,44 +15,87 @@ take
   flask = item yeast_250ml_flask
 end
 
+#   IF   STATEMENT
 
-step
-  description: "Harvest the cells by centrifugation"
-  check: "Label 50mL falcon tubes with the id number of the 250mL flasks, so that each tube has
-          the same number as one the flasks."
-  check: "Transfer 25mL of the diluted yeast culture from each flask to the 50mL falcon tube with the same id number."
+if num == 1
+  step
+    description: "Harvesting the cells by centrifugation"
+    check: "Label a 50 mL falcon tube with the id number of the 250mL flasks."
+    check: "Pour 25 mL of the diluted yeast culture from the flask to the labeled 50mL falcon tube."
+  end
+
+  step
+    description: "Centfifuging the tube"
+    check: "Centfifuge the labeled 50mL tube with a yeast liquid culture for 5 min at 3000 x g
+            in the table top centrifuge located at B3.355 (or B1.340???)."
+    warning: "Be sure you balanced the centrifuge. To balance the labeled tube you have to take
+              another 50 mL falcon tube and fill it with 25 mL of water."
+  end
+
+  step
+    description: "Washing the cells"
+    check: "Collect the labeled 50 mL tube from the centrifuge and place it in a tube rack on your bench."
+    check: "Pour out the liquid from the tube into the sink at the washing station."
+    warning: "Be careful. Don't destroy the pellet."
+  end
+
+  step
+    description: "Washing the cells. Continuation"
+    check: "Take a 1.5 mL tube. Label it with the id number of the labeled 50mL falcon tube."
+    check: "Add 1 mL of molecular grade water into the labeled 50 mL falcon tube and resuspend the cells by vortexing.
+            Transfer the resuspended cells (~1mL) from the 50 mL tube to the labeled 1.5 mL tube."
+  end
+
+  step
+    description: "Washing the cells. Continuation"
+    check: "Centrifuge the labeled 1.5mL tube for 1 min in a mini-centrifuge located on your bench."
+    check: "Using P1000 pipettor carefully remove the supernatant from the tube. Do not destroy the pellet."
+    check: "Add 1 mL of the 100 mM LiOAc liquid to the 1.5 mL tube and resuspend the cells by vortexing."
+    check: "Centrifuge the 1.5 mL tube for 1 min in a mini-centrifuge located on your bench."
+    check: "Using P1000 pipettor carefully remove the supernatant from the tube. Do not destroy the pellet."
+  end
+else
+  step
+    description: "Harvesting the cells by centrifugation"
+    check: "Label 50 mL falcon tubes with the id number of the 250 mL flasks, so that each tube has
+            the same number as one the flasks."
+    check: "Pour 25 mL of the diluted yeast culture from each flask to the 50 mL falcon tube with the same id number."
+  end
+
+  step
+    description: "Centfifuging the tubes"
+    check: "Centfifuge all the 50 mL tubes with a yeast liquid culture for 5 min at 3000 x g
+            in the table top centrifuge located at B3.355 (or B1.340???)."
+    warning: "Be sure you balanced the centrifuge."
+  end
+
+  step
+    description: "Washing the cells"
+    check: "Collect all the 50 mL tubes from the centrifuge and place them in a tube rack on your bench."
+    check: "Pour out the liquid from the tubes into the sink at the washing station."
+    warning: "Be careful. Don't destroy the pellet."
+  end
+
+  step
+    description: "Washing the cells. Continuation"
+    check: "Take %{num} 1.5 mL tubes. Label them with the id numbers of the 50 mL falcon tubes."
+    check: "Add 1 mL of molecular grade water into each 50 mL falcon tube and resuspend the cells by vortexing.
+            Transfer the resuspended cells (~1 mL) from the 50 mL tubes to the 1.5 mL tubes
+            with the corresponding id numbers."
+  end
+
+  step
+    description: "Washing the cells. Continuation"
+    check: "Centrifuge all the 1.5 mL tubes for 1 min in a mini-centrifuge located on your bench."
+    check: "Using P1000 pipettor carefully remove the supernatant from each tube. Do not destroy the pellet."
+    check: "Add 1 mL of the 100 mM LiOAc liquid to each 1.5 mL tube and resuspend the cells by vortexing."
+    check: "Centrifuge all the 1.5 mL tubes for 1 min in a mini-centrifuge located on your bench."
+    check: "Using P1000 pipettor carefully remove the supernatant from each tube. Do not destroy the pellet."
+  end
 end
 
-step
-  description: "Centfifuge the tubes"
-  check: "Centfifuge all the 50mL tubes with yeast liquid culture for 5 min at 3000 x g
-          in the table top centrifuge located at B3.355 (or B1.340???)."
-  warning: "Be sure you balanced the centrifuge."
-end
+#  THE  END   OF  THE   IF   STATEMENT
 
-step
-  description: "Wash the cells"
-  check: "Collect all the 50mL tubes from the centrifuge and place them in a tube rack on your bench."
-  check: "Pour out the liquid from the tubes into the liquid waste bottle placed on your bench."
-  warning: "Be careful. Don't destroy the pellet."
-end
-
-step
-  description: "Wash the cells. Continue"
-  check: "Take %{num} 1.5mL centrifuge tubes. Label them with the id numbers of the 50mL falcon tubes."
-  check: "Add 1mL of the molecular grade water into each 50mL falcon tube and resuspend the cells by pipetting
-          up and down. Transfer the resuspended cells (~1mL) from the 50mL tubes to the 1.5mL centrifuge tubes
-          with the corresponding id numbers."
-end
-
-step
-  description: "Wash the cells. Continue"
-  check: "Centrifuge all the 1.5mL tubes for 1 min in a mini-centrifuge located on your bench."
-  check: "Pour out the supernatant."
-  check: "Add 1mL of the 100mM LiOAc liquid to each 1.5mL tube and resuspend the cells by vortexing."
-  check: "Centrifuge all the 1.5mL tubes for 1 min in a mini-centrifuge located on your bench."
-  check: "Pour out the supernatant."
-end
 
 
 ii = 0
