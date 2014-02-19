90ff234b029dad0796d7d36ab4f119da90e3bb56
vchavali <vchavali@users.noreply.github.com>
2014-02-18 17:41:01 -0800

    Create image_gel.pl

---
+++ labw14/group4/image_gel.pl
@@ -0,0 +1,73 @@
+argument
+  gel_count: number, "Number of gels to image"
+end
+
+i = 0
+while i < gel_count
+  i = i+1
+  step
+    description: "Wipe down transilluminator"
+    note: "Spray with ethanol and wipe down with a kimwipe"
+  end
+  
+  step
+    description: "Take the gel (the one you just finished electrophoresis) out of gel box and put it on transilluminator"
+    note: "Carefully remove the holder from the gel box. The gel is slippery and may fall out unexpectedly.
+    Slide the gel off of the holder and onto the surface of the transilluminator"
+    image: "slide_gel"
+  end
+  
+  step
+    description: "Wear the UV protection glasses"
+    note: "The UV protection glasses are located above the bench in the gel station. 
+    Though the transilluminators we are using emit in the visible blue spectrum, these 
+    glasses protect your eyes from the bright lights and allows you to see the DNA band.
+    If you wear glasses, wear the glasses over your glasses."
+    image: "wear_uv"
+  end
+  
+  step
+    description: "Turn on the transilluminator, put on the camera hood"
+    note: "The on switch for the transilluminator is located on the side."
+  end
+  
+  step
+    description: "Turn on the camera"
+    note: "The camera is turned on by pressing the power button once."
+  end
+  
+  step
+    description: "Adjust camera settings"
+    note: "The camera settings must be properly adjusted for gel photography if they are not already."
+    bullet: "Turn the large dial to manual (M)"
+    bullet: "Switch the camera to macro mode by pressing the button with a flower and
+    rotating the menu wheel to the flower icon."
+    bullet: "Set the ISO to 100 by turning the larger dial"
+    bullet: "Set the white balance to tungsten by pressing the center button and navigating to tungsten."
+    bullet: "Set the shutter speed to 4 seconds by turning the menu wheel."
+    bullet: "Adjust the focus by manully tuning the focus"
+  end
+  
+  step
+    description: "Take a picture of the gel."
+    note: "Zoom in such that the gel takes up the entire field of view."
+    note: "Press the trigger half way to focus until you hear two beeps and all the way down to take the photo. Leave the camera on
+    as it takes time to transmit the photo to the computer wirelessly"
+  end
+  
+  step
+    description: "Clean up the transilluminator and gel station"
+    note: "Turn off the transilluminator. Dispose of the gel and any gel fragments by placing it in the waste container. 
+    Spray the surface of the transilluminator with ethanol and wipe until dry using kimwipes or paper towel. If the
+    lights in the room are turned off, turn the lights back on. Remove the UV goggles and put them back where you
+    found them."
+  end
+  
+  step
+    description: 
+      "Rename the picture in Dropbox"
+    note: 
+      "In gel room touch screen computer, open Dropbox/GelImages, under today's date folder and find the picture you just took.\n
+       Rename the picture as the labw14_week5_gel_[group-id]_%{i}, where group-id is replaced with a group number"
+  end
+end
