7d99b1efcc8bfde7fc785930a3c1dac7c78a3f88
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-02-19 14:05:05 -0800

    Update test_function.pl

--- labw14/test/test_function.pl
+++ labw14/test/test_function.pl
@@ -1,21 +1,24 @@
 argument
-  tube_id: sample, "The small tube to be incubated."
+  tube_array: sample array, "The small tube to be incubated."
 #  e_coli_strain_id_2: sample, "The second plate to be incubated"
   temp: number, "The temperature (in °C) at which to incubate the plate."
 end
 
 take
-  y = item tube_id
+  tube_array_sample = item tube_id
 end
 
-function incubate_small_tube(tube_id, temp)
-  step
-    description: "Put the tube with id %{tube_id} in a small green tube holder."
-	  image: "green_tube_holder"
-  end
-  step
-    description: "Place in the %{temp}°C incubator, location is B14.310."
-	  image: "put_green_tube_holder_to_incubator"
+function incubate_small_tube(tube_sample_array, temp)
+  foreach tube in tube_sample_array
+    tube_id = info(tube)[:id]
+    step
+      description: "Put the tube with id %{tube_id} in a small green tube holder."
+      image: "green_tube_holder"
+    end
+    step
+      description: "Place in the %{temp}°C incubator, location is B14.310."
+      image: "put_green_tube_holder_to_incubator"
+    end
   end
 end
 
