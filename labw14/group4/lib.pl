b1f0f0f444843bea1c4a5037cc3699e5b3515d70
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-02-19 14:33:41 -0800

    working find-original!

--- labw14/group4/lib.pl
+++ labw14/group4/lib.pl
@@ -3,7 +3,6 @@
 # takes in an original sample id and a sample array of hashes and returns the first descendent sample as a hash
 function find_original( o_id, sample_array)
   foreach elem in sample_array
-    local orig_id = elem[:original_id]
     if o_id == elem[:original_id]
       return elem
     end
