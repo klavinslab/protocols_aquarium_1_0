b4ae5b81517a277a1e2b073569578d0b740afb60
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-02-19 14:01:30 -0800

    Update functionlib.pl

--- labw14/group4/functionlib.pl
+++ labw14/group4/functionlib.pl
@@ -3,7 +3,7 @@
 # takes in an original sample id and a sample array and returns the id of the first descendent sample
 function find_original( o_id, sample_array)
   foreach elem in sample_array
-    local orig_id = elem{:data}{:original_id}
+    local orig_id = elem[:data][:original_id]
     if o_id == orig_id
       return elem
     end
