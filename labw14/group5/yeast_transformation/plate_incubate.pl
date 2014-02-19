b0d9c74b6e78b17811b075f44d9acf006a247a46
alexbaryshev <baryshev@uw.edu>
2014-02-18 20:17:42 -0800

    Update plate_incubate.pl

--- labw14/group5/yeast_transformation/plate_incubate.pl
+++ labw14/group5/yeast_transformation/plate_incubate.pl
@@ -1,4 +1,4 @@
-information "Spread cells onto a petri dish containing ~25mL agar media."
+information "You will spread transformed yeast cells onto a petri dish containing ~25mL YPAD media."
 
 argument
    yeast_strain_id: sample array, "Transformed yeast mixtures"
@@ -8,49 +8,66 @@ end
 
 num = length(yeast_strain_id)
 
+step
+   description: "Preparing for plating"
+   note: "Collect tubes %{yeast_strain_id} with transformed yeast cells from the heat block, location: B3.355.
+          Place them in a small tube rack."
+   check: "Spin the tube down for 30 seconds on a small tabletop centrifuge."
+   check: "Carefully remove the supernatant with a pipettor (~400μL setting total)."
+   check: "Add 600μL of molecular grade water to each tube."
+   check: "Vortex the tubes for 15-30 sec."
+end
+
 take
   strain = item yeast_strain_id
   plate  = num plate_type
-#  plate_2 = 1 plate_type_2
-#  beads = 1 "Glass Bead Aliquot (sterile)"
-#  collector = 1 "Glass Bead Waste Collector"
   note: "Do not take plate from top of the stack if there is a date stick on it, take from the middle of the stack."
 end
 
 step
-  description: "Write down your initials and date on the plate"
+  description: "Label the YPAD plates"
+  note: "Write down your initials and date on the YPAD plates. Write 'CONTROL' on one of the plates."
   image: "write_on_the_plate"
 end
 
-#
-#
-#
-#  REMOVING SUPERNATANT
-#
-#
+
 ii = 0
 r  = []
 
 while ii < length(yeast_strain_id)
 
-   step
-     description: "Add sterile glass beads to the plate"
-     note: "Invert the plate so that the lid is on the bench. 
-            Add 5-10 beads to the inside of the lid (lift the plate up). 
-            Place the plate back on the lid afterwards."
-     image: "pour_beads_on_plate"
+   num_id = yeast_strain_id[ii]
+
+   if ii == 0
+      step
+        description: "Add sterile glass beads to the plate"
+        check: "Take 'CONTROL' YPAD plate."
+        note: "Invert the plate so that the lid is on the bench. 
+               Add 5-10 beads to the inside of the lid (lift the plate up). 
+               Place the plate back on the lid afterwards."
+        image: "pour_beads_on_plate"
+      end
+   else
+      step
+        description: "Add sterile glass beads to the plate"
+        check: "Take another sterile YPAD plate."
+        note: "Invert the plate so that the lid is on the bench. 
+               Add 5-10 beads to the inside of the lid (lift the plate up). 
+               Place the plate back on the lid afterwards."
+        image: "pour_beads_on_plate"
+      end
    end
 
    step
      description: "Vortex the sample"
-     note: "Vortex the sample on vortexor."
+     note: "Vortex tube %{num_id} with transformed yeast cells on vortexor."
      image: "vortex_tube"
    end
 
    step
      description: "Transfer the sample to the center of the plate"
      note: "Invert the plate so the beads are on the agar surface. Lift the lid and 
-            pipette 200 µL of the transformed yeast mixture on the agar surface
+            pipette 200 µL of the transformed yeast mixture from tube %{num_id} on the agar surface
             and put the lid back on the plate."
      warning: "Do not place the place the lid on lab bench while you do this."
      image: "pipette_culture_on_plate"
