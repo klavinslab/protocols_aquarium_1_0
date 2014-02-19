37b754435e1d19700643f8b35d18951cf4199f01
alexbaryshev <baryshev@uw.edu>
2014-02-19 13:36:22 -0800

    Update heat_shock.pl

--- labw14/group5/yeast_transformation/heat_shock.pl
+++ labw14/group5/yeast_transformation/heat_shock.pl
@@ -1,8 +1,8 @@
-#argument
-#  yeast_plasmid_mixture: sample array, "Mixture to transform"
-#end
+argument
+  yeast_transformation_mixtures: sample array, "Mixture to transform"
+end
 
-#num = length(yeast_plasmid_mixture)
+num = length(yeast_transformation_mixtures)
 
 step
  description: "This protocol describes how to heat shock the yeast mixture"
@@ -14,10 +14,22 @@ end
 
 step
   description: "Heat shock yeast - plasmid mixture"
-  note: "Place all the 1.5mL tubes with yeast-plasmid mixtures into the heat block 42C for 15 min, location: B3.355."
+  note: "Place all the 1.5 mL tubes with yeast transformation mixtures into the heat block 42C for 15 min, location: B3.355."
+  bullet: "Tubes should have the following id numbers: %{yeast_transformation_mixtures}."
+end
+
+ii = 0
+
+while ii < length()
+  modify
+    y[ii]
+    location:"B14.310"
+    inuse:0
+  end
+  ii = ii + 1
 end
 
 step
   description: "Wait 15 minutes here"
-  timer: { hours: 0, minutes: 0, seconds: 15 }
-end  
+  timer: { hours: 0, minutes: 15, seconds: 0 }
+end
