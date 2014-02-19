d8fb25f2f7ed2c77636aff458854614099794839
lavania <chandrashekharla@gmail.com>
2014-02-18 16:00:29 -0800

    Update streak.pl
    Modified for use in flow

--- labw14/group1/streak.pl
+++ labw14/group1/streak.pl
@@ -1,5 +1,5 @@
 argument
-  e_coli_strain_plate: sample, "Your plated E. coli"
+  e_coli_glycerol_stock: sample, "Glycerol Stock of frozen cells"
 end
 
 take
@@ -17,54 +17,54 @@ step
   #TODO: Image woμLd be a great idea
 end
 
-step
-  description: "Take four 1.5 mL tubes and pipette 100 μL LB into each one"
-end
+#step
+#  description: "Take four 1.5 mL tubes and pipette 100 μL LB into each one"
+#end
 
 take
-  plate = item e_coli_strain_plate
+  stock = item e_coli_glycerol_stock
 end
 #plate = plate[0]
 
-step
-  description: "Pick four colonies and transfer a different colony into each of the four 1.5 mL tubes"
-  bullet: "Find a colony that is not touching any other colony."
-  bullet: "Take a 200 μL tip (the one used by the 10-100 μL or 20-200 μL pipettor) by hand."
-  bullet: "With the pipette tip, scrape up the colony and then mix it into one of the 1.5mL tubes. Scrape the end of the tip against the wall of the tube below the liquid level to ensure the colony has been deposited"
-  check: "Repeat the above untill each 1.5 mL tube has a single colony."
-  warning: "Be gentle.  The agar is soft."
-end
+#step
+#  description: "Pick four colonies and transfer a different colony into each of the four 1.5 mL tubes"
+#  bullet: "Find a colony that is not touching any other colony."
+#  bullet: "Take a 200 μL tip (the one used by the 10-100 μL or 20-200 μL pipettor) by hand."
+#  bullet: "With the pipette tip, scrape up the colony and then mix it into one of the 1.5mL tubes. Scrape the end of the tip against the wall of the tube below the liquid level to ensure the colony has been deposited"
+#  check: "Repeat the above untill each 1.5 mL tube has a single colony."
+#  warning: "Be gentle.  The agar is soft."
+#end
 
 step
-  description: "Deposit each colony into a sector and streak"
+  description: "Deposit Glycerol stock containing the cells to a sector and streak"
   bullet: "Pipette 5 μL from one of the 1.5 mL tubes on to the outer edge of the plate in the center of one of the sectors"
   bullet: "Take a fresh 200 μL tip by hand"
   bullet: "At a low angle streak the tip starting from the liquid gently drag the tip back and fourth from sector edge to sector edge in non-overlapping lines eventually reaching near the center."
   bullet: "Discard the 1.5 mL tube you just used"
-  check: "Repeat until you have run out of tubes and empty sectors"
+  check: "Repeat until you have run out of empty sectors"
   warning: "Do not cross the sector lines with a streak"
   warning: "Streak lines should not cross"
-  warning: "You are not trying to spead the liquid.  You are just trying to get some cells on the tip so you deposit individual cells as you streak.  That 5 μl probably have 10,000 cells and you only want one by the end of the streak."
+  warning: "You are not trying to spead the liquid."
   image: "sector_streak"
 end
 
 step
-  description: "Store the plate %{e_coli_strain_plate} in 4C fridge."
-  note: "Wrap up the plate with id %{e_coli_strain_plate} using parafilm and put it in the Box 1 in deli fridge located at D2.100."
+  description: "We will now incubate the cells overnight."
 end
 
 
 produce
-  1 "Agar plate" from plate[0]
+  1 "Agar plate" from stock[0]
   note: "Place the streaked plate upside-down (lid on the bottom) in 30 C incubator at A1.110"
   release sector_plate
   location: "A1.110"
 end
 
 release lb
+release stock
 
-modify
-  plate[0]
-	location: "DFP.0"
-  inuse: 0
-end
+#modify
+#  plate[0]
+#	location: "DFP.0"
+#  inuse: 0
+#end
