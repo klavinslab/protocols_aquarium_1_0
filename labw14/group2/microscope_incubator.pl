037533af5891ee01b6efc083bbdda48af1b3a8f1
hamsanchez <hamilton.sanch@gmail.com>
2014-02-19 13:22:01 -0800

    Update microscope_incubator.pl

--- labw14/group2/microscope_incubator.pl
+++ labw14/group2/microscope_incubator.pl
@@ -8,5 +8,10 @@ step
   bullet: "Ask lab staff for access to the biohazard level 2 room"
   bullet: "Head to the biohazard level 2 room, and turn on the incubator for the microscope"
   bullet: "The room is adjacent to the back door of Prof. Klavins lab"
-  note: "The incubator will be labeled with '0'."
+  note: "The incubator will be labeled with '0' and should be set to 37 degrees"
+end
+
+step
+  description: "Finished!"
+  note: "Click next to end the protocol"
 end
