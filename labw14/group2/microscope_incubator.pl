dbe1c99d4ee3fd5638a30c224ab1d9c2df13aa50
hamsanchez <hamilton.sanch@gmail.com>
2014-02-19 13:20:25 -0800

    Update microscope_incubator.pl

--- labw14/group2/microscope_incubator.pl
+++ labw14/group2/microscope_incubator.pl
@@ -8,5 +8,5 @@ step
   bullet: "Ask lab staff for access to the biohazard level 2 room"
   bullet: "Head to the biohazard level 2 room, and turn on the incubator for the microscope"
   bullet: "The room is adjacent to the back door of Prof. Klavins lab"
-  note: "The microscope incubator is set to 37 degrees. This ensures cells maintain optimal temperature for growth."
+  note: "The incubator will be labeled with '0'."
 end
