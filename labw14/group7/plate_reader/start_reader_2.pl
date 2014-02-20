49e0b86c974b17c03777dbc99eda98966478e2f4
dstarkebaum <dasman@uw.edu>
2014-02-20 14:21:14 -0800

    Update start_reader_2.pl

--- labw14/group7/plate_reader/start_reader_2.pl
+++ labw14/group7/plate_reader/start_reader_2.pl
@@ -1,28 +1,3 @@
-step #The flllowing two steps should be a "take" instead, but the antifog solution and assay plates are not in the Aquarium inventory
-  description: "Fetch your loaded assay plate"
-  note: "You should try to move quickly through these steps, as your cells will continue to grow as they are sitting there!"
-  check: "You should have a pre-loaded 96 well assay plate in your posession to start this protocol"
-end
-
-step 
-  description: "Fetch defogger solution"
-  check: "Retrieve the 'Fog Tech anti-fog solution' from location B5.530"
-end
-
-step
-  description: "Wipe down the plate lid with defogger solution"
-  note: "This prevents condensation from disrupting the measurements"
-  check: "Squirt a small amount of anti-fog solution onto a Kimwipe, and use it to wipe the inside of the lid"
-end
-
-step
-  description: "Seal the plate"
-  check: "Retrieve a box of aluminum foil from location x.x"
-  check: "Cut out a small (3x3cm) square of aluminum, and fold it over 3~4 times to construct a small (3mm thick) shim"
-  check: "Insert the small foil shim into the corner of the assay plate, and close the lid (This shim will keep the lid from rattling during the run)"
-  #TODO: include picture
-end
-
 step
   description: "Start the plate reader software"
   check: "Move to the 'Biotech Synergy HT' plate reader at location B9.355"
@@ -32,15 +7,9 @@ end
 
 step
   description: "Clear the previous data"
-  check: "Go to 'File->Open...', and open the file 'Lab B Sample Run'"
+  check: "In the intro pop-up menu, open the file 'Lab B Sample Run' (Look in recent files)"
   check: "Immediately go to 'File->Save As...', and save this into a new file name"
   check: "Click on 'Procedure->Change run time' to clear all previous data"
-  check: "Click on the 'Run Procedure' button to start.  This should cause the door to open up"
-end
-
-step
-  description: "Insert the plate into the plate reader"
-  check: "Load the assay plate into the plate reader"
-  check: "Click the 'read' button to start the program"
-  check: "You will now wait (30 minutes?) until your first induction step"
+  check: "Click on the 'Read Plate' button (at the top) to start the program."
+  check: "Click the 'Start Run' button in the dialog that opens. This should produce a pop-up asking your to wait for the bulb to heat up to 37C"
 end
