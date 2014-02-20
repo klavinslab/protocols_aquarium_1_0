9d75cf73fa60bbb7ec4e3a30cd83c7c55156214f
Greg Nelson <gln@georgetown.edu>
2014-02-19 18:00:42 -0800

    Update get_matrix_sample_cols_upto_16_rows.pl

--- labw14/group8/get_matrix_sample_cols_upto_16_rows.pl
+++ labw14/group8/get_matrix_sample_cols_upto_16_rows.pl
@@ -1,8 +1,23 @@
 argument
   
   samples: sample array, "Ingredients"
+  # could have a key argument here to set key, for now just say amount
   row1: number array , "Amount in microliters for each ingredient, enter 0 for none"
-
+  row2: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row3: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row4: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row5: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row6: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row7: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row8: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row9: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row10: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row11: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row12: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row13: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row14: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row15: number array , "Amount in microliters for each ingredient, enter 0 for none"
+  row16: number array , "Amount in microliters for each ingredient, enter 0 for none"
 end
 
 # gather all errors ie not enough of a reagent
@@ -12,6 +27,11 @@ end
 # depending on total amount used
 
 # process the array and return
+if length(row2) > 0
+  log
+    gt0: 1
+  end
+end
 
 result = [ 
            [ {input: 1, microliters: 0.1}, {input: 2, amount: 2}],
@@ -20,4 +40,3 @@ result = [
 
 log
   return: { output: result}
-end
