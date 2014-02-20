1b5937e69897de56e7666adc55733bc59141ffd6
Greg Nelson <gln@georgetown.edu>
2014-02-19 18:44:54 -0800

    Create gibson_results_summary.pl

---
+++ labw14/group8/gibson_results_summary.pl
@@ -0,0 +1,8 @@
+argument
+  gibson_results: sample array, "the resulting gibsons"
+end
+
+step
+  description: "print out the gibson results, using :data on the samples"
+  note: "%{gibson_results}"
+end
