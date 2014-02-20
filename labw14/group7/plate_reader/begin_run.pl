77e02bf5dcb26f1439846465d6f2a1d978d2d8f9
dstarkebaum <dasman@uw.edu>
2014-02-20 14:22:49 -0800

    Create begin_run.pl

---
+++ labw14/group7/plate_reader/begin_run.pl
@@ -0,0 +1,12 @@
+step #The flllowing two steps should be a "take" instead, but the antifog solution and assay plates are not in the Aquarium inventory
+  description: "Fetch your loaded assay plate"
+  note: "You should try to move quickly through these steps, as your cells will continue to grow as they are sitting there!"
+  check: "You should have a pre-loaded 96 well assay plate in your posession to start this protocol"
+end
+
+step
+  description: "Insert the plate into the plate reader"
+  check: "Load the assay plate into the plate reader"
+  check: "Click the 'read' button to start the program"
+  check: "You will now wait (30 minutes?) until your first induction step"
+end
