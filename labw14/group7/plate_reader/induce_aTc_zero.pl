7b664ae191576dfb0509558f017ebdd6133f84d1
Erik909 <erik909@uw.edu>
2014-02-19 16:52:55 -0800

    Update induce_aTc_zero.pl

--- labw14/group7/plate_reader/induce_aTc_zero.pl
+++ labw14/group7/plate_reader/induce_aTc_zero.pl
@@ -1,17 +1,17 @@
 argument
-#  aTc_inducer_in: object, "The aTc inducer to add"
+  aTc_inducer_in: object, "The aTc inducer to add"
   plate_chart: string, "The filename of the chart to use"
 end
 
-#take
-#  aTc_inducer: 1 aTc_inducer_in
-#end
+take
+  aTc_inducer: 1 aTc_inducer_in
+end
 
 step
   description: "Add the inducer to the wells indicated in the chart"
   check: "Pipette 1uL of aTc solution into each well"
   note: "Use a new tip each time to avoid contamination"
-  image: plate_chart
+  image: "%{plate_chart}"
 end
 
-#release aTc_inducer
+release aTc_inducer
