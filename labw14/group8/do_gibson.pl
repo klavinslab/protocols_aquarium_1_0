10b9afe283b840c96762cfda4ceb287c9fe02fca
Greg Nelson <gln@georgetown.edu>
2014-02-20 14:58:58 -0800

    Update do_gibson.pl

--- labw14/group8/do_gibson.pl
+++ labw14/group8/do_gibson.pl
@@ -2,7 +2,7 @@
 # return: {gibsons: g, hash: {egkey: "egvalue"}}
 
 argument
-  sample_concentrations: sample array, "the gibsons to do" 
+  gibsons: sample array, "the gibsons to do" 
     # can i silently produce gibsons with data for the concentrations
     # then use that data to display to user what to do to make them?
     # then destroy / replace the fake items
@@ -10,11 +10,11 @@ end
 
 step
   description: "print out the input gibson array"
-  note: "%{sample_concentrations}"
+  note: "%{gibsons}"
 end
 
 aborted_samples = [ ]
 
 log
-  return: {completed_samples: sample_concentrations, aborted_samples: aborted_samples} # put resulting samples from produce here
+  return: {completed_samples: gibsons, aborted_samples: aborted_samples} # put resulting samples from produce here
 end
