c0df9ad60441936528379131ccebca4146b761cd
Greg Nelson <gln@georgetown.edu>
2014-02-20 13:57:46 -0800

    Update do_gibson.pl

--- labw14/group8/do_gibson.pl
+++ labw14/group8/do_gibson.pl
@@ -5,6 +5,7 @@ argument
   sample_concentrations: sample array, "the gibsons to do" 
     # can i silently produce gibsons with data for the concentrations
     # then use that data to display to user what to do to make them?
+    # then destroy / replace the fake items
 end
 
 step
