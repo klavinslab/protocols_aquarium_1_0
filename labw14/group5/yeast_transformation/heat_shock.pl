95065d8c787a8cf8e9ebabea28836f0f4a0f367c
bonghoe <bonghoe@users.noreply.github.com>
2014-02-19 17:02:35 -0800

    Update heat_shock.pl

--- labw14/group5/yeast_transformation/heat_shock.pl
+++ labw14/group5/yeast_transformation/heat_shock.pl
@@ -16,6 +16,7 @@ step
   description: "Heat shock yeast - plasmid mixture"
   note: "Place all the 1.5 mL tubes with yeast transformation mixtures into the heat block 42C for 15 min, location: B3.355."
   bullet: "Tubes should have the following id numbers: %{yeast_transformation_mixtures}."
+  image: "heat_shock_yeast_transformation"
 end
 
 ii = 0
