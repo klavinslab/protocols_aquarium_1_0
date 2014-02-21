83e1d659a4893734bc024b2c9470961e484c219b
Nathanrollins <nrollins@uw.edu>
2014-02-20 16:07:17 -0800

    Update gene_assembly_PCA.pl

--- labw14/group6/full_length_protocols/gene_assembly_PCA.pl
+++ labw14/group6/full_length_protocols/gene_assembly_PCA.pl
@@ -64,13 +64,13 @@ step
 end
 
 step
-  description: "wait about 2 hours"
+  description: "Wait about 2 hours"
   note: "Meanwhile, you may run the pour_gel protocol inside the gel room to have your gel ready before the next step."
 end
 
 step
-  description: "Go to the thermal cycler to pick up the PCR tube you put after it is done."
-  note: "Put it on you bench after you picked up the PCR tube"
+  description: "Go to the thermal cycler and pick up the product of the PCA."
+  note: "And place the tube at your bench"
 end
 
 produce
