f74c9c748b97cb02ceb97ac829c9541e1dd151ad
vchavali <vchavali@users.noreply.github.com>
2014-02-24 13:08:14 -0800

    Update lib.pl

--- labw14/group4/lib.pl
+++ labw14/group4/lib.pl
@@ -3,7 +3,7 @@
 # takes in an original sample id and a sample array of hashes and returns the first descendent sample as a hash
 function find_original( o_id, sample_array)
   foreach elem in sample_array
-    if o_id == elem[:original_id]
+    if o_id == elem[:data][:original_id]
       return elem
     end
   end
