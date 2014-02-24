440cedd060a65fc8e7f418896310b7dfddf87868
vchavali <vchavali@users.noreply.github.com>
2014-02-24 15:03:39 -0800

    Update test.pl

--- labw14/group4/test.pl
+++ labw14/group4/test.pl
@@ -5,23 +5,5 @@ require "labw14/group4/lib.pl"
 a = [{id: 1, original_id: "a"}, {id: 2, original_id: "b"}, {id: 3, original_id: "a"}]
 
 step
-  description: "%{a}"
-end
-
-s = find_original("a", a)
-
-step
-  description: "sample %{s} came from the original sample 'a'"
-end
-
-d = find_original("b", a)
-
-step
-  description: "sample %{d} came from the original sample 'b'"
-end
-
-f = find_original("a", a)
-
-step
-  description: "the first occurance of 'a' is %{f}. This should not be id:3"
+  description: "Hi"
 end
