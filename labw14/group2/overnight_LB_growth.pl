ecde86f774894eeaad5ec552e32215ec4ce6e4da
hamsanchez <hamilton.sanch@gmail.com>
2014-02-24 20:24:54 -0800

    Update overnight_LB_growth.pl

--- labw14/group2/overnight_LB_growth.pl
+++ labw14/group2/overnight_LB_growth.pl
@@ -5,13 +5,11 @@ end
 take
   falcon_tube = 1 "50 mL LB liquid aliquot (sterile)"
   test_tube = 1 "14 mL Test Tube"
-  glycerol = item id
 end
   
 step
   description: "Prepare 14 mL test tube for incubation"
   note: "Click next to begin the incubation process"
-  warning: "This procedure must be done before the glycerol stock thaws!"
 end
 
 step
@@ -19,9 +17,14 @@ step
   note: "Pipette 2mL of liquid LB into the 14 mL test tube. Make sure to use a fresh sterile pipette tip"
 end
 
+take
+  glycerol = item id
+end
+
 step
   description: "Mix in glycerol stock"
   warning: "BE EXTREMELY CAREFUL NOT TO CONTAMINATE THE GLYCEROL STOCK!"
+  warning: "This must be done before the glycerol thaws!"
   bullet: "Using a fresh 1mL pipette tip scrape a small amount of glycerol stock onto the pipette tip"
   bullet: "Tilt test tube slightly so that the liquid LB is on the side of the 14 mL test tube"
   bullet: "swirl the tip gently into the LB solution in the test tube, and dispose of the pipette tip"
