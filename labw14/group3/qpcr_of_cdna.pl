5196b2a8f0280e08579c63f99a2210af024e6dc6
manewton <michael.andrew.newton@gmail.com>
2014-02-24 17:49:45 -0800

    Update qpcr_of_cdna.pl

--- labw14/group3/qpcr_of_cdna.pl
+++ labw14/group3/qpcr_of_cdna.pl
@@ -1,15 +1,29 @@
 argument
-  fragment: sample,"The cDNA sample to run in qPCR"
+  fragment1: sample,"The cDNA sample to run in qPCR"
+  fragment2: sample,"The cDNA sample to run in qPCR"
   enzyme: object, "The iTaq Universal SYBR Green Supermix"
-  primer_forward: sample, "The forward primer"
-  primer_reverse: sample, " The reverse primer"
+  primer_forward1: sample, "The forward primer"
+  primer_reverse1: sample, " The reverse primer"
+  primer_forward2: sample, "The forward primer"
+  primer_reverse2: sample, " The reverse primer"
+  primer_forward3: sample, "The forward primer"
+  primer_reverse3: sample, " The reverse primer"
+  primer_forward4: sample, "The forward primer"
+  primer_reverse4: sample, " The reverse primer"
 end
 
 take
-  y = item fragment
+  x = item fragment1
+  y = item fragment2
   sybr = item enzyme
-  pf = item primer_forward
-  pr = item primer_reverse
+  pf1 = item primer_forward1
+  pr1 = item primer_reverse1
+  pf2 = item primer_forward2
+  pr2 = item primer_reverse2
+  pf3 = item primer_forward3
+  pr3 = item primer_reverse3
+  pf4 = item primer_forward4
+  pr4 = item primer_reverse4
 end
 
 information "qPCR of cDNA - Prep and Run"
@@ -23,26 +37,60 @@ end
 step
   description: "Dilution of cDNA (if necessary)"
   bullet: "Label a 1.5 mL tube to contain diluted cDNA sample"
-  bullet: "Transfer 1 μL of cDNA to new tube"
-  bullet: "Add __ μL of Molecular Grad Water to new tube"
+  #bullet: "Transfer 1 μL of cDNA to new tube"
+  #bullet: "Add __ μL of Molecular Grade Water to new tube"
+  bullet: "Dilute the cDNA to a concentration of about 50 ng/μL"
   bullet: "Stir dilution with pipette tip or vortex"
 end
 
 step 
   description: "Prepare Enough Master Mix for All Reactions"
-  bullet: "Add 10 μL of iTaq SYBR Green Supermix to each qPCR plate well for the number of total reactions."
-  bullet: "Add __ μL of the Forward Primer"
-  bullet: "Add __ μL of the Reverse Primer"
-  bullet: "Stir the mixture with your pipette tip"
+  check: "Obtain a 96 well qPCR Plate and Optically Transparent Film"
+  bullet: "Add 10 μL of iTaq SYBR Green Supermix to 6 qPCR plate wells."
+end
+
+step
+  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward1}"
+  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse1}"
+  note: "Be sure to use proper sterile technique to guarantee accuracy of results."
+end
+
+step
+  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward2}"
+  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse2}"
+  note: "Be sure to use proper sterile technique to guarantee accuracy of results."
+end
+
+step
+  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward3}"
+  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse3}"
+  note: "Be sure to use proper sterile technique to guarantee accuracy of results."
+end
+
+step
+  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward4}"
+  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse4}"
   note: "Be sure to use proper sterile technique to guarantee accuracy of results."
 end
 
 step
   description: "Addition of Diluted cDNA Samples"
-  bullet: "Add 1 μL of diluted cDNA to its corresponding wells in qPCR plate"
-  note: "Total cDNA added should be around 50 ng"
-  bullet: "Stir mixture with pipette tip"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment1} to well A1"
+  check: "Stir mixture with pipette tip"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment1} to well B1"
+  check: "Stir mixture with pipette tip"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment1} to well C1"
+  check: "Stir mixture with pipette tip"
 end
+
+step
+  description: "Addition of Diluted cDNA Samples (continued)"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment2} to well D1"
+  check: "Stir mixture with pipette tip"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment2} to well E1"
+  check: "Stir mixture with pipette tip"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment2} to well F1"
+  check: "Stir mixture with pipette tip"
 
 step
   bullet: "Seal the qPCR plate wells with optically transparent film"
@@ -51,19 +99,23 @@ end
 
 step
   description: "Load qPCR Plate"
-  bullet: "Move the qPCR over to the BioRad CFX96 ThermoCycler"
+  bullet: "Move the qPCR over to the BioRad CFX96 ThermoCycler ___"
+  bullet: "Open the qPCR software"
   bullet: "Use computer software to open the lid"
   bullet: "Place the tray into the thermocycler with well A1 in the upper left corner"
   bullet: "Click the Close Lid Button on the computer"
 end
 
 step 
-  bullet: "Label the wells in the software to correspond with Reaction Mixtures in the plate"
-  bullet: "Select the ____ program in the software."
-  bullet: "Click the Start Run button to begin program."
+  check: "Label the wells in the software to correspond with Reaction Mixtures in the plate"
+  check: "Select the ____ program in the software."
+  check: "Click the Start Run button to begin program."
   note: "Program will take approximately 45-50 minutes"
 end
 
 step
   bullet: "Analyze the data after run"
+  check: "Save the data to the folder on the desktop on the computer"
 end
+
+release [x[0], y[0], sybr[0], pf1[0], pr1[0], pf2[0], pr2[0], pf3[0], pr3[0], pf4[0], pr4[0]]
