4ebde077f5626b83271e5e785fd365b0b4260d20
pegahsz <pegahsz@uw.edu>
2014-02-25 20:56:32 -0800

    Update produce_slice2.pl

--- labw14/group6/full_length_protocols/produce_slice2.pl
+++ labw14/group6/full_length_protocols/produce_slice2.pl
@@ -2,6 +2,10 @@ argument
   gel_lane: sample, "The gel lane you got after run the gel"
 end
 
+step
+  description: "Put down the id generetaed by this protocol on the second tube containing the gel slice."
+end
+
 produce
   t = 1 "Gel Slice" of "fGA"
 end
