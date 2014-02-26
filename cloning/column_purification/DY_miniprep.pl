7506a381b2b395119c81993b2826b03b1e49ecda
David Younger <dyounger@gmail.com>
2014-02-26 13:32:59 -0800

    Update DY_miniprep.pl

--- cloning/column_purification/DY_miniprep.pl
+++ cloning/column_purification/DY_miniprep.pl
@@ -1,7 +1,6 @@
 argument
   volume: number, "The elution volume in ul"
-  strain_id: sample("Transformed E coli Strain"), "The transformed E. coli strain that you are using"
-  plasmid_id: sample("Plasmid"), "The name of the plasmid you are purifying"  
+  strain_id: sample("Plasmid"), "An E. coli overnight of the plasmid to be purified"
 end
 
 step
@@ -16,64 +15,105 @@ y=length(strain_id)
 
 step
   description: "Grab %{y} eppendorf tubes and label them"
-  note: ""
+  note: "Label the top of the tubes 1, 2, 3, ... , %{y}"
 end
 
-
-
-step 
-  description: "Pipet 19 µL molecular grade water into wells 1 through %{y}."
-  note:"Be careful to pipette into the bottom of the tube, not onto the side of the tube."
-end
 
 x=0
 while x < y
-  a=template_ids[x]
+  a=strain_id[x]
   z=x+1
   step 
-    description: "Pipet 1 µL of plasmid with id %{a} into well %{z}."
+    description: "Pipet 1000 uL of plasmid with id %{a} into tube %{z}."
   end
   x = x+1
-end
-
-x=0
-while x < y
-  a=forward_ids[x]
-  b=reverse_ids[x]
-  z=x+1
-  step 
-    description: "Add both forward and reverse primers"
-    check: "Pipet 2.5 µL of primer with id %{a} into well %{z}."
-    check: "Pipet 2.5 µL of primer with id %{b} into well %{z}."
-  end
-  x = x+1
-end
-
-step 
-  description:"Pipet 25 µL Phusion Master Mix with id %{enzyme_id} into wells 1 through %{y}."
-  note:"USE A NEW PIPETTE TIP FOR EACH WELL AND PIPETTE UP AND DOWN TO MIX"
 end
 
 step
-  description: "Begin the PCR reaction with a thermal cycler"
-  check: "Put the cap on the PCR strip tubes and press each one very hard to make sure they are sealed."
-  check: "Place the tube into an available thermal cycler and close the lid"
-  check: "Click Home then click Saved Protocol, choose DAVID, choose CLONEPCR."
-  check: "Set the anneal temperature to %{tanneal}"
-  check: "hit 'run' and select 50ul"
-  image: "thermal_cycler_home"
+  description: "Spin down the cells"
+  note: "Spin at 5,800Xg for 2 minutes"
+  warning: "Make sure to balance the centrifuge. If you have an odd number of samples, use a balance tube with water"
 end
 
-release [phusion_stock[0]]
-release forward_primer_stock
-release reverse_primer_stock
-release plasmid_stock
+step 
+  description: "Remove the supernatant"
+  note:"Pour off the supernaant into liquid waste, being sure not to upset the pellet. Pipette out the residual supernatant"
+end
+
+step 
+  description: "Resuspend in P1"
+  note:"Pipette 250 ul of P1 into each tube and vortex strongly to resuspent"
+end
+
+step 
+  description: "Add P2 and gently invert to mix"
+  note:"Pipette 250 ul of P2 into each tube and gently invert 10 times to mix. Tube contents should turn blue."
+  warning:"This step should be done rapidly. Cells should not be exposed to active P2 for more than 5 minutes"
+end
+
+step 
+  description: "Add N3 and gently invert to mix"
+  note:"Pipette 350 ul of N3 into each tube and gently invert 10 times to mix. Tube contents should turn colorless."
+end
+
+step
+  description: "Spin tubes and set up columns and final tubes"
+  note: "Spin at 17,000Xg for 10 minutes"
+  warning: "Make sure to balance the centrifuge. If you have an odd number of samples, use a balance tube with water"
+  check: "Grab %{y} purple miniprep spin columns"
+  check: "Label the side of the columns 1, 2, 3, ... , %{y}"
+  check: "Grab %{y} new eppendorf tubes"
+  check: "Label the top of the tubes 1, 2, 3, ... , %{y}"
+end
+
+step 
+  description: "Carefully pour the supernatant into the columns"
+  note:"The contents of tube 1 should be poured into column 1, the contents of tube 2 should be poured into column 2, ..."
+end
+
+step 
+  description: "Spin the columns"
+  note:"Spin at 17,000Xg for 1 minute"
+end
+
+step 
+  description: "Wash the column with PE buffer"
+  check: "Remove the columns from the centrifuge and discard the flow through into a liquid waste container"
+  warning: "Make sure the PE bottle that you are using as ethanol added!"
+  check: "Add 750 ul of PE buffer to each column."
+  check: "Close the PE bottle tightly."
+  check: "Spin the columns at 17,000Xg for 1 minute"
+end
+
+step 
+  description: "Perform a final spin to fully dry the columns"
+  check: "Remove the columns from the centrifuge and discard the flow through into a liquid waste container"
+  check: "Spin the columns at 17,000Xg for 1 minute"
+  check: "During the spin, open the clean eppendorf tubes labeled 1 - %{y}"
+end
+
+step 
+  description: "Elute with water"
+  check: "Remove the columns from the centrifuge"
+  check: "Inidividually take each column out of the flowthrough collector and put it into the open eppendorf tube of the same number."
+  warning: "For this step, use a new pipette tip for each sample to avoid cross contamination"
+  check: "Pipette %{volume} ul of water into the CENTER of each column"
+  check: "Let the tubes sit on the bench for 5 minutes"
+  check: "Spin the columns at 17,000Xg for 1 minute"
+  check: "Remove the tubes and discard the columns"
+end
+
+step 
+  description: "Re-label the final tubes"
+  note: "In the next %{y} screens, you will see the ID numbers of each sample in order. Write this number on a white tube cap sticker and stick it on the tube over the previously written number."
+end
 
 x=0
 while x < y
   produce
-    q = 1 "PCR Result" of fragment_names[x]
-    location: "Thermocycler"
+    q = 1 "PCR Result" of strain_id[x](____NAME____)
   end
   x = x+1
 end
+
+release overnight
