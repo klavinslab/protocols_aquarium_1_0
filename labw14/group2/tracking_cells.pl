edec7aa4771c8019c6e094a6c0453d80d7fc82d3
hamsanchez <hamilton.sanch@gmail.com>
2014-02-25 20:28:51 -0800

    Update tracking_cells.pl

--- labw14/group2/tracking_cells.pl
+++ labw14/group2/tracking_cells.pl
@@ -24,6 +24,7 @@ step
   bullet: "Press the 'Connect' button in the pop-up menu"
   bullet: "Close the pop-up menu"
   note: "If the device is already connected ignore this step"
+  image: "manage_devices"
 end
 
 step
@@ -33,11 +34,13 @@ step
   bullet: "Click on the 'Restore' button"
   bullet: "Navigate to D:/LABW14/Phase GRCY .xml"
   bullet: "Open the file to apply settings"
+  image: "optical_settings"
 end
 
 step
   description: "Finalizing optical settings"
   bullet: "On the toolbar click on 'Phase GFP'"
+  image: "tool_bar1"
 end
 
 step
@@ -45,6 +48,7 @@ step
   bullet: "Click on the 'View' menu"
   bullet: "Hover over the 'Aquisition Controls' sub menu"
   bullet: "Select both 'Cool Snaps ES Settings' and 'Ti Pad' options"
+  image: "aquisition_controls"
 end
 
 step
@@ -52,12 +56,14 @@ step
   bullet: "Click on the 'View' menu"
   bullet: "Hover over the 'Visualization Controls' option"
   bullet: "Select both 'XY Overview' and 'LUTs' options"
+  image: "view_controls"
 end
 
 step
   description: "Merge the UI windows"
   note: "Click and drag the UI windows into each other to merge them as tabs"
   note: "The program is a little finiky so when dragging the pop-ups, drag them to align with the tabs of another window before dropping"
+  image: "tools"
 end
 
 step
@@ -66,12 +72,13 @@ step
   bullet: "Hover over the 'Acquisition Controls' sub menu"
   bullet: "Click on the 'ND Aquisition' option"
   note: "Don't merge this window with the others. It will make your task more difficult."
-  image: "NIS_setup_UI"
+  image: "nd_aquire"
 end
 
 step
   description: "Turn on the camera"
   note: "Press the play button, on the tool bar, under the menu bar to turn on the camera"
+  image: "tool_bar2"
 end
 
 step
@@ -80,6 +87,7 @@ step
   bullet: "Click on the red minus sign button to turn it green"
   bullet: "Slide the marker next to the green bar to adjust the light allowed by the lens"
   note: "The light should be in a grey ish contrast where you can see texture on the screen"
+  image: "tools"
 end
 
 step
@@ -88,6 +96,7 @@ step
   bullet: "Click on the small box with arrows in four directions, in the bottom right corner, to center the LUT range" 
   bullet: "Adjust the left and right cursors on the LUTs tab to encompass the peaks"
   note: "This must be played by ear, sometimes encompassing the peaks makes it too bright so add some dark regions into the range to improve contrast"
+  image: "luts"
 end
 
 step
@@ -96,6 +105,7 @@ step
   bullet: "Click on the top most option of the tool bar to open the probe box"
   bullet: "Click and drag on the black edges of the box to create a 2x2 inch box at the center of the screen"
   bullet: "Right click on the probe and click on the 'Center to screen' option"
+  image: "image_bar1"
 end
 
 step
@@ -132,6 +142,7 @@ step
   description: "Navigating the cells"
   bullet: "Select the 'X-Y overview' tab; this will help you keep track of our selected cells"
   bullet: "Click on the 'adjust to points' button on the 'X-Y overview' window"
+  image: "xy_overview"
 end
 
 step
@@ -141,6 +152,7 @@ step
   bullet: "Press the black button on the remote focus controller to zero the z-axis"
   bullet: "Click on the 'Offset z-position' button"
   note: "Offsetting the z-position will set the value to the current controller value"
+  image: "xy_pos1"
 end
 
 step
