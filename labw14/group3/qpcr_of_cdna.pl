71422ca799fefc61886aaf627379293fb2f3d6c1
manewton <michael.andrew.newton@gmail.com>
2014-02-26 14:49:15 -0800

    Update qpcr_of_cdna.pl

--- labw14/group3/qpcr_of_cdna.pl
+++ labw14/group3/qpcr_of_cdna.pl
@@ -2,14 +2,14 @@ argument
   fragment1: sample,"The cDNA sample to run in qPCR"
   fragment2: sample,"The cDNA sample to run in qPCR"
   enzyme: sample, "The iTaq Universal SYBR Green Supermix"
-  primer_forward1: sample, "The forward primer"
-  primer_reverse1: sample, " The reverse primer"
-  primer_forward2: sample, "The forward primer"
-  primer_reverse2: sample, " The reverse primer"
-  primer_forward3: sample, "The forward primer"
-  primer_reverse3: sample, " The reverse primer"
-  primer_forward4: sample, "The forward primer"
-  primer_reverse4: sample, " The reverse primer"
+  primer_forward1: sample, "The forward primer ACT1"
+  primer_reverse1: sample, " The reverse primer ACT1"
+  primer_forward2: sample, "The forward primer ALG9"
+  primer_reverse2: sample, " The reverse primer ALG9"
+  primer_forward3: sample, "The forward primer gRNA_handle"
+  primer_reverse3: sample, " The reverse primer gRNA_handle"
+  primer_forward4: sample, "The forward primer eYFP"
+  primer_reverse4: sample, " The reverse primer eYFP"
 end
 
 take
@@ -51,29 +51,43 @@ end
 
 step
   description: "Addition of Primers"
-  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward1})"
-  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse1})"
+  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward1}) to well A1"
+  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse1}) to well A1"
   note: "Be sure to use proper sterile technique to guarantee accuracy of results."
 end
 
 step
   description: "Addition of Primers"
-  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward2})"
-  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse2})"
+  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward2}) to well A2"
+  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse2}) to well A2"
   note: "Be sure to use proper sterile technique to guarantee accuracy of results."
 end
 
 step
   description: "Addition of Primers"
-  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward3})"
-  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse3})"
+  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward3}) to well A3"
+  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse3}) to well A3"
   note: "Be sure to use proper sterile technique to guarantee accuracy of results."
 end
 
 step
   description: "Addition of Primers"
-  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward4})"
-  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse4})"
+  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward1}) to well A4"
+  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse1}) to well A4"
+  note: "Be sure to use proper sterile technique to guarantee accuracy of results."
+end
+
+step
+  description: "Addition of Primers"
+  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward2}) to well A5"
+  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse2}) to well A5"
+  note: "Be sure to use proper sterile technique to guarantee accuracy of results."
+end
+
+step
+  description: "Addition of Primers"
+  bullet: "Add 4.5 μL of the Forward Primer (sample id: %{primer_forward4}) to well A6"
+  bullet: "Add 4.5 μL of the Reverse Primer (sample id: %{primer_reverse4}) to well A6"
   note: "Be sure to use proper sterile technique to guarantee accuracy of results."
 end
 
@@ -81,19 +95,19 @@ step
   description: "Addition of Diluted cDNA Samples"
   check: "Add 1 μL of diluted cDNA (sample id: %{fragment1}) to well A1"
   check: "Stir mixture with pipette tip"
-  check: "Add 1 μL of diluted cDNA (sample id: %{fragment1}) to well B1"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment1}) to well A2"
   check: "Stir mixture with pipette tip"
-  check: "Add 1 μL of diluted cDNA (sample id: %{fragment1}) to well C1"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment1}) to well A3"
   check: "Stir mixture with pipette tip"
 end
 
 step
   description: "Addition of Diluted cDNA Samples (continued)"
-  check: "Add 1 μL of diluted cDNA (sample id: %{fragment2}) to well D1"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment2}) to well A4"
   check: "Stir mixture with pipette tip"
-  check: "Add 1 μL of diluted cDNA (sample id: %{fragment2}) to well E1"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment2}) to well A5"
   check: "Stir mixture with pipette tip"
-  check: "Add 1 μL of diluted cDNA (sample id: %{fragment2}) to well F1"
+  check: "Add 1 μL of diluted cDNA (sample id: %{fragment2}) to well A6"
   check: "Stir mixture with pipette tip"
 end
 
@@ -105,7 +119,7 @@ end
 
 step
   description: "Load qPCR Plate"
-  bullet: "Move the qPCR over to the BioRad CFX96 ThermoCycler ___"
+  bullet: "Move the qPCR over to the BioRad CFX96 ThermoCycler at B9.340"
   bullet: "Open the qPCR software"
   bullet: "Use computer software to open the lid"
   bullet: "Place the tray into the thermocycler with well A1 in the upper left corner"
@@ -115,7 +129,7 @@ end
 step 
   description: "Program Software and Run"
   check: "Label the wells in the software to correspond with Reaction Mixtures in the plate"
-  check: "Select the ____ program in the software."
+  check: "Select the qPCR_Template program located in the LabW14_qPCR_Protocols folder on the desktop."
   check: "Click the Start Run button to begin program."
   note: "Program will take approximately 45-50 minutes"
 end
