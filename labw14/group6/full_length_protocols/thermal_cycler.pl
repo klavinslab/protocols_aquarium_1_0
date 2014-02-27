45205e88f24b5a620b4efdb0390507c3f0702895
pegahsz <pegahsz@uw.edu>
2014-02-26 20:20:03 -0800

    Create thermal_cycler.pl

---
+++ labw14/group6/full_length_protocols/thermal_cycler.pl
@@ -0,0 +1,23 @@
+
+
+step
+  description: "You will now be asked to use the lab thermal cycler"
+  note: "The thermal cycler will be shared by all groups."
+  warning: "Make sure it is not in use. Never open the lid of the running thermal cycler."
+  image: "thermal_cycler_off"
+end
+
+step
+  description: "Place the tube into thermal cycler T2 at B3.335."
+end
+
+step
+  description: "Choose program on the thermal cycler."
+  note: "Click Home then click Saved Protocol, choose Nick folder, choose assemble."
+  image: "thermal_cycler_home"
+end
+
+step
+  description: "When both samples are loaded, hit 'run' on the thermal cycler and select 50 µL setting."
+  image: "thermal_cycler_select"
+end
