2fdc624d72c8797d87e5020e40ff612de86f4273
dstarkebaum <dasman@uw.edu>
2014-02-26 16:49:19 -0800

    Update prepare_media.pl

--- labw14/group7/plate_reader/prepare_media.pl
+++ labw14/group7/plate_reader/prepare_media.pl
@@ -1,4 +1,10 @@
 #By David Starkebaum and Erik Josberger, 2014/02/13
+step
+  description: "This protocol will prepare two 30mL aliquots of 0.4 percent glucose with amp and kan."
+  note: "This is a serological pipette (you will fech this in the next step)"
+  image: "serological pipette"
+  note: "The 'M9 liquid Glucose' listed in the next step should be 0.4 percent glucose (you may want to write this down this down)"
+end
 
 take
  media_tube = 2 "50 mL Falcon Tube"
@@ -22,8 +28,10 @@ step
 end
 
 step
-  description: "Add 'M9 liquid Glucose' to the 50mL falcon tubes"
+  description: "Add 30mL of 'M9 liquid Glucose' to two 50mL falcon tubes"
+  check: "Unscrew the caps of the glucose solution, as well as your two empty falcon tubes"
   check: "Attach the 25mL tip to the serological pipetter"
+  note: "You will add a total of 30mL glucose media to each falcon tube (in two steps, since the max volue is 25mL for the pipetter"
   check: "Use the electric serological pipette to add 15 mL of 'M9 liquid Glucose' into a 50mL falcon tube"
   check: "Add another 15 mL of 'M9 liquid Glucose' into the 50mL falcon tube"
   check: "Add 15mL of 'M9 liquid Glucose' into the second 50mL falcon tube"
@@ -31,6 +39,13 @@ step
   check: "Dispose of your 25mL serological pipette tip in tip waste"
 end
 
+step
+ description: "Make sure the antibiotics are fully melted"
+ note: "If the kan and amp solutions are not completely melted yet, you should hold them in your hand to warm them up until they are fully liquid"
+ check: "Vortex the kan and amp solutions or 5 seconds before proceeding"
+ note: "You will need at least 0.12 mL of amp, and 0.30 mL of kan for the next step, so check the level of the liquid in your aliquots to be sure they are sufficient."
+ check: "Grab another aliquot of kan or amp if you do not have enough"
+end
 
 step
   description: "Add antibiotics to the 50mL falcon tubes"
