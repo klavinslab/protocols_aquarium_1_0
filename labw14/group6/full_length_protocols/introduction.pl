5952e1ac3adb7c39b829dc150c00009b688ca5de
pegahsz <pegahsz@uw.edu>
2014-02-26 20:03:44 -0800

    Update introduction.pl

--- labw14/group6/full_length_protocols/introduction.pl
+++ labw14/group6/full_length_protocols/introduction.pl
@@ -1,19 +1,19 @@
-Step
+step
   description: "Our metacol guides the user through the steps required to perform gene assembly."
   note: "The user will input the initial oligonucleotides (short pieces of DNA) at the beginning, and then produce four stock mixtures of the samples. These are then mixed into two different PCR tubes, the first stock with the second, and the third stock with the fourth. And then they are heat treated in a thermal cycler in order to produce the assembled genes. After that, the assemblies are purified and measured using gel electrophoresis. The two genes are combined using Gibson assembly. And the resultant plasmid is transformed into E.coli which is cultured for use in colony sequencing. Finally, the product can be mini-prepped and submitted for gene sequencing."
 end
 
-Step
+step
   description: "The steps are as follows:"
   bullet: "Oligonucleotides are combined to create four stock solutions."
   bullet: "Stock solutions are combined (1 with 2, 3 with 4) into two PCR tubes and PCA is run."
-  Warning: "In this step, you may prepare both PCR tubes in a parallel manner and use the thermal cycler once for both."
+  warning: "In this step, you may prepare both PCR tubes in a parallel manner and use the thermal cycler once for both."
   bullet: "The resultant fragments are purified using gel electrophoresis."
   bullet: "The purified fragments are combined into a single plasmid using Gibson assembly."
   bullet: "This plasmid is transformed into E.coli."
   bullet: "The product gene is further identified through colony sequencing, and even gene sequencing."
 end
 
-Step
-  You may now start the Gene Assembly. Have fun!
+step
+  description: "You may now start the Gene Assembly. Have fun!"
 end
