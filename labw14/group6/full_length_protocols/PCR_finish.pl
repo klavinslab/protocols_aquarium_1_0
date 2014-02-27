aa64f7266e663cce28104f39a7c90ca4034661e6
pegahsz <pegahsz@uw.edu>
2014-02-26 20:24:56 -0800

    Update PCR_finish.pl

--- labw14/group6/full_length_protocols/PCR_finish.pl
+++ labw14/group6/full_length_protocols/PCR_finish.pl
@@ -1,11 +1,9 @@
 
-
 step
-  description: "Go to the thermal cycler and pick up the product of the PCA."
+  description: "Go to the thermal cycler and pick up the products of the PCA."
   note: "And place the tube at your bench"
+  warning: "You can keep them in -4C fridge (SF) in case you are not using them right away."
 end
-
-
 
 produce
   r = 1 "PCR Result" of "fGA"
@@ -17,11 +15,6 @@ produce
 
 end
 
-release phusion_stock
-release primer1_stock
-release primer2_stock
-release masterstock1
-release masterstock2
 
 log
   return: { PCR_Result_id1: r[:id] }
