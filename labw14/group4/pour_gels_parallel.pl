13d2b972cc1631813c942ad59d8c176f96898267
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-02-28 15:51:25 -0800

    Update pour_gels_parallel.pl

--- labw14/group4/pour_gels_parallel.pl
+++ labw14/group4/pour_gels_parallel.pl
@@ -97,7 +97,7 @@ end
 release [agarose[0], gel_green[0]]
 
 i = 0
-while i < gel_n #pcr_count 
+#while i < gel_n #pcr_count 
   i = i+1
   take
     gel_box = 1 "49 mL Gel Box With Casting Tray (clean)"
@@ -134,7 +134,7 @@ while i < gel_n #pcr_count
     release gel_box
   end
 
-end
+#end
 
 step
   description: "Clean up!"
@@ -146,4 +146,4 @@ log
   return: {gel_n: gel_n}
 end
 
-release [glasses[0], glove[0]
+release [glasses[0], glove[0]]
