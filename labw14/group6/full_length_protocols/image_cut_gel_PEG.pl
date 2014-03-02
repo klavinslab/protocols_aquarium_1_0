ba435a195d4494fe20d16b44fea5874f0cbcc284
pegahsz <pegahsz@uw.edu>
2014-03-01 19:07:31 -0800

    Update and rename image_cut_gel2.pl to image_cut_gel_PEG.pl

--- labw14/group6/full_length_protocols/image_cut_gel_PEG.pl
+++ labw14/group6/full_length_protocols/image_cut_gel_PEG.pl
@@ -6,6 +6,12 @@ produce
   r = 1 "Gel Slice" of "fGA"
 end
 
+produce 
+  q = 1 "Gel Slice" of "fGA"
+end
+
+result = append (r[:id],q[:id])
+
 log
-  return: {Gel_Slice_id: r[:id]}
+  return result
 end
