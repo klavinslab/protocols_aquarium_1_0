c7f14580b36c94f56a44d90dd98b40fc4d9db24a
lavania <chandrashekharla@gmail.com>
2014-03-03 16:28:29 -0800

    Update pcr_prepare.pl
    Changed Volumes

--- labw14/group1/pcr_prepare.pl
+++ labw14/group1/pcr_prepare.pl
@@ -48,7 +48,7 @@ end
 
 step
   description: "Put Reaction. "
-  check: "Pipet 7.5 µL molecular grade water into each of the new labeled PCR tube(%{First} to %{Last})."
+  check: "Pipet 19 µL molecular grade water into each of the new labeled PCR tube(%{First} to %{Last})."
 end
 
 jj = 0
@@ -61,9 +61,9 @@ tube_number = 0
     tube_number = tube_number + 1
     step 
       description: "Prepare reaction for tube %{tube_number}"
-      check: "Pipet 0.5 uL of extracted DNA into tube %{tube_number}."
-      check: "Pipet 1 uL of primer with id %{fwd} into tube %{tube_number}."
-      check: "Pipet 1 uL of primer with id %{rev} into tube %{tube_number}."
+      check: "Pipet 1 uL of extracted DNA into tube %{tube_number}."
+      check: "Pipet 2.5 uL of primer with id %{fwd} into tube %{tube_number}."
+      check: "Pipet 2.5 uL of primer with id %{rev} into tube %{tube_number}."
       check: "Use the tip to gently mix."
       note: "Be careful to pipette into the liquid, not the side of the tube. Always use a new tip."
       
@@ -77,7 +77,7 @@ tube_number = 0
 
 step
   description: "Prepare reaction"
-  check: "Pipet 10 µL of Phusion HF Master Mix with id %{enzyme_id} into each PCR tube (%{First} to %{Last})."
+  check: "Pipet 25 µL of Phusion HF Master Mix with id %{enzyme_id} into each PCR tube (%{First} to %{Last})."
   note: "Use the tip to gently mix after each pipette."
 end
 step
