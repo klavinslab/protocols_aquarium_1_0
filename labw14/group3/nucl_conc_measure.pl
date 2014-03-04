50dbda70b03ad8a45f8fb7656b1616ef8dedc790
Joshua Borgman <joshborgman@comcast.net>
2014-03-03 20:57:14 -0800

    Update nucl_conc_measure.pl

--- labw14/group3/nucl_conc_measure.pl
+++ labw14/group3/nucl_conc_measure.pl
@@ -1,9 +1,11 @@
 argument
-  fragment: sample,"The nucleic acid sample that you want to spec its concentration"
+  fragment1: sample,"The nucleic acid sample that you want to spec its concentration"
+  fragment2: sample,"The second nucleic acid"
 end
 
 take
-  y = item fragment
+  y1 = item fragment1
+  y2 = item fragment2
 end
 
 information "Measure Nucleic Acid Concentration using Nanodrop Spectrophotometer."
@@ -32,18 +34,30 @@ step
 end
 
 step
-  description: "Measure the Sample"
+  description: "Measure the first Sample"
   check: "Open the pedestal"
   check: "Wipe both lower (sensor) pedestal and the upper (lid) pedestal with Kimwipe."
   check: "Add 1.5 µL of your sample with id %{fragment} on the lower pedestal."
   check: "Close the pedestal."
   check: "Click Measure in the software."
+  check: "Write down the concentration"
 end
 
 step
-  description: "Record the concentration shown on the nanodrop computer"
+  description: "Measure the second Sample"
+  check: "Open the pedestal"
+  check: "Wipe both lower (sensor) pedestal and the upper (lid) pedestal with Kimwipe."
+  check: "Add 1.5 µL of your sample with id %{fragment} on the lower pedestal."
+  check: "Close the pedestal."
+  check: "Click Measure in the software."
+  check: "Write down the concentration"
+end
+
+step
+  description: "Record the concentrations shown on the nanodrop computer"
   getdata
-    conc: number,"Write down the number shown on the computer after ng/µL"
+    conc1: number, "Write down the concentration of the second sample"
+    conc2: number, "Write down the concentration of the second sample"
   end
 end
 
@@ -54,7 +68,7 @@ step
 end
 
 log
-  return:{conc: conc}
+  return:{conc_rna1: conc1, conc_rna2: conc2}
 end
 
-release y
+release concat(y1,y2)
