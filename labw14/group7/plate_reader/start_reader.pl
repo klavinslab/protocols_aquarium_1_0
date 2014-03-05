f9f6fdf8aa0b8636980ce058a3552fe8371be350
dstarkebaum <dasman@uw.edu>
2014-03-05 03:56:23 -0800

    Update start_reader.pl

--- labw14/group7/plate_reader/start_reader.pl
+++ labw14/group7/plate_reader/start_reader.pl
@@ -13,17 +13,22 @@ step
 end
 
 step
-  description: "Start up the plate reader"
+  description: "Initialize the program"
   check: "In the intro pop-up menu, open the file 'Lab B Sample Run' (Look in recent files)"
   check: "Immediately go to 'File->Save As...', and save this into a new file name"
   note: "A filename such as 'yyyymmdd MyExperimentName' should be appropriate"
-  check: "Click on 'Protocol->Procedure' to edit the procedure (marked '1' in the screenshot)."
-  check: "Double-click on 'Start Kinetic [Run hh:mm:ss, Interval h:mm:ss]' (marked '3' in the screenshot)."
-  check: "change the run time to 36 hours (marked '1' in the screenshot). A 10min interval is fine.
-          This will clear all previous data"
-  check: "Start the program by clicking the 'Read' button in the 'Procedure' dialog window
-          (marked '2' in the screenshot), or the 'Read' icon at the top of the window
-          (marked '5' in the screenshot)."
+  #Todo: add screenshot
+end
+
+step
+  description: "Start up the plate reader"
+  note: "If the screenshot appears too small, you may right-click on it and select 'view image' to see it larger"
+  check: "Click on 'Protocol->Procedure' to edit the procedure (marked '1')."
+  check: "Double-click on 'Start Kinetic [Run hh:mm:ss, Interval h:mm:ss]' (marked '3')."
+  check: "Change the run time to 36 hours (marked '4'). A 10min interval is fine."
+  bullet: "Note: This will clear all previous data (there should not be any previous data anyway)"
+  check: "Start the program by clicking the 'Read' button in the 'Procedure' dialog window (marked '2'),
+          or the 'Read' icon at the top of the window (marked '5')."
   bullet: "This should produce a pop-up asking your to wait for the bulb to heat up to 37C"
   image: "plate_reader_set_run_time"
 # TODO: check the accuracy of this explanation
