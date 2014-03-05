2705372af7d75b38e93eafca35bf0f24c15b6340
dstarkebaum <dasman@uw.edu>
2014-03-05 04:19:06 -0800

    Update induce_aTc.pl

--- labw14/group7/plate_reader/induce_aTc.pl
+++ labw14/group7/plate_reader/induce_aTc.pl
@@ -15,24 +15,38 @@ end
 #end
 
 step
-  description: "Pause the program and remove the 96-well plate"
+  description: "Pause the program"
   check: "Click the 'Abort read' button on in the 'Biotech Gen5 1.11 program'"
-  check: "Push the black button on the side of the plate reader to open the loading tray"
-  #TODO: details on pausing the program
+  bullet: "This should automatically open the plate reader door.
+          If it does not open, just follow the instructions on the next page"
+  image: "plate_reader_abort"
+end
+
+step
+  description: "Remove the 96-well plate"
+  check: "Push the black button on the side of the plate reader to open the loading tray (marked '1')"
+  image: "plate_reader_open_button"
 end
 
 step
   description: "Add the inducer to the wells indicated in the chart"
-  check: "Pipette 1uL of aTc solution into each well indicated in the plate chart"
+  check: "Pipette 1uL of aTc solution into each well shown in orange in the chart"
+  bullet: "The already induced wells are shown in yellow, the un-induced wells are shown in green"
   note: "Use a new tip each time to avoid contamination"
   image: "%{plate_chart}"
   #image: "induce_2"
 end
 
 step
-  description: "Load the plate back into the reader and resume the program"
+  description: "Load the plate back into the reader"
   check: "Push the black button on the side of the plate reader to close the loading tray"
-  check: "Click the 'Run' button in the 'Biotech Gen5 1.11 program' to resume collecting data"
+  image: "plate_reader_loaded"
+end
+
+step
+  description: "Resume the program"
+  check: "Click the 'Run' button in the 'Biotech Gen5 1.11 program', then click 'okay' to resume collecting data"
+  image: "plate_reader_confirm"
 end
 
 release aTc_inducer
