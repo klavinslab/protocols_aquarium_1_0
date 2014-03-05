fcefed70acf0e162b72f9f7cc2d105af4e0da3f2
Greg Nelson <gln@georgetown.edu>
2014-03-05 11:41:18 -0800

    Update do_gibson.pl

--- labw14/group8/do_gibson.pl
+++ labw14/group8/do_gibson.pl
@@ -3,18 +3,24 @@
 
 argument
   fragments: sample array, "the fragments for the gibsons"
-  gibsons: sample array, "the gibsons to do" 
+  gibsons: sample array, "the gibsons to do"
+  complex_datastructure: sample, "does it ignore type"
     # can i silently produce gibsons with data for the concentrations
     # then use that data to display to user what to do to make them?
     # then destroy / replace the fake items
 end
 
 # show one screen in which all fragments are taken
+step
+  description: "Show complex_datastructure"
+  note: "%{complex_datastructure}"
+end
 
 step
   description: "Take fragments for Gibsons"
   note: "Go to the locations listed to take fragments for all the gibsons.
-         Check old gibson protocols to see if they need to be kept on ice, etc."
+         Check old gibson protocols to see if they need to be kept on ice, etc.
+         %{fragments}"
 end
 
 
