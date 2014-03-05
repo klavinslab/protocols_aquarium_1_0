23ebb4960abfbbabf522983a8fcc55ff1a13fbc5
Greg Nelson <gln@georgetown.edu>
2014-03-05 12:18:13 -0800

    Update do_gibson.pl

--- labw14/group8/do_gibson.pl
+++ labw14/group8/do_gibson.pl
@@ -1,8 +1,16 @@
 #  last step
 # return: {gibsons: g, hash: {egkey: "egvalue"}}
 
+#   pipetting_plan = [
+#         { fragment_name: name, 
+#           fragment_ids: [ ], # fragments to take from
+#           total_amount: total, 
+#           add_to_sample_ids: [ {id: x, amount: in_ul}, ...]
+#         }, ... ] 
+                                            
 argument
-  fragments: sample array, "the fragments for the gibsons"
+  pipetting_plan: sample, "a hash with fragment names and amounts"
+  fragments: sample array, "the fragments for the gibsons (added to cart in last step)"
   gibsons: sample array, "the gibsons to do"
   complex_datastructure: sample, "does it ignore type"
     # can i silently produce gibsons with data for the concentrations
@@ -15,6 +23,7 @@ end
 # instead of holding values in memory / putting them in arrays
 
 # show one screen in which all fragments are taken
+
 step
   description: "Show complex_datastructure"
   note: "%{complex_datastructure}"
@@ -27,6 +36,11 @@ step
          %{fragments}"
 end
 
+foreach f in pipetting_plan
+  take
+    x = 1 f[:fragment_name]
+  end
+end
 
 step
   description: "print out the input gibson array"
