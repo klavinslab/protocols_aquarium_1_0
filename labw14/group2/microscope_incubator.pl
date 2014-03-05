1473395f380f9f8b5298a61fd2c1480c32693975
aquasonal <sonalj@uw.edu>
2014-03-05 13:49:07 -0800

    Update microscope_incubator.pl

--- labw14/group2/microscope_incubator.pl
+++ labw14/group2/microscope_incubator.pl
@@ -7,8 +7,8 @@ end
 step
   description: "Turn on the microscope incubator"
   bullet: "Ask lab staff for access to the biohazard level 2 room"
-  bullet: "Head to the biohazard level 2 room, and turn on the incubator for the microscope"
   bullet: "The room is adjacent to the back door of Prof. Klavins lab"
+  bullet: "Head to the biohazard level 2 room, and turn on the incubator for the microscope"
   note: "The incubator will be labeled with '0' and should be set to 37 degrees"
   image: "incubator_controller"
 end
