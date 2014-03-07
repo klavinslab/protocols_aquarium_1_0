dabeb2f975d2db41da0d1cabba9f4e908c2f5c92
Eric Klavins <klavins@uw.edu>
2014-03-06 20:35:08 -0800

    hash example

--- plankton/sandbox/hash.pl
+++ plankton/sandbox/hash.pl
@@ -1,4 +1,19 @@
+require "plankton/standard.pl"
+
 g = { a: 1, b: "hello" }
 h = { a: 2, c: "goodbye" }
 i = merge(g,h)
 
+print("key(g,:c)",key(g,:c))
+print("g",g)
+print("h",h)
+print("i",i)
+
+q = :a
+
+print("g[q]",g[q])
+print("keys",keys(g))
+
+j = delete(h,:a)
+
+print("j",j)
