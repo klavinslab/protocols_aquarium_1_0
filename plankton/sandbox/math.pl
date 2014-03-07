8a060de1be86fb83d5ae0eb7825f39ae74b0e8e8
Eric Klavins <klavins@uw.edu>
2014-03-07 14:49:52 -0800

    test of math functions

---
+++ plankton/sandbox/math.pl
@@ -0,0 +1,15 @@
+require "plankton/standard.pl"
+
+x = [ sqrt(2), log(2.71), log2(2.71), log10(2.71), cos(1), sin(1), tan(1), acos(1), asin(1), atan(1) ]
+
+print("x",x)
+
+y = [
+  min(1,2),
+  max(1,2),
+  atan2(1,2),
+  floor(1.5),
+  ceil(1.5)
+]
+
+print("y",y)
