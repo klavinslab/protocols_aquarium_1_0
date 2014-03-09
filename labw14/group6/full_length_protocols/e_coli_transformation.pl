8a5cd4ca91ae49a01d61c2b78c4db501d3de6720
pegahsz <pegahsz@uw.edu>
2014-03-08 21:04:17 -0800

    Update e_coli_transformation.pl

--- labw14/group6/full_length_protocols/e_coli_transformation.pl
+++ labw14/group6/full_length_protocols/e_coli_transformation.pl
@@ -109,13 +109,13 @@ step
 end
 
 produce
-  r = 1 "1.5 mL tube transformations" of "pGAA"
+  r = 1 "1.5mL tube transformations" of "pGAA"
   note: "Keep the tube on the bench to use in the next protocol. (no need to edit the location below)"
   location: "Bench"
 end
 
 log
-  return: { 1.5_mL_tube_transformations_id: r[:id] }
+  return: { 1.5mL_tube_transformations_id: r[:id] }
 end
 
 release [electroporator[0]]
