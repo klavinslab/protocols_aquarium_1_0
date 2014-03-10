80f733ecbe56972d4d23adf980e206a5324ad8a9
Greg Nelson <gln@georgetown.edu>
2014-03-10 13:35:11 -0700

    Update read_concentrations_and_take.pl

--- labw14/group8/read_concentrations_and_take.pl
+++ labw14/group8/read_concentrations_and_take.pl
@@ -32,23 +32,50 @@ end
 
 fragments_to_take = unique(fragments_to_take)
 
-# silently produce all the samples here
-# associate their ids
-# with the correct fragments so we get
-# fragment => [{id, amount}]
+#used to give tempids to the gibsons
+letters = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "M", "L", "Q", "R"]
+temp_labels = [["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A15"],
+["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "B13", "B14", "B15"],
+["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15"],
+["E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10", "E11", "E12", "E13", "E14", "E15"],
+["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15"],
+["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "G10", "G11", "G12", "G13", "G14", "G15"],
+["H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15"],
+["I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8", "I9", "I10", "I11", "I12", "I13", "I14", "I15"],
+["J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "J10", "J11", "J12", "J13", "J14", "J15"],
+["K1", "K2", "K3", "K4", "K5", "K6", "K7", "K8", "K9", "K10", "K11", "K12", "K13", "K14", "K15"],
+["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10", "M11", "M12", "M13", "M14", "M15"],
+["L1", "L2", "L3", "L4", "L5", "L6", "L7", "L8", "L9", "L10", "L11", "L12", "L13", "L14", "L15"],
+["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10", "Q11", "Q12", "Q13", "Q14", "Q15"],
+["R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15"]]
+
 
 pipetting_plan = [ ]
 # todo extract these into hash and other libraries
 # NOTE this doesn't work like a hash in other languages
 foreach name in fragments_to_take
   total = 0
+  j=0
+  plasmid_letter_from_to_amounts = [ ]
   foreach p in plasmids_to_make
+    name = p[:plasmid_name_to_make]
+    quantity = p[:quantity_to_make] 
+    
     foreach f in p[:fragment_amounts_in_ul]
       if  f[:name] == name
         total = total + p[:quantity_to_make]*f[:amount]
+        plfta = {
+          plasmid_name: name,
+          letter: letters[j],
+          from: 1,
+          to: quantity,
+          amount: f[:amount]}
+        plasmid_letter_from_to_amounts = append( plasmid_letter_from_to_amounts, plfta)
       end
     end
+    j = j+1
   end
+    
   # get ids from the data file for the fragments to use
   f_ids = [ ]
   foreach f_to_ids in g[:debug][:fragment_name_to_sample_ids]
@@ -57,7 +84,8 @@ foreach name in fragments_to_take
     end
   end
   pipetting_plan = append(pipetting_plan, { fragment_name: name, fragment_ids: f_ids, 
-                                            total_amount: total})
+                                            total_amount: total,
+                                            plasmid_letter_from_to_amounts: plasmid_letter_from_to_amounts})
 end
 
 #foreach p in plasmids_to_make
@@ -90,20 +118,6 @@ end
 # todo give temporary names here such as A1,2,3...
 # B1,2,3.. for another set of gibsons, etc
 # so easier to write on small tube
-letters = [["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A15"],
-["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "B13", "B14", "B15"],
-["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15"],
-["E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10", "E11", "E12", "E13", "E14", "E15"],
-["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15"],
-["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "G10", "G11", "G12", "G13", "G14", "G15"],
-["H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13", "H14", "H15"],
-["I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8", "I9", "I10", "I11", "I12", "I13", "I14", "I15"],
-["J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "J10", "J11", "J12", "J13", "J14", "J15"],
-["K1", "K2", "K3", "K4", "K5", "K6", "K7", "K8", "K9", "K10", "K11", "K12", "K13", "K14", "K15"],
-["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10", "M11", "M12", "M13", "M14", "M15"],
-["L1", "L2", "L3", "L4", "L5", "L6", "L7", "L8", "L9", "L10", "L11", "L12", "L13", "L14", "L15"],
-["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10", "Q11", "Q12", "Q13", "Q14", "Q15"],
-["R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15"]]
 
 
 samples_to_make = [ ]
@@ -121,15 +135,16 @@ foreach p in plasmids_to_make
        data
            fragment_amounts: p[:fragment_amounts_in_ul]
            location: "Bench"
-           temp_label: letters[j][i]
+           temp_label: temp_labels[j][i]
        end
     end
-    i = i + 1
+    
     samples_to_make = append (samples_to_make, r)
     
     # todo refactor to not produce silently here
     planned_gibsons = append(planned_gibsons, 
-                      {temp_label: letters[j][i], fragment_amounts:  p[:fragment_amounts_in_ul]})
+                      {plasmid_name: name, temp_label: temp_labels[j][i], fragment_amounts:  p[:fragment_amounts_in_ul]})
+    i = i + 1
   end
   j = j + 1
 end
