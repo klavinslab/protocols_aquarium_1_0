50be3373b849009b6d37dc9214fb6eaa1c3a1074
Joshua Borgman <joshborgman@comcast.net>
2014-03-10 11:09:18 -0700

    Update rna_isolate_array.pl

--- labw14/group3/rna_isolate_array.pl
+++ labw14/group3/rna_isolate_array.pl
@@ -20,7 +20,7 @@ i = 0
 while i<n
   thisSusp = yeast_susp[i]
   step
-    description: "Transfer 100µL of the Yeast Sample %{thisSusp[:id]} to one 1.5mL Tube"
+    description: "Transfer 100µL of the Yeast Sample %{thisSusp} to one 1.5mL Tube"
     bullet: "Yeast cell concentration should be approximately 10^6 cells/µL"
     note: "Tube content volume is 100 µL."
   end
