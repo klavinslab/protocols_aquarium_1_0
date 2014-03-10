859ea78c4fd231586a252a3c28179353f782651d
Greg Nelson <gln@georgetown.edu>
2014-03-10 12:45:21 -0700

    Update read_concentrations_and_take.pl

--- labw14/group8/read_concentrations_and_take.pl
+++ labw14/group8/read_concentrations_and_take.pl
@@ -99,7 +99,7 @@ foreach p in plasmids_to_make
   while i <= quantity
     temp_id = to_string(i)
     letter = letters[j]
-    temp_id = "%{letter}%{temp_id}"
+    temp_id = to_string("%{letter}%{temp_id}")
     produce silently
        r = 1 "Gibson Reaction Result" of name
        location: "Bench"
