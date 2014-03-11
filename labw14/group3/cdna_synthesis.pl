a6465a2374dc80aa299a8ddaef989a13f6861f64
Joshua Borgman <joshborgman@comcast.net>
2014-03-11 16:58:02 -0700

    Update cdna_synthesis.pl

--- labw14/group3/cdna_synthesis.pl
+++ labw14/group3/cdna_synthesis.pl
@@ -84,10 +84,11 @@ step
 end
 
 return_array = []
-i = 1
-while i < n+1
+i = 0
+while i < n
+  thisStrain = parent_yeast[i]
   produce
-    r = 1 "Yeast cDNA" of parent_yeast[i-1]
+    r = 1 "Yeast cDNA" of thisStrain
     note: "Keep the tubes on the bench to use in the next protocol"
   end
   return_array = append(return_array, r[:id])
