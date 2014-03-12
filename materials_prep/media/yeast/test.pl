0dbdd82f02623e81c7d5fb09ef5153e08898c933
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-03-11 17:23:13 -0700

    Update test.pl

--- materials_prep/media/yeast/test.pl
+++ materials_prep/media/yeast/test.pl
@@ -1,3 +1,4 @@
+volume = 400
 if volume == 800
     bottle_size =  "1 L Bottle"
 elsif volume == 400
@@ -6,4 +7,6 @@ elsif volume == 200
     bottle_size = "250 mL Bottle"
 end
 
-take bottles = n_bottle bottle_size
+take 
+  bottles = n_bottle bottle_size
+end
