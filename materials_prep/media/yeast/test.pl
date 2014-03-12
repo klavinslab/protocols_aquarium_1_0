cac362013e641904cd1582bcaf181b59387d07ad
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-03-11 17:22:00 -0700

    Create test.pl

---
+++ materials_prep/media/yeast/test.pl
@@ -0,0 +1,9 @@
+if volume == 800
+    bottle_size =  "1 L Bottle"
+elsif volume == 400
+    bottle_size =  "500 mL Bottle"
+elsif volume == 200
+    bottle_size = "250 mL Bottle"
+end
+
+take bottles = n_bottle bottle_size
