d6e34bd207ca771dd743981e0b9e48d92d8a2e48
tamimeur <tamimeur@gmail.com>
2014-03-13 08:59:08 -0700

    Create vision_objects.pl

---
+++ tests/vision_objects.pl
@@ -0,0 +1,31 @@
+information "Test vision error finding algorithms."
+
+take
+    bottles = 1 "1 L Bottle"
+end
+
+take
+    lb_liquid = 1 "800 mL LB liquid (sterile)"
+end
+
+take
+    petri = 1 "Small Petri Dish"
+end
+
+take
+    falcon = "50 mL Falcon Tube"
+end
+
+step
+  description: "Add lb_liquid to empty bottle"
+  note: "Pour some of the lb_liquid into the empty bottle"
+  warning: "Do this quickly, do not leave the cap off of the sterile media bottle for too long"
+end
+
+release bottles
+
+release lb_liquid
+
+release petri
+
+release falcon
