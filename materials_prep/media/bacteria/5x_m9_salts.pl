bc48e1f0c7e25badeba2ff97e88279de10738731
David Zong <davidzng@uw.edu>
2014-03-14 11:36:58 -0700

    Update 5x_m9_salts.pl

--- materials_prep/media/bacteria/5x_m9_salts.pl
+++ materials_prep/media/bacteria/5x_m9_salts.pl
@@ -1,31 +1,34 @@
-information "Prepare an 400 mL bottle of M9 Minimal Media with options for carbon source and biotin."
-# Inputs without protocols:
-#   5X M9 Salts @ 160 mL
-#   100X MgSO4 CaCl2 @ 8 mL
-#   Sterile CAS amino acid solution @ 3.2 mL
-#   4% Sterile Filtered Cellobiose @ 80 mL
-#   20% Sterile Filtered Glucose
+information "Prepare 800 mL of 5X M9 Salts to be autoclaved"
 
 take
-  bottle = 1 "500 mL Bottle"
-  salts = 1 "800 mL Difco M9 Minimal Salts Solution, 5X (unsterile)"
-  saltsol = 1 "100X MgSO4 CaCl2"  # TODO: When preparing this, add CaCl2 first
-  water = 1 "DI Water, Sterile"
+  bottle = 1 "1 L Bottle"
+  salts = 1 "Difco M9 Minimal Salts, 5x"
 end
 
-
-# TODO: Input checking a la SDO liquid protocol
 step
-  description: "Add M9 salts"
-  note: "Using a serological pipette and 25 mL tip, add 80 mL of 5X M9 salts to the bottle. To conserve tips, use the same one repeatedly, adding 30 mL 2 times, then finally 20 mL."
-  warning: "Sterile technique is very important for this step."
+  description: "Weigh out M9 Salts"
+  note: "Using place large weigh boat on scale and zero. Weigh out 45.12 g"
+end
+
+step
+  description: "Pour salts into bottle"
+  note: "Fold the boat and carefully pour the salts into the bottle"
+end
+
+step
+  description: "Fill bottle to 800 mL with DI Water"
+  note: "Be sure to stop filling as soon as 800 mL is reached"
+end
+
+step
+  description: "Cap bottle and shake until the salts are dissoved"
 end
 
 
-release [salts[0], saltsol[0], water[0]]
+release [salts[0]]
 
 
 produce
-  m9 = 1 product_label
+  m9 = 1 "800 mL Difco M9 Minimal Salts Solution, 5X (unsterile)"
   release bottle
 end
