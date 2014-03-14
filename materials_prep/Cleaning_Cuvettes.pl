d2928297ae77ff690b88511c29262a9835649955
loganmc <logan.mccoll@gmail.com>
2014-03-13 19:26:49 -0700

    Create Cleaning_Cuvettes.pl

---
+++ materials_prep/Cleaning_Cuvettes.pl
@@ -0,0 +1,63 @@
+information "Clean Cuvettes"
+# FIXME: NEED TO ADD "PRODUCE"
+# Add pictures of broken cuvettes
+
+step
+    description: "obtain dirty 1st use cuvettes"
+    note: "take cuvettes and lids from 1st Use Cuvette container at A8.415"
+    end
+    
+step
+    description: "discard broken cuvettes"
+    note: "Look through the clear side of the cuvettes. If any cracks are visible dispose of in the biohazard waste"
+    
+step
+    description: "rinse cuvettes with DI water"
+    note: "use DI water spray bottle at A8.410 and spray water stream directly into cuvette opening. Empty cuvettes upside down into sink after rinsing"
+    end
+    
+step
+    description: "rinse cuvettes with ethanol"
+    note: "use ethanol spray bottle at A8.410 and spray ethanol stream directly into cuvette opening. Empty cuvettes upside down into sink after rinsing."
+    end
+    
+step
+    description: "Leave cuvettes to dry"
+    note: "place cuvettes upside down in the testube rack at A8.315"
+    end
+    
+step
+    description: "rinse lids"
+    note: "Rinse cuvette lids in sink and leave to dry on clean paper towel beside the test tube rack."
+    end
+    
+step
+    description: "Obtain dirty 2nd use cuvettes"
+    note: "take cuvettes and lids from 2nd Use Cuvette container at A8.415"
+    end
+    
+step
+    description: "discard broken cuvettes"
+    note: "Look through the clear side of the cuvettes. If any cracks are visible dispose of in the biohazard waste"
+    
+step
+    description: "rinse cuvettes with DI water"
+    note: "use DI water spray bottle at A8.410 and spray water stream directly into cuvette opening. Empty cuvettes upside down into sink after rinsing"
+    end
+    
+step
+    description: "rinse cuvettes with ethanol"
+    note: "use ethanol spray bottle at A8.410 and spray ethanol stream directly into cuvette opening. Empty cuvettes upside down into sink after rinsing."
+    end
+    
+step
+    description: "Leave cuvettes to dry"
+    note: "place cuvettes upside down in the testube rack at A8.325 (the leftmost rack)"
+    end
+    
+step
+    description: "rinse lids"
+    note: "Rinse cuvette lids in sink and leave to dry on clean paper towel beside the test tube rack."
+    end
+    
+    
