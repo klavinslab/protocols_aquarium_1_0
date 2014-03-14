3027adc0b2b22ff37d884e076c6b96ed13b4854a
loganmc <logan.mccoll@gmail.com>
2014-03-13 18:34:37 -0700

    Update Cleaning_glassware.pl

--- materials_prep/media/Cleaning_glassware.pl
+++ materials_prep/media/Cleaning_glassware.pl
@@ -43,34 +43,36 @@ end
   else
     product_name = "800 mL Bottle (unsterile)"
   end
- 
-  take
-    bottles = n_bottle bottle_type
-  end
 
  step
       description: "Remove Caps, lids, any autoclave tape, and any labelling tape."
     end
+    
  step
       description: "Rinse bottles once"
       note: "Rinse more than once if solids, e.g agar, remain after first rinse"
     end
+    
   step
        description: "Add dishwashing soap to each bottle"
        note: "Fill each bottle just below halfway with water"
       end
+      
   step
        description: "Use a large brush to scrub around the inside of bottles"
        note: "If the large brush doesn't fit, use the next size down.
        Be sure to brush the area around the neck/mouth of the bottles as well."
       end
+      
   step
        description: "Rinse with water 4-5 times as needed after scrubbing"
        note: "if soap bubbles remain rinse as needed-dependent on amount of soap used and size of bottle – smaller bottles may require less rinses."
       end
+      
   step
        description: "Hang on hooks to dry (A8.410)"
        end
+       
   step
        description: "Scrub inside of bottle caps/lids"
        note: "Use a small amount of soap, use the same brush as for the glassware, and rinse 2-3 times.
