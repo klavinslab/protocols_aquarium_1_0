54a0f25cd66db81157745d580a4695a5fdaad593
Greg Nelson <gln@georgetown.edu>
2014-03-15 20:30:21 -0700

    Update test_hash_assignment.pl

--- labw14/group8/test_hash_assignment.pl
+++ labw14/group8/test_hash_assignment.pl
@@ -42,3 +42,24 @@ step
          and from hash
          %{result}"
 end
+
+ha[a] = "ha[a]"
+ha["testkey"] = "ha['testkey']"
+ha[:testkey] = "ha[:testkey]"
+from_string = ha["testkey"]
+from_symbol = ha[:testkey]
+from_var = ha[a]
+
+result = { from_symbol: ha[:testkey],
+           from_var   : ha[a],
+           from_string:  ha["testkey"]}
+
+step
+  description: "Maybe says assigning to a string variable works this way"
+  note: "from_symbol %{from_symbol} 
+         from_var %{from_var} 
+         from_string %{from_string}
+         and from hash
+         %{result}"
+end
+
