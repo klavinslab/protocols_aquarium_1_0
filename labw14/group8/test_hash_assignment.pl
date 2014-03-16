954d1f0e9a117a320611a69af99fc6014e94fb6a
Greg Nelson <gln@georgetown.edu>
2014-03-15 20:22:18 -0700

    Update test_hash_assignment.pl

--- labw14/group8/test_hash_assignment.pl
+++ labw14/group8/test_hash_assignment.pl
@@ -6,12 +6,30 @@ step
 end
 
 # behavior for assigning to a string variable
-ha[a] = "yes"
-from_symbol = ha[:testkey]
+ha[a] = "ha[a]"
+symbols_and_strings_overlap = ha[:testkey]
 from_var = ha[a]
 from_string = ha["testkey"]
 
-result = { from_symbol: ha[:testkey],
+result = { symbols_and_strings_overlap: ha[:testkey],
+           from_var   : ha[a],
+           from_string:  ha["testkey"]}
+
+step
+  description: "Maybe says assigning to a string variable works this way"
+  note: "from_symbol %{from_symbol} 
+         from_var %{from_var} 
+         from_string %{from_string}
+         and from hash
+         %{result}"
+         
+ha[a] = "ha[a]"
+symbols_and_strings_overlap = ha[:testkey]
+from_var = ha[a]
+ha["testkey"] = "ha['testkey']"
+from_string = ha["testkey"]
+
+result = { symbols_and_strings_overlap: ha[:testkey],
            from_var   : ha[a],
            from_string:  ha["testkey"]}
 
