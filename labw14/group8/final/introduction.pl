8fca937b6e94036ad15411e9d2e3190032b3d54b
Greg Nelson <gln@georgetown.edu>
2014-03-15 19:33:45 -0700

    Create introduction.pl

---
+++ labw14/group8/final/introduction.pl
@@ -0,0 +1,31 @@
+step
+  description: "This metacol guides you through performing multiple Gibsons"
+  note: "You will combine several fragments with a larger plasmid (serving as a backbone). 
+    You will then add Gibson master mix, which has x y z enzymes, which work in concert to 
+    trim back, allow the fragments to join over overlapping regions (homologies), and then
+    be stiched back together (add an image here from original paper). 
+    This technique was discoverd in 20xx? and allows you to do in an hour what used to take
+    x days.
+    The original paper is at http..."
+end
+
+step
+  description: "The steps are as follows:"
+  bullet: "Take the fragments and plasmid backbones"
+  bullet: "Label and fill PCR tubes with water, then fragements"
+  warning: "You must check that the thermocycler is available first, since exonuclease in master mix 
+            begins eating fragments immediately"
+  bullet: "Add the master mix, then put them in the thermocycler for X minutes 
+  
+  bullet: "Transform a bit of the resulting plasmid into E. Coli (to magnify it to make stock and test the transform successful)"
+  bullet: "Plate the transformed cells overnight"
+  bullet: "Image the plates to check the colony counts (measure of success) and retry failed Gibsons"
+  bullet: "Calculate equimolar amounts to make a more likely succesful Gibson for failed ones"
+  bullet: "Do another Gibson with the amounts you calculated. Transform and plate overnight"
+  bullet: "For Gibsons that still didn't work, the person who wanted them done will try to troubleshoot what went wrong.
+           Sometimes they don't work without any fault on your part, especially with more fragments."
+end
+
+step
+  description: "You may now start doing some Gibsons. Have fun!"
+end
