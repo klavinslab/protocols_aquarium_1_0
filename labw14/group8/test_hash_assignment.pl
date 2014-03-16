a2f719e12fc37ad6f458497df2df097a1765fb8a
Greg Nelson <gln@georgetown.edu>
2014-03-15 20:08:56 -0700

    Update test_hash_assignment.pl

--- labw14/group8/test_hash_assignment.pl
+++ labw14/group8/test_hash_assignment.pl
@@ -5,8 +5,16 @@ step
   description: "Demonstrate hash assignment"
 end
 
-ha[a] = "again"
-x = ha[:a]
+ha[a] = "assigning to a string variable works"
+x = ha[:hello]
+
+step
+  description: "What values does x have?"
+  note: "%{x}"
+end
+
+ha[:hello] = "reading from a string variable works"
+x = ha[a]
 
 step
   description: "What values does x have?"
