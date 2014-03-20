dd2239da60b3d62f15af4b1fcb489ded694b7c42
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-03-20 16:27:53 -0700

    dilution now rounds

--- labw14/group4/make_log_phase.pl
+++ labw14/group4/make_log_phase.pl
@@ -8,8 +8,9 @@ information "Dilute cells and bring them to log phase"
 dilution = 1.0/71
 total_V = 18 # ml -- too high?
 
-e_V = (total_V*dilution) * 1000 # ul;
-LB_V = (total_V-e_V)  # ml
+e_V_ml = (total_V*dilution)  # ml;
+e_V_micro = ceil(e_V_ml * 1000) # uL
+LB_V = floor( (total_V-e_V_ml) * 10) / 10.0 # ml
 
 sample_count = length(unique_e_coli_strains)
 net_volume = sample_count *  total_V
@@ -32,9 +33,9 @@ while i < sample_count
 	i = i + 1
 	step
 		description: "Dilute E. coli cells from sample %{e}"
-		check: "Add %{LB_V} ml of LB media to test tube %{i} (round to the nearest .1 ml)" # -- large pipette?
-		check: "Add %{e_V} μl of e. coli sample %{e} to test tube %{i}."
-		bullet: "You should end up with %{total_V} ml of liquid in the test tube"
+		check: "Add %{LB_V} ml of LB media to test tube %{i}" # -- large pipette?
+		check: "Add %{e_V_micro} μl of e. coli sample %{e} to test tube %{i}"
+		bullet: "You should end up with approximately %{total_V} ml of liquid in the test tube"
 	end
 end
 
