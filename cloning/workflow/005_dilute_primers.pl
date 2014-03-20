dae43152ae72b9c709e367f808a57215e66f5cc6
David Younger <dyounger@gmail.com>
2014-03-20 15:56:29 -0700

    Update 005_dilute_primers.pl

--- cloning/workflow/005_dilute_primers.pl
+++ cloning/workflow/005_dilute_primers.pl
@@ -1,1 +1,35 @@
-dilute primers
+argument
+  primer_ids: sample("Primer") array, "Enter the primer IDs that you wish to make a diluted working stock of"
+end
+
+take
+    primer_stock = item unique(primer_ids)
+end
+len=length(primer_ids)
+
+step
+  description: "Label sterile Eppendorf tubes"
+  check: "Grab %{len} eppendorf tubes"
+  check: "Put a circular label sticker on the cap of each tube."
+end
+
+
+
+
+
+x=0
+while x < y
+  produce
+    q = 1 "PCR Result" of fragment_names[x]
+    location: "Thermocycler"
+  end
+  x = x+1
+end
+
+
+
+
+
+
+
+release primer_stock
