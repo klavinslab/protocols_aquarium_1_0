f6ca5bdfa9f743fdb1cdfdc1d3a7bf7798c9ba16
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-03-20 15:22:32 -0700

    Update test.pl

--- labw14/group4/test.pl
+++ labw14/group4/test.pl
@@ -1,13 +1,15 @@
-
 dilution = 1.0/71
 total_V = 18 # ml -- too high?
 
 e_V = total_V*dilution # ml;
 LB_V = (total_V-e_V) # ml
 
+e_V_micro = e_V * 1000 # in uL
+
 step
   description: "Hello"
   note: "dilution = %{dilution}"
   note: "e_V = %{e_V}"
+  note: "e_V_micro = %{e_V_micro}"
   note: "LB_v = %{LB_V}"
 end
