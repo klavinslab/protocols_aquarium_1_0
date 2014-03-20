ac89da25f3a66217b816b272874e6a34ffb1a3f0
horsetailfiddlehead <horsetailfiddlehead@users.noreply.github.com>
2014-03-20 15:16:51 -0700

    testing float rounding

--- labw14/group4/test.pl
+++ labw14/group4/test.pl
@@ -1,3 +1,13 @@
+
+dilution = 1.0/71
+total_V = 18 # ml -- too high?
+
+e_V = total_V*dilution # ml;
+LB_V = (total_V-e_V) # ml
+
 step
   description: "Hello"
+  note: "dilution = %{dilution}"
+  note: "e_V = %{e_V}"
+  note: "LB_v = %{LB_V}"
 end
