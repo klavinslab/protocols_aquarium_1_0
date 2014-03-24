ed6b97b3b022bc94ff81ff60f1110b28687d2c06
MilesGander <gandermiles@gmail.com>
2014-03-23 20:38:41 -0700

    Update gel_purify.pl

--- cloning/gel/gel_purify.pl
+++ cloning/gel/gel_purify.pl
@@ -53,9 +53,11 @@ count3=0
 while count3 < y
   label=count3+1
   qg=weights[count3]*3000
+  qg=floor(qg)
   step
     description: "Add %{qg} µl of QG buffer into tube %{label}"
   end
+  count3=count3+1
 end
   
 
