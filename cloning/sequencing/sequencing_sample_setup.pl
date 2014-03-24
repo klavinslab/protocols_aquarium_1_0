effd91faffff4616f4396852101e48c97da82c0f
MilesGander <gandermiles@gmail.com>
2014-03-24 11:51:44 -0700

    Update sequencing_sample_setup.pl

--- cloning/sequencing/sequencing_sample_setup.pl
+++ cloning/sequencing/sequencing_sample_setup.pl
@@ -1,2 +1,15 @@
 #this protocol describes how to set up a sequencing reaction to send to genewiz
 
+
+argument 
+  plasmids: sample("Plasmid") array, "Enter the plasmids you wish to seqeunce"
+  concentrations: number array, "Enter the concentration of the above plasmid samples in ng/µl"
+end
+
+y=length(plasmids)
+
+step
+  getdata
+    
+  end
+end
