72020a0e9208f3b7cbb5f643cb493d4edb828e2d
Greg Nelson <gln@georgetown.edu>
2014-03-24 10:22:55 -0700

    Update introduction.pl

--- labw14/group8/final/introduction.pl
+++ labw14/group8/final/introduction.pl
@@ -4,9 +4,11 @@ step
     a larger plasmid (serving as a backbone). 
     You will then add Gibson master mix, which has multiple enzymes that work in concert to 
     trim back, allow the fragments to join over overlapping regions (homologies), and then
-    be stiched back together (add an image here from original paper). 
+    be stiched back together. 
     This technique was published in 2009."
 end
+
+#todo add image from original paper
 
 step
   description: "The steps are as follows:"
@@ -20,12 +22,13 @@ step
   bullet: "Transform a bit of the resulting plasmid into E. Coli"
   bullet: "Plate the transformed cells overnight"
   bullet: "Day 2"
-  bullet: "Image the plates to check the colony counts (measure of success) and retry failed Gibsons"
-  bullet: "Calculate equimolar amounts to make a more likely succesful Gibson for failed ones"
-  bullet: "Do another Gibson with the amounts you calculated. Transform and plate overnight (Day 2, Day3), then image"
-  bullet: "For Gibsons that still didn't work, the person who wanted them done will try to troubleshoot what went wrong.
+  bullet: "Image the plates to check the colony counts (measure of success)"
+  bullet: "For Gibsons that didn't work, the person who wanted them done will try to troubleshoot what went wrong.
            Sometimes they don't work without any fault on your part, especially with more fragments."
 end
+
+#bullet: "Calculate equimolar amounts to make a more likely succesful Gibson for failed ones"
+#bullet: "Do another Gibson with the amounts you calculated. Transform and plate overnight (Day 2, Day3), then image"
 
 step
   description: "You may now start doing some Gibsons. Have fun!"
