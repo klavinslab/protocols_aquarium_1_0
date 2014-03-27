309fc08da2ee79dc5fefc99a5c0cf726a4e30092
David Younger <dyounger@gmail.com>
2014-03-27 11:47:31 -0700

    Update produce_take_modify.pl

--- tests/errors/produce_take_modify.pl
+++ tests/errors/produce_take_modify.pl
@@ -1,5 +1,5 @@
 argument
-  primer_ids: sample("Primer") array, "Enter the primer IDs that you wish to make a diluted working stock of"
+  primer_ids: sample("Primer") array, "Enter primer stocks"
 end
 take
     primer_stock = item unique(primer_ids)
@@ -18,32 +18,6 @@ while x < len
   x = x+1
 end
 
-step
-  description: "Prepare sterile Eppendorf tubes"
-  check: "Grab %{len} eppendorf tubes"
-  check: "Put a circular label sticker on the cap of each tube."
-  check: "Pipette 90ul of molecular grade water into each tube."
-end
-
-
-x=0
-while x < len
-  y=newid[x]
-  z=primer_stock[x][:id]
-  step
-    description: "Label a tube and add primer to make a diluted stock"
-    check: "Grab an unlabeled tube"
-    check: "Label the tube with the ID number %{y}"
-    check: "Pipette 10ul from the tube with ID %{z} into the newly labeled tube with ID %{y}"
-  end
-  x=x+1
-end
-
-step
-  description: "Ignore the following take"
-  check: "primer_stock: %{primer_stock}, newid: %{newid}, newitem: %{newitem}"
-end
-
 x=0
 while x < len
   y=newid[x]
@@ -52,10 +26,6 @@ while x < len
       inuse: 1
   end
   x=x+1
-end
-
-step
-  description: "Do NOT ignore the following release"
 end
 
 release a
