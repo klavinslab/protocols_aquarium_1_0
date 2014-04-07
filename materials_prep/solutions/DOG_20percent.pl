62939dac3ea1da19351d6b8cd8e4a1b7f9d40f3f
Chris Takahashi <cnt@uw.edu>
2014-04-07 16:17:37 -0700

    new DOG stock solutionprotocol

---
+++ materials_prep/solutions/DOG_20percent.pl
@@ -0,0 +1,43 @@
+information "Prepare a 20%% (g / mL) filter-sterilized solution of 2-deoxy-D-galactose (DOG)"
+
+
+take
+  tube = 1 "50 mL Falcon Tube"
+  dog = 1 "2-Deoxy-D-galactose"
+end
+
+
+percentage = 20
+ml = 5
+
+
+step
+  description: "Transfer the full 1g of deoxy-galactose to the falcon tube."
+  note: "Open the cap and dump the contents of the 1g container into the falcon tube."
+end
+
+step
+  description: "Get the remaining powder out of the container."
+  note: "Using the P-1000, aspirate 1ml of distilled or molecular grade water (B15.530) and dispense into the empty deoxy-galactose bottle.  Mix gently and then transfer the liquid to the falcon tube.  Thow out the empty deoxy-galactose bottle when you're done."
+end
+
+step
+  description: "Fill falcon tube to %{ml} mL mark with dI water."
+  note: "Fill the falcon tube up to the %{ml} mL mark with deionized water from jug J1 at B15.530."
+end
+
+
+include "includes/materials_prep/filter_sterilize_syringe.pl"
+  volume: ml
+  name: "the DOG solution"
+end
+
+
+produce
+  dextrose_solution = 1 "20%% DOG Solution (sterile)"
+  release tube
+  release dog
+  note: "Write 20%% DOG Solution (sterile) and the date on the label in addition to the id number."
+end
+
+
