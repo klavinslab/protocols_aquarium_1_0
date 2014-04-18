83f80dc81df0add32f2d3fb1d74a5f381cbec337
Eric Klavins <klavins@uw.edu>
2014-04-18 12:44:36 -0700

    example of new quantity behavior

---
+++ plankton/sandbox/sample_quantity.pl
@@ -0,0 +1,8 @@
+q0 = quantity("Primer Aliquot")
+q1 = quantity({object: "Primer Aliquot", sample: "lacI_h2"})
+
+step
+  description: "Quantities"
+  note: "%{q0} Primer Aliquots total"
+  note: "%{q1} Primer Aliquots of the primer lacI_h2"
+end 
