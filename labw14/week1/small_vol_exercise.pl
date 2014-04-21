5bb1dd065f09681afc0fbd08773c28f1924f2901
tamimeur <tamimeur@gmail.com>
2014-04-21 01:20:18 -0700

    Update and rename small_vol_exercise.pdl to small_vol_exercise.pl

---
+++ labw14/week1/small_vol_exercise.pl
@@ -0,0 +1,101 @@
+
+information: "This protocol is for practicing pipetting small volumes."
+
+step
+  description: "Label the Tubes."
+  note:
+  "Take out 3 1.5 mL tubes, close the caps, and label them C - E on the top of each cap."
+  warning: "Be careful to always use a sterile technique when handling the tubes."
+  image: "labeled_tubes_small_volume_exercise"
+end
+
+step
+  description: "A note about pipetting small volumes:"
+  note:
+  "When pipetting a small volume of liquid into a tube, place the end of the pipette tip on the side of the tube
+  that you are pipetting in, so that when you release the liquid, it sticks to the side of the tube as a drop."
+  image: "how_to_pipette_small_volume_small_volume_exercise"
+end
+
+step
+  description: "Add Solution I to Tube C."
+  note:
+  "Add 4 µL of Solution I to the tube labeled C, making sure to open and close the caps of the tube and test
+  tube by using a sterile technique. Be sure to use the correct pipette and set it to 4 µL before adding a tip."
+  image: "solution_I_small_volume_exercise"
+end
+
+step
+  description: "Add Solution I to Tube D."
+  note:
+  "Using a fresh tip and maintaining a sterile technique, add 4 µL of Solution I to Tube D."
+  image: "solution_I_small_volume_exercise"
+end
+
+step
+  description: "Add Solution I to Tube E."
+  note:
+  "Using a fresh tip and maintaining a sterile technique, add 4 µL of Solution I to Tube E."
+  image: "solution_I_small_volume_exercise"
+end
+
+step
+  description: "Add Solution II to Tube C."
+  note:
+  "Using a fresh tip and maintaining a sterile technique, add 5 µL of Solution II to Tube C. Be sure to change
+  the volume of the pipette before adding a new tip."
+  image: "solution_II_small_volume_exercise"
+end
+
+step
+  description: "Add Solution II to Tube D."
+  note:
+  "Using a fresh tip and maintaining a sterile technique, add 5 µL of Solution II to Tube D."
+  image: "solution_II_small_volume_exercise"
+end
+
+step
+  description: "Add Solution II to Tube E."
+  note:
+  "Using a fresh tip and maintaining a sterile technique, add 4 µL of Solution II to Tube E. Be sure to change
+  the volume of the pipette before adding a new tip."
+  image: "solution_II_small_volume_exercise"
+end
+
+step
+  description: "Add Solution III to Tube C."
+  note:
+  "Using a fresh tip and maintaining a sterile technique, add 1 µL of Solution III to Tube C. Be sure to change
+  the volume of the pipette before adding a new tip."
+  image: "solution_III_small_volume_exercise"
+end
+
+step
+  description: "Add Solution III to Tube E."
+  note:
+  "Using a fresh tip and maintaining a sterile technique, add 1 µL of Solution III to Tube E."
+  image: "solution_III_small_volume_exercise"
+end
+
+step
+  description: "Add Solution IV to Tube D."
+  note:
+  "Using a fresh tip and maintaining a sterile technique, add 1 µL of Solution IV to Tube D."
+  image: "solution_IV_small_volume_exercise"
+end
+
+step
+  description: "Add Solution IV to Tube E."
+  note:
+  "Using a fresh tip and maintaining a sterile technique, add 1 µL of Solution IV to Tube E."
+  image: "solution_IV_small_volume_exercise"
+end
+
+step
+  description: "Tubes C, D, and E should each contain 10 µL of liquid."
+  note:
+  "These are the total volumes. Make sure to centrifuge down the liquid in each tube by using the table-top centrifuge."
+  warning: "Be sure to balance the centrifuge!"
+  image: "10_mL_total_small_volume_exercise"
+end
+
