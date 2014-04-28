d42033b164338b95ffcf5a3d7c61e161ba64c3b0
jumroon <jumroon.rada@gmail.com>
2014-04-28 16:52:51 -0700

    Create Mitrotiter_plate_biofilm3.pl

---
+++ assays/Mitrotiter_plate_biofilm3.pl
@@ -0,0 +1,13 @@
+##third part of microtiter plate assay
+
+information "third part of the microtiter biofilm plate assay"
+
+take
+  solvent = 1 "bottle of 80% ethanol 20% acetone"
+  CV = 1 "small bottler of .1% cv in water"
+  media = 1 "bottle of M9" 
+  tray = 4 "empty large pipette tip boxes"
+end
+
+step
+  description: "
