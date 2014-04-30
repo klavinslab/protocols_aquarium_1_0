bdd37e1e35f068c7325fadb59e137f7e1e6e20e5
tamimeur <tamimeur@gmail.com>
2014-04-30 16:32:06 -0700

    Update 052_plate.pl

--- cloning/workflow/052_plate.pl
+++ cloning/workflow/052_plate.pl
@@ -10,7 +10,7 @@ take
   #strains = item plasmid_ids
   #strains = item 25713
   plates = n plate_type
-  note: "Do not take plate from top of the stack if there is a date stick on it, take from the middle of the stack."
+  #note: "Do not take plate from top of the stack if there is a date stick on it, take from the middle of the stack."
 end
 
 step
