e6c2f145f0f681f1358da8878527df69e4045835
tamimeur <tamimeur@gmail.com>
2014-04-30 16:32:55 -0700

    Update 052_plate.pl

--- cloning/workflow/052_plate.pl
+++ cloning/workflow/052_plate.pl
@@ -7,8 +7,7 @@ end
 n = length(plasmid_ids)
 
 take
-  #strains = item plasmid_ids
-  #strains = item 25713
+  strains = item plasmid_ids
   plates = n plate_type
   #note: "Do not take plate from top of the stack if there is a date stick on it, take from the middle of the stack."
 end
