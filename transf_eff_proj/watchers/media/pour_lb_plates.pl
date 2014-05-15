73c2f100f333fe89b399da6aa7d66a692b0902a8
tamimeur <tamimeur@gmail.com>
2014-05-14 17:19:35 -0700

    Update pour_lb_plates.pl

--- transf_eff_proj/watchers/media/pour_lb_plates.pl
+++ transf_eff_proj/watchers/media/pour_lb_plates.pl
@@ -236,13 +236,13 @@ if antibiotic == "None"
   produce
     n_poured product_name
     release poured_plates
-    location: "B1.110"
+    location: "A1.110"
   end
 else
   produce
     n_poured product_name
     release poured_plates
-    location: "B1.110.R1.300"
+    location: "A1.110"
   end
   release antibiotic_aliquots
 end
