2b2ac46aca6f6ed9dc8206ec74dcdfdf70252f25
Eric Klavins <klavins@uw.edu>
2014-06-01 07:44:57 -0700

    collection test

--- plankton/sandbox/collection.pl
+++ plankton/sandbox/collection.pl
@@ -1,6 +1,25 @@
-c = collection({ rows: 8, columns: 12})
+take
+  x = item 3689
+end
+
+produce silently
+  y = 1 "Stripwell"
+  data
+    matrix: col_new_matrix(4,4)
+  end
+end
+
+source = x[0][:id]
+dest = y[:id]
+
+col_set(dest,2,2,1)
+
+t = col_transfer ( [ source ], [ dest ] )
 
 step
-  description: "Results"
-  note: "c = %{c}"
-end 
+  table: t
+  table: col_get_matrix(dest)
+  note: "dest(1,1) = " + to_string(col_get(dest,1,1))
+end
+
+release x
