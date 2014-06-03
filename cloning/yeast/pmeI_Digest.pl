8dd1882c23867952d2af1ecb31d174593f61bf11
MilesGander <gandermiles@gmail.com>
2014-06-03 11:21:23 -0700

    Update pmeI_Digest.pl

--- cloning/yeast/pmeI_Digest.pl
+++ cloning/yeast/pmeI_Digest.pl
@@ -1,2 +1,14 @@
 ##This protocol creates 50µl digests of yeast vectors to be integrated via transformation
 
+argument
+  plasmids: sample("Plasmid") array, "Plasmids"
+  Plasmids_conc: number array, "Plasmid DNA Concentrations in ng/µl"
+end
+
+take
+  
+end
+
+step
+  
+end
