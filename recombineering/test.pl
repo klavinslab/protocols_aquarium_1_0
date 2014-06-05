114a3b4c715950ce2fd67d72d328cee89ddd6bd3
Chris Takahashi <cnt@uw.edu>
2014-06-05 15:27:24 -0700

    test

--- recombineering/test.pl
+++ recombineering/test.pl
@@ -1,7 +1,7 @@
 json_file = "recombineering/test.json"
 
 input
-  p = "plankton/sandbox/parameters.json" #"recombineering/test.json"# json_file
+  p = "protocols/plankton/sandbox/parameters.json" #"recombineering/test.json"# json_file
 end
 
 step
