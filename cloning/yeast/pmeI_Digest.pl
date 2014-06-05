ed12f9cae961dd9cfdf59d338b768b1946a3bb59
MilesGander <gandermiles@gmail.com>
2014-06-05 11:15:52 -0700

    Update pmeI_Digest.pl

--- cloning/yeast/pmeI_Digest.pl
+++ cloning/yeast/pmeI_Digest.pl
@@ -3,12 +3,22 @@
 argument
   plasmids: sample("Plasmid") array, "Plasmids"
   Plasmids_conc: number array, "Plasmid DNA Concentrations in ng/µl"
+  pmei: sample("Enzyme"), "Tube of PMEI to be used."
+  bsa: sample("Enzyme Buffer Stock"), "Tube of BSA to be used"
+  NEB4: sample("Enzyme Buffer Stock"), "Tube of NEB Buffer 4 to be used."
 end
 
 take
+  bsa_stock = item bsa
   
 end
 
+step
+  description: "In the following step you will take PMEI enzyme out of the freezer. Make sure the enzyme is kept on ice for the duration of the protocol."
+end
+
+take
+end
 step
   
 end
