742815e12b5bf6e93cc1bf3e590a1d7720eb03dc
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-06-05 15:04:52 -0700

    Update dilute_culture.pl

--- cloning/yeast/dilute_culture.pl
+++ cloning/yeast/dilute_culture.pl
@@ -1,2 +1,20 @@
 information "Dilute overnight culcutre to grow cells into log phase for testing purposes"
 
+argument
+  yeast_overnight_id: sample, "Choose the plate you intended to do overnight suspension"
+end
+
+take
+  yeast_overnight = item yeast_overnight_id
+  yeast_media = 1 "50 mL SC liquid aliquot (sterile)"
+  test_tube = 1 "14 mL Test Tube"
+end
+
+release [yeast_overnight[0],yeast_media[0]]
+
+produce
+  y = 1 "Yeast Overnight Suspension" from yeast_plate[0]
+  note: "Place the test tube in 30 C incubator shaker at B13.125"
+  location: "B13.125"
+  release test_tube
+end
