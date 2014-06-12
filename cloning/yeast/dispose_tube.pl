767b470bbcc5394df87bfebe75c6f629926a8d45
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-06-12 15:45:37 -0700

    Create dispose_tube.pl

---
+++ cloning/yeast/dispose_tube.pl
@@ -0,0 +1,11 @@
+argument
+  yeast_tube_id: sample, "Choose the tube you intended to do dispose"
+end
+
+take
+  yeast_tube = item yeast_tube_id
+step
+  description: "Choose to dispose the tube in the release step"
+end
+
+release yeast_tube
