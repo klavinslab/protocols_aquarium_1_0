aa39127ab28749cccfa4ac2171e5d09ec3b446c9
David Younger <dyounger@gmail.com>
2014-06-19 15:05:32 -0700

    Update ecoli_comp_cells_day1.pl

--- cloning/ecoli_comp_cells_day1.pl
+++ cloning/ecoli_comp_cells_day1.pl
@@ -1,33 +1,35 @@
 information "Set up for making e.coli electrocompetent cells tomorrow."
 
 argument
-  e_coli_strain_type: object, "The electrocompetent E. coli aliquot type."
+  e_coli_id: sample, "The E. coli glycerol stock that you want to make competent cells from."
+  e_coli_strain: string, "The E. coli strain name that you are making competent cells of"
 end
 
 take 
-  2 250mL flasks
-  LB
+  flask = 2 "250 mL Baffled Flask"
+  LB = 1 "800 mL LB liquid (sterile)"
 end
 
 step
-  Label each flask with the strain name
+  description: "Label both flasks with the strain name: %{e_coli_strain}."
 end
 
 step
-  Add 50mL LB broth into each of the 2 250mL flasks using a serological pipette.
+  description: "Add 50mL LB broth into each of the 2 250mL flasks using a serological pipette."
 end
-
 
 take
-  strain = n e_coli_strain_type
-
-
-
-
-step
-  Innoculate each flask with cells from a glycerol stock
+  ecoli = item e_coli_id
 end
 
 step
-  Put both flasks into the 37 degree shaker incubator
+  description: "Using a 100ul pipette tip, scrape some cells out of the glycerol stock and add them to the first flask"
 end
+
+release ecoli
+
+step
+  description: "Put both flasks into the 37 degree shaker incubator"
+end
+
+release concat(flask,LB)
