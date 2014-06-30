1c1e1b8998ea3bfc3cd248041a87e6e3fb53ea62
MilesGander <gandermiles@gmail.com>
2014-06-30 16:22:52 -0700

    Update Sterile_DIH20.pl

--- cloning/Sterile_DIH20.pl
+++ cloning/Sterile_DIH20.pl
@@ -1,1 +1,29 @@
-##This protocol makes 800mls sterile DI water
+##This protocol makes 800ml of sterile DI water
+
+
+
+step
+  description: "This protocol makes 800ml of sterile DI water"
+end
+
+take
+  bottle = 1 "1 L Bottle"
+end
+
+step
+  description:"Fill the 1 L bottle with 800ml of DI water."
+end
+
+step
+  description: "Place autoclave tape on the bottle"
+end
+
+step
+  description: "Place bottle in autoclave and run the 15 minute and 121ºC cycle"
+end
+
+produce
+  r = 1 "800ml Sterile DI-H2O" 
+  location: "Bench"
+  release bottle
+end
