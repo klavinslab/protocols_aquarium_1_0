45be20304487527bd9091b554053e527b9faeb1e
loganmc <logan.mccoll@gmail.com>
2014-07-01 07:40:48 -0700

    Update Yeast_Frozen_CompCells.pl

--- culturing/Yeast_Frozen_CompCells.pl
+++ culturing/Yeast_Frozen_CompCells.pl
@@ -25,48 +25,48 @@ step
   note: "Dilute the culture using a 1:50 or 1:100 ratio with 50mL of prewarmed YPAD"
 end
 
-step: 
+step
   description: "Put diluted cells into shaking incubator"
   note: "Wait 5-6 hours (1:50 dilution) or 6.5-8 hours (1:100 dilution) before harvesting the cells growing in the incubator"
 end
 
-step:
+step
   description: "Spin down cells"
   note: "transfer contents of baffled flask(s) to 50mL or 250mL falcon tubes."
   note: "Spin down in large centrifuge at 3,000xg for 5 minutes at 20 degrees."
 end
 
-step:
+step
   description: "Pour out the supernatant"
   note: "Make sure to not disturb the pellet. After pouring out the supernatant add 1ml of molecular grade water for every 50mL of culture originally added to the tube. Resuspend by vortexing."
 end
 
-step:
+step
   description: "Transfer ~1.2mL of resuspended cells to epinedorf tubes."
   note: "Try to distribute cells equally among 6 or fewer 1.5mL epinedorf tubes."
 end
 
-step:
+step
   description: "Spin down epinedorf tubes on tabletop microfuge"
   note: "Make sure the microfuge is balanced. Spine down for 10-20 seconds and remove supernatant from resulting pellet using a 1000uL pipette."
 end
   
-step:
+step
   description: "Add 1mL of molecular grade water to tubes"
   note: "resuspend by vortexing, then again spin down the tubes in the microfuge and remove the supernatant."
 end
 
-step:
+step
   description: "Resuspend the cells by vortexing the pellet(s) with Frozen Compotent Cell Solution (FCC Solution)"
   note: "Add 4 parts of FCC for every one part of cell volume in the epinedorf tubes."
 end
 
-step:
+step
   description: "Create yeast cell aliquots"
   note: "Label 1.5 mL epinedorf tubes with the yeast strain identifier. Add 50uL of the resuspended cell and FCC solution to each tube."
 end
 
-step:
+step
   description: "Freeze aliquots in M.80"
   note: "Place yeast strain aliquots in the appropriate box in the M.80 fridge"
 end
