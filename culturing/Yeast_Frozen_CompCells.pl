006839b3ccd083e726868347238c5a2d9c04f9c3
loganmc <logan.mccoll@gmail.com>
2014-07-01 07:20:34 -0700

    Update and rename Yeast_Frozen_CompCells to Yeast_Frozen_CompCells.pl

--- culturing/Yeast_Frozen_CompCells.pl
+++ culturing/Yeast_Frozen_CompCells.pl
@@ -16,8 +16,31 @@ end
 step
   description: "Grow up culture"
   note: "Label the test tube with the yeast strain ID, your initials, and today's date."
-  note: "Place the test tube in the 30 degree rotary shaker to grow up overnight."
+  note: "Place the test tube in the 30 degree shaking incubator to grow up overnight."
 end 
 
 step
-  description: "
+  description: "Dilute the overnight culture"
+  note: "Prewarm 50mL YPAD in baffled flask(s) for 15-30 minutes in the shaking incubator"
+  note: "Dilute the culture using a 1:50 or 1:100 ratio with 50mL of prewarmed YPAD"
+end
+
+step: 
+  description: "Put diluted cells into shaking incubator"
+  note: "Wait 5-6 hours (1:50 dilution) or 6.5-8 hours (1:100 dilution) before harvesting the cells growing in the incubator"
+end
+
+step:
+  description: "Spin down cells"
+  note: "transfer contents of baffled flask(s) to 50mL or 250mL falcon tubes."
+  note: "Spin down in large centrifuge at 3,000xg for 5 minutes at 20 degrees."
+end
+
+step:
+  description:
+  note: "
+  
+  
+  
+  
+  
