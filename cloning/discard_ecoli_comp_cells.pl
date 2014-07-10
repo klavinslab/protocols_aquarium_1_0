1fb2c35694e66f846104a37164c88c73cbb92c24
tamimeur <tamimeur@gmail.com>
2014-07-10 11:58:05 -0700

    Update discard_ecoli_comp_cells.pl

--- cloning/discard_ecoli_comp_cells.pl
+++ cloning/discard_ecoli_comp_cells.pl
@@ -2,14 +2,25 @@ argument
   e_coli_strain_id: number, "The ID of the comp cell box/batch to discard."
 end
 
+take
+  discard = item {%e_coli_strain_id}
+end
+
 step
   description: "Throw away aliquots in box {%e_coli_strain_id}
   note: "Go to the minus 80 freezer, fourth shelf and discard the ecoli aliquots in box ID {%e_coli_strain_id}"
 end
 
-step
-  description: "Delete aliquots in box {%e_coli_strain_id} from the inventory."
-  note: "Go to Inventory->Basic->Cloning and delete the electrocompetent aliquot with ID {%e_coli_strain_id}"
+#step
+#  description: "Delete aliquots in box {%e_coli_strain_id} from the inventory."
+#  note: "Go to Inventory->Basic->Cloning and delete the electrocompetent aliquot with ID {%e_coli_strain_id}"
+#end
+
+
+modify 
+    discard[0]
+    location: "deleted"
+    inuse: 0
 end
 
 
