82861ccfedbc0a393f4fe391e857575d10bd8f7d
tamimeur <tamimeur@gmail.com>
2014-07-10 11:39:40 -0700

    Create take_error.pl

---
+++ cloning/take_error.pl
@@ -0,0 +1,18 @@
+
+take
+  x = item 26450
+end
+
+take
+  y = 3 "DH5alpha Electrocompetent Aliquot"
+end
+
+take
+  z = 3 item 26450
+end
+
+take
+  a = item [26450,26450,26450]
+end
+
+return [x[0],y[0],z[0],a[0]]
