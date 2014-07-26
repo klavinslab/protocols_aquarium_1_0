d13020c2f0431cd07496a9e9bf4be396b0e586b3
Chris Takahashi <cnt@uw.edu>
2014-07-25 22:00:39 -0700

    modified protocol

--- recombineering/validation/pick_colonies.pl
+++ recombineering/validation/pick_colonies.pl
@@ -11,37 +11,41 @@ end
 foreach p in taken_plates
   pid = p[:id]
   step
-    description: "Take the plate with ID %{pid}"
-    note: "Take the plate with id %{pid} and set it in front of you agar side up."
+    description: "Inspect the plate with ID %{pid}"
+    getdata 
+      count: number, "Enter the approximate number of colonies (eg 1,2,3,10,50,100,1000)"
+    end
   end
   
-  step
-    description: "Collect %{n_col} 1.5 mL tubes and place them in a tube rack."
-  end
-
-  ii = 0
-  while ii<n_col
+  if n>0
     step
-      description: "pipette 1 mL of LB into an empty tube"
+      description: "Collect %{n_col} 1.5 mL tubes and place them in a tube rack."
     end
 
-    produce silently
-      tube = 1 "Overnight suspension" from p
-      location: "SI4"
-    end
+    ii = 0
+    while ii<n_col
+      step
+        description: "pipette 1 mL of LB into an empty tube"
+      end
+  
+      produce silently
+        tube = 1 "Overnight suspension" from p
+        location: "SI4"
+      end
 
-    step
-      description: "Pick a colony"
-      note: "Read these steps once through first"
-      bullet: "Grab a pipette tip from the green box with your dominant hand.  Pick up the agar side of the plate infront of you with your other hand."
-      bullet: "Select an isolated colony to pick."
-      bullet: "Using the pointy end of the pipett tip, scrape the colony you just choose with a short stroke.  It may not look like you picked up much."
-      bullet: "Dip the tip into the 1.5 mL tube of LB below the liquid surface and scape it along the side to dislodge any cells."
-      bullet: "Cap the tube and write the item number " + to_string(tube[:id]) + " on top of a sticker on the top of the tube."
-      warning: "make sure to use sterile technique and to never touch the end of the pipette tip to anything but a single colony and a single tube"
+      step
+        description: "Pick a colony"
+        note: "Read these steps once through first"
+        bullet: "Grab a pipette tip from the green box with your dominant hand.  Pick up the agar side of the plate infront of you with your other hand."
+        bullet: "Select an isolated colony to pick."
+        bullet: "Using the pointy end of the pipett tip, scrape the colony you just choose with a short stroke.  It may not look like you picked up much."
+        bullet: "Dip the tip into the 1.5 mL tube of LB below the liquid surface and scape it along the side to dislodge any cells."
+        bullet: "Cap the tube and write the item number " + to_string(tube[:id]) + " on top of a sticker on the top of the tube."
+        warning: "make sure to use sterile technique and to never touch the end of the pipette tip to anything but a single colony and a single tube"
+      end
+      
+      ii = ii+1
     end
-    
-    ii = ii+1
   end
 end
 
