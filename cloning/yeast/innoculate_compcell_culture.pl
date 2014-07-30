c43bb3ddec9f7bee48af4847c85e0c1df348c4fd
arjunkhakhar <arjunkhakhar@gmail.com>
2014-07-30 15:34:44 -0700

    Update innoculate_compcell_culture.pl

--- cloning/yeast/innoculate_compcell_culture.pl
+++ cloning/yeast/innoculate_compcell_culture.pl
@@ -6,7 +6,7 @@ number = length(strains_id)
 
 take
   number "250 mL Baffled Flask"
-  1 "800 mL YPAD liquid (sterile)"
+  ypad = 1 "800 mL YPAD liquid (sterile)"
 end
 
 step
@@ -18,10 +18,12 @@ foreach x in strains_id
   step
     description: "Making culture flasks up."
     check: "Add 50ml of YPAD media to the 250ml baffeled flask using a serological pipette"
-    check: "Label the flask with a peice of tape that says %{x}"
+    check: "Put a peice of tape on its side for labeling"
     note: "MAKE SURE TO NOT TOUCH THE TIP OF THE PIPETTE TO ANYTHING (Tubes, bottle etc.)."
   end
 end
+
+release ypad
 
 step
   description: "Take flasks to cloning bay."
@@ -31,26 +33,25 @@ take
   strains = item unique(strains_id)
 end
 
-foreach x in strains_id
+
+foreach x in strains
   step
     description: "Innoculating Cultures"
-    check: "Add 1ml of the overnight labeled %{x} to one of the unlabeled flasks."
-    check: "Label the peice of tape with the ID number %{x}"
+    check: "Add 1ml of the overnight labeled %{x[:id]} to one of the unlabeled flasks."
+      produce
+        y = 1 "Yeast 50ml culture" from x
+        location: "30C Shaker"
+      end
+     check: "Label the flask %{y[:id]}." 
     note: "MAKE SURE TO NOT TOUCH THE TIP OF THE PIPETTE TO ANYTHING (Tubes, bottle etc.)."
   end
 end
+
 
 step
   description: "Grow cultures for 5 hours"
   check: "Put flasks in the 30C shaker."
   check: "Set a timer for 5 hours."
-end
-
-foreach x in strains
-  produce
-    1 "Yeast 50ml culture" from x
-    location: "30C Shaker"
-  end
 end
 
 
