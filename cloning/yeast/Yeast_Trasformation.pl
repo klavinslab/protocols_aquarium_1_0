cf59a47d35b805de476b9ec9daebc6bf3500f531
MilesGander <gandermiles@gmail.com>
2014-07-30 12:02:21 -0700

    Update Yeast_Trasformation.pl

--- cloning/yeast/Yeast_Trasformation.pl
+++ cloning/yeast/Yeast_Trasformation.pl
@@ -124,7 +124,7 @@ while x < y
   produce
     q = 1 "Yeast Plate" of products[x]
     location: "30º Incubator"
-    release [frozen_aliquots[x]]
+    release frozen_aliquots[x]
   end
   x = x+1
 end
