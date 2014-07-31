859b78a8fc17dee47feb527f695350567e6cb6a8
arjunkhakhar <arjunkhakhar@gmail.com>
2014-07-31 05:58:07 -0700

    Update check_yeast_plates.pl

--- cloning/yeast/check_yeast_plates.pl
+++ cloning/yeast/check_yeast_plates.pl
@@ -15,15 +15,18 @@ step
 end
 
 step
+  description: "a"
   check: "Look at each plate carefully. Seperate the ones that have colonies from the ones that dont."
   check: "Get a rough estimate of the number of colonies on each plate. If there are less than 20 count them and write the number down on the plate, if not then simply seperate them into a more than 20 pile"
 end
 
 step
+  description: "a"
   check: "Email the grad student whose initials are on the plates with a report. The format should be plate_id- # of colonies (0, a count for less than 20 or just more than 20). AJ/AK - arjunkhakhar at gmail.com, MG - miles, DY - david, NB- nick"
 end
 
 step
+  description: "a"
   check: "Wrap the plates that have colonies in parafilm and put in the fridge according to their location in aquarium, leave the plates without colonies on the benchtop"
 end
 
