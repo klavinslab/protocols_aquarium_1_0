12692b56c22d32efbd4473e10b6264d85322000e
tamimeur <tamimeur@gmail.com>
2014-07-31 13:28:54 -0700

    Update check_yeast_plates.pl

--- cloning/yeast/check_yeast_plates.pl
+++ cloning/yeast/check_yeast_plates.pl
@@ -2,16 +2,16 @@ argument
   yeast_plate_ids: sample array, "Enter the plates which you want to be checked."
 end
 
-tab = []
+#tab = []
 
-foreach x in yeast_plate_ids
-  tab = append(tab,x)
-end
+#foreach x in yeast_plate_ids
+#  tab = append(tab,x)
+#end
 
 step
   description: "Retrieve plate"
   check: "Go to 30C incubator and remove the plates with the following ID numbers"
-  table: tab
+#  table: tab
 end
 
 step
@@ -20,13 +20,13 @@ step
   check: "Get a rough estimate of the number of colonies on each plate. If there are less than 20 count them and write the number down on the plate, if not then simply seperate them into a more than 20 pile"
 end
 
-step
-  description: "a"
-  check: "Email the grad student whose initials are on the plates with a report. The format should be plate_id- # of colonies (0, a count for less than 20 or just more than 20). AJ/AK - arjunkhakhar at gmail.com, MG - miles, DY - david, NB- nick"
-end
+#step
+#  description: "a"
+#  check: "Email the grad student whose initials are on the plates with a report. The format should be plate_id- # of colonies (0, a count for less than 20 or just more than 20). AJ/AK - arjunkhakhar at gmail.com, MG - miles, DY - david, NB- nick"
+#end
 
-step
-  description: "a"
-  check: "Wrap the plates that have colonies in parafilm and put in the fridge according to their location in aquarium, leave the plates without colonies on the benchtop"
-end
+#step
+#  description: "a"
+#  check: "Wrap the plates that have colonies in parafilm and put in the fridge according to their location in aquarium, leave the plates without colonies on the benchtop"
+#end
 
