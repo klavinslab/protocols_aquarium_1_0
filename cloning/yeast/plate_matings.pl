d92b25ac12be7a7f2a186532aec967e855163d10
arjunkhakhar <arjunkhakhar@gmail.com>
2014-07-31 01:17:21 -0700

    Update plate_matings.pl

--- cloning/yeast/plate_matings.pl
+++ cloning/yeast/plate_matings.pl
@@ -11,6 +11,7 @@ end
 
 counter=0
 foreach z in matings
+  mid=matings_ids[counter]
   if plate_type[counter]==1
     take
       1 "SDO -Ura -Trp Plate (sterile)"
@@ -45,7 +46,7 @@ foreach z in matings
   step
     description: "Plating the mating"
     check: "Vortex the mating on setting 4 to resuspend yeast"
-    check: "Pipette 10ul of %{matings_ids[counter]} onto the plate near, but not too close, to the edge of the plate."
+    check: "Pipette 10ul of %{mid} onto the plate near, but not too close, to the edge of the plate."
     check: "Take a 10ul pipette tip in you hand and dip the tip into the droplet of yeast culture. Pull the tip down the plate in a zig-zag pattern gently so as not to rip the agar."
   end
   
