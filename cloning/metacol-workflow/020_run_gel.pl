ca082e6afa0f8959e05c478dede99a01348ee00e
arjunkhakhar <arjunkhakhar@gmail.com>
2014-08-01 07:09:59 -0700

    Update 020_run_gel.pl

--- cloning/metacol-workflow/020_run_gel.pl
+++ cloning/metacol-workflow/020_run_gel.pl
@@ -1,7 +1,7 @@
 information "Load and run an agarose gel."
 
 argument
-  fragment_names: sample("Fragment") array, "Fragment Names"
+  fragment_names: sample array, "Fragment Names"
   ladder_one: sample
 end
 
