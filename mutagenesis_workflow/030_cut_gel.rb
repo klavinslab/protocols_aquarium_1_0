baf7c772f17a3596232a1334d600493db8e344fa
sunshineyyy <yaoyu.yang88@gmail.com>
2014-08-29 16:54:57 -0700

    update

--- mutagenesis_workflow/030_cut_gel.rb
+++ mutagenesis_workflow/030_cut_gel.rb
@@ -39,7 +39,7 @@ class Protocol
 
 
     show {
-      title "Take out #{num_samples} 1.5 mL tubes and label each #{gel_slice_list} and #{size_list}"
+      title "Take out #{num_samples} 1.5 mL tubes and label each #{gel_slice_list.collect { |g| " #{g}" }} and #{size_list.collect {|s| "#{s}"}}"
       note "Take out #{num_samples}  1.5 mL tubes and label them (ON THE SIDE) 1 through #{num_samples}."
     }
 
