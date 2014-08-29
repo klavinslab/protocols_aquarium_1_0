9a442038e9459586fee24ad2897b1444d3d303e5
sunshineyyy <yaoyu.yang88@gmail.com>
2014-08-29 10:27:57 -0700

    update

--- mutagenesis_workflow/030_cut_gel.rb
+++ mutagenesis_workflow/030_cut_gel.rb
@@ -13,16 +13,21 @@ class Protocol
       volume: 50       # The volume of PCR fragment to load in µL
     }
   end
+
+  def main
   
-  stripwells = input[:stripwell_ids].collect { |sid| collection_from sid }
-  gels = input[:gels_ids].collect { |gid| collection_from gid }
+    stripwells = input[:stripwell_ids].collect { |sid| collection_from sid }
+    gels = input[:gels_ids].collect { |gid| collection_from gid }
 
-  take gels
-    
-  num_samples = stripwells.inject(0) { |sum,sw| sum + sw.num_samples }
+    take gels
+      
+    num_samples = stripwells.inject(0) { |sum,sw| sum + sw.num_samples }
 
-  show {
-    title "Take out #{num_samples} 1.5 mL tubes and label each"
-    note "Take out #{num_samples}  1.5 mL tubes and label them (ON THE SIDE) 1 through #{num_samples}."
-  }
+    show {
+      title "Take out #{num_samples} 1.5 mL tubes and label each"
+      note "Take out #{num_samples}  1.5 mL tubes and label them (ON THE SIDE) 1 through #{num_samples}."
+    }
+
+  end
+
 end
