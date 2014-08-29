1680677a81d07b6a78c2f608604e1f4895da90ef
sunshineyyy <yaoyu.yang88@gmail.com>
2014-08-28 23:28:46 -0700

    update

--- mutagenesis_workflow/010_PCR.rb
+++ mutagenesis_workflow/010_PCR.rb
@@ -42,7 +42,7 @@ class Protocol
     show {
       title "Fragment Information"
       note "This protocol will build the following fragments:"
-      note "templates.collect { |t| " #{t}" }
+      note templates.collect { |t| " #{t}" }
       note (fragments.collect { |f| " #{f}" })
       if not_ready.any?
         separator
