e09a7fbf3d45ed368ce977b9e3fba036b1ec3ffc
sunshineyyy <yaoyu.yang88@gmail.com>
2014-08-29 11:41:37 -0700

    update

--- mutagenesis_workflow/005_rehydrate_primers.rb
+++ mutagenesis_workflow/005_rehydrate_primers.rb
@@ -15,14 +15,20 @@ class Protocol
   end
 
   def main
+  	primer_id_list = []
   	primer_stock_list = []
+
+  	input[:primer_ids].each do |pid|
+  		primer_id_list.push pid
+  	end
+
   	show {
-  	  input[:primer_ids].each do |pid|
+  	  primer_id_list.each do |pid|
 		  primer = find(:sample,{id: pid})[0]
 		  name = primer.name
 		  primer_stock = produce new_sample name, of: "Primer", as: "Primer Stock"
 		  #primer_stock_id = primer_stock.id		  
-		  primer_stock_list = primer_stock_list.push primer_stock
+		  primer_stock_list.push primer_stock
 		  note "Label Primer with item number on the lid"
 	  end
   	}
