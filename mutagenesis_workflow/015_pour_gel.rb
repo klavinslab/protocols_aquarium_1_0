416ebbeab3cc4fcf2627d896f3b6d5f3b35c4e3c
sunshineyyy <yaoyu.yang88@gmail.com>
2014-08-29 10:44:05 -0700

    update

--- mutagenesis_workflow/015_pour_gel.rb
+++ mutagenesis_workflow/015_pour_gel.rb
@@ -26,7 +26,7 @@ class Protocol
     comb_1 = input[:comb_1] || 2
     comb_2 = input[:comb_2] || 2
     percentage = input[:percentage] || 1
-    stripwells = input[:stripwell_ids].collect { |sid| sid }
+    stripwells = input[:stripwell_ids].collect { |sid| collection_from sid }
 
     take stripwells
 
@@ -177,7 +177,7 @@ class Protocol
 
     release stripwells
 
-    return input.merge gel_ids: gels
+    return input.merge gels_ids: gels
 
   end
 
--- mutagenesis_workflow/020_run_gel.rb
+++ mutagenesis_workflow/020_run_gel.rb
@@ -19,7 +19,7 @@ class Protocol
   def main
 
     stripwells = input[:stripwell_ids].collect { |i| collection_from i }
-    gels = input[:gel_ids].collect { |i| collection_from i }
+    gels = input[:gels_ids].collect { |i| collection_from i }
     volume = input[:volume] || 20
 
     take stripwells + gels
