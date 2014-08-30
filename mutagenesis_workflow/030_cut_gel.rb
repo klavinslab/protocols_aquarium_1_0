7726c853a67f1d8f9befaa31b7ab41df5a7ceb37
sunshineyyy <yaoyu.yang88@gmail.com>
2014-08-29 18:23:53 -0700

    update

--- mutagenesis_workflow/030_cut_gel.rb
+++ mutagenesis_workflow/030_cut_gel.rb
@@ -22,26 +22,26 @@ class Protocol
     stripwells = input[:stripwell_ids].collect { |sid| collection_from sid }
 
     take gels, interactive: true
-      
+
     num_samples = stripwells.inject(0) { |sum,sw| sum + sw.num_samples }
 
-    gel_slice_list = []
+    gel_slice_ids = []
     size_list = []
 
     input[:fragment_ids].each do |fid|
       fragment = find(:sample,{id: fid})[0]
       name = fragment.name
       prop = fragment.properties
-      size = prop["Length"]
+      size = prop["Forward Primer"]
       gel_slice = produce new_sample name, of: "Fragment", as: "Gel Slice"
-      gel_slice_list = gel_slice_list.push gel_slice
+      gel_slice_ids = gel_slice_ids.push gel_slice
       size_list = size_list.push size
     end
 
 
     show {
       title "Take out #{num_samples} 1.5 mL tubes and label each"
-      note gel_slice_list.collect { |g| "#{g}" } 
+      note gel_slice_ids.collect { |g| "#{g}" } 
       note size_list.collect {|s| "#{s}"}
       note "Take out #{num_samples}  1.5 mL tubes and label them (ON THE SIDE) 1 through #{num_samples}."
     }
