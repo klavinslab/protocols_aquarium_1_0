3614fcdec595d25fa1f753b9d380290fc339b744
sunshineyyy <yaoyu.yang88@gmail.com>
2014-09-12 11:26:48 -0700

    update

--- mutagenesis_workflow/test_take_show.rb
+++ mutagenesis_workflow/test_take_show.rb
@@ -17,9 +17,10 @@ class Protocol
       warning "Do not leave the freezer open too long!"
     }
 
+    items_by_id = find(:item, id: 761)
     items = find(:item, object_type: {name: "GelGreen Nucleic Acid Stain"})
 
-    take items, interactive: true
+    take items_by_id, interactive: true
 
     gel_green = choose_object "500 mL Bottle"
 
