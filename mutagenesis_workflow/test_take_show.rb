5fa30ed9ef820aa2dd052502934c678f47fdb843
sunshineyyy <yaoyu.yang88@gmail.com>
2014-09-12 00:19:04 -0700

    update

--- mutagenesis_workflow/test_take_show.rb
+++ mutagenesis_workflow/test_take_show.rb
@@ -13,7 +13,7 @@ class Protocol
   def main
 
     fragment = find(:sample,{id: 2048})[0]
-    take (fragment, interactive: true,  method: "boxes")
+    take fragment, interactive: true,  method: "boxes"
   end
 
 end
