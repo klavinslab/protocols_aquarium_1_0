adbe643b6fa914bc2ca35c57b9536429f7ffdcd8
sunshineyyy <yaoyu.yang88@gmail.com>
2014-09-12 00:22:38 -0700

    update

--- mutagenesis_workflow/test_take_show.rb
+++ mutagenesis_workflow/test_take_show.rb
@@ -13,7 +13,7 @@ class Protocol
   def main
 
     items = find(:item, { sample: { name: "pLAB1" }, object_type: { name: "Plasmid Stock" } } )
-    take(items, interactive: true) {
+    take(items, interactive: true, method: "boxes") {
       warning "Do not leave the freezer open too long!"
     }
 
