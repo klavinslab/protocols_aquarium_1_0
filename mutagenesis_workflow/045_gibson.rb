89ac3d0abf00723c2ab2c8ad2cceb4a448f234d7
sunshineyyy <yaoyu.yang88@gmail.com>
2014-09-17 13:36:31 -0700

    update

--- mutagenesis_workflow/045_gibson.rb
+++ mutagenesis_workflow/045_gibson.rb
@@ -78,32 +78,34 @@ class Protocol
       plasmid_fragment_volume[pid] = volumes
     end
 
-    # old fashioned way
-    length = fragment_info_list.collect { |fi| fi[:length] }
-    stock  = fragment_info_list.collect { |fi| fi[:stock] }
-    conc   = fragment_info_list.collect { |fi| fi[:conc] }
-    conc_over_length = conc.map.with_index {|c,i| c/length[i]}
+    # # old fashioned way
+    # length = fragment_info_list.collect { |fi| fi[:length] }
+    # stock  = fragment_info_list.collect { |fi| fi[:stock] }
+    # conc   = fragment_info_list.collect { |fi| fi[:conc] }
+    # conc_over_length = conc.map.with_index {|c,i| c/length[i]}
 
-    # calculate volumes to add for each fragment stock assuming 5 µL of total volume
-    total_vector = Matrix.build(conc.length, 1) {|row, col| gibson_vector row}
-    coefficient_matrix = Matrix.build(conc.length, conc.length) {|row, col| gibson_coefficients row, col, conc_over_length}
-    volume_vector = coefficient_matrix.inv * total_vector
-    volumes = volume_vector.each.to_a
+    # # calculate volumes to add for each fragment stock assuming 5 µL of total volume
+    # total_vector = Matrix.build(conc.length, 1) {|row, col| gibson_vector row}
+    # coefficient_matrix = Matrix.build(conc.length, conc.length) {|row, col| gibson_coefficients row, col, conc_over_length}
+    # volume_vector = coefficient_matrix.inv * total_vector
+    # volumes = volume_vector.each.to_a
 
 
     # Tell the user what we are doing
     show {
       title "Fragment Information"
-      note (length.collect {|l| "#{l}"})
-      note (conc.collect {|c| "#{c}"})
-      note (total_vector.collect {|t| "#{t}"})
-      note (conc_over_length.collect {|cl| "#{cl}"})
-      note (coefficient_matrix.each {|e| "#{e}"})
-      note (volumes.collect {|v| "#{v.round(1)}"})
-      note (plasmid_uniq.collect {|p| "#{p}"})
-      note (plasmid_ids.collect {|p| "#{p}"})
-      note (plasmid_fragment_conc_over_length[1923].collect {|p| "#{p}"})
-      note (plasmid_fragment_volume[1923].collect {|p| "#{p}"})
+      note "This protocol will build the following plasmids using Gibson Assembly method"
+      note plasmid_uniq.collect {|p| "#{p}"}
+      # note (length.collect {|l| "#{l}"})
+      # note (conc.collect {|c| "#{c}"})
+      # note (total_vector.collect {|t| "#{t}"})
+      # note (conc_over_length.collect {|cl| "#{cl}"})
+      # note (coefficient_matrix.each {|e| "#{e}"})
+      # note (volumes.collect {|v| "#{v.round(1)}"})
+      # note (plasmid_uniq.collect {|p| "#{p}"})
+      # note (plasmid_ids.collect {|p| "#{p}"})
+      # note (plasmid_fragment_conc_over_length[1923].collect {|p| "#{p}"})
+      # note (plasmid_fragment_volume[1923].collect {|p| "#{p}"})
     }
     
     # produce gibson results ids
@@ -119,7 +121,7 @@ class Protocol
     show {
       title "Take Gibson Aliquots and label them with ids"
       note "Take #{plasmid_uniq.length} Gibson Aliquots"
-      note "Write" + gibson_ids.collect {|gid| "#{gid}"}.to_s + "on top of each Gibson Aliquot tube"
+      # note "Write" + gibson_ids.collect {|gid| "#{gid}"}.to_s + "on top of each Gibson Aliquot tube"
       note "Label each unused Gibson Aliquot with the following ids using round dot labels"
       note (gibson_results_list.collect {|gid| "#{gid}"})
       # gibson_results_list.each do |gsid|
