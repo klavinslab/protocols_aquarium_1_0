712283366163717caed73340c7761b7dd5ac3a8c
sunshineyyy <yaoyu.yang88@gmail.com>
2014-09-17 15:26:34 -0700

    update

--- mutagenesis_workflow/test_find.rb
+++ mutagenesis_workflow/test_find.rb
@@ -5,6 +5,8 @@ class Protocol
     i1 = find(:item, object_type: {name: "GelGreen Nucleic Acid Stain"})
     i2 = find(:item, id: 374)
 
+    take i1
+
     show {
       title "Items"
       table i1.collect { |i| [ i.id, i.object_type.name ] }
