431701b3dad1808e0b40c3fcb8ca96cc4e109d53
arjunkhakhar <arjunkhakhar@gmail.com>
2014-09-17 21:32:07 -0700

    Update make_glycerol_stock_yeast.rb

--- cloning/Krill_Workflows/Yeast_Workflow/make_glycerol_stock_yeast.rb
+++ cloning/Krill_Workflows/Yeast_Workflow/make_glycerol_stock_yeast.rb
@@ -9,14 +9,42 @@ class Protocol
 
   def arguments
     {
-      overnights: (SampleType.where("name='Overnight suspension culture'")[0])
+      overnights:SampleType.where("name='Yeast Overnight'")[0]
     }
   end
   
   def main
-
     
-  
+      overnights = find(:item, { sample: input[:overnights] } )
+      
+      take overnights
+      
+      number_overnights = overnights.length
+      
+      glycerol = choose_sample "50 percent Glycerol (sterile)"
+      
+      show 
+      {
+        title "Pipette 900 µL of 50 percent Glycerol stock into #{number_overnights} Cyro tube(s)."
+        warning "Make sure not to touch the inner side of the Glycerol bottle with the pipetter."
+      }
+      
+    
+      (overnights).each do |overnight|
+        
+        show 
+        {
+          check "Pipette 900 µL of yeast overnight #{overnight.id} into a Cyro tube."
+          check "Cap the Cryo tube and then vortex on a table top vortexer for about 20 seconds"
+        }
+        
+        j = produce new_sample overnight, of: "Yeast Overnight", as: "Yeast Glycerol Stock"
+        
+        release[j]
+        
+      end
+      
+      release [ overnights glycerol]
   end
   
 end
