2e297d386953d466b347c4108ed4d5af4e9a0680
arjunkhakhar <arjunkhakhar@gmail.com>
2014-09-17 21:33:35 -0700

    Update make_glycerol_stock_yeast.rb

--- cloning/Krill_Workflows/Yeast_Workflow/make_glycerol_stock_yeast.rb
+++ cloning/Krill_Workflows/Yeast_Workflow/make_glycerol_stock_yeast.rb
@@ -25,7 +25,7 @@ class Protocol
       
       show 
       {
-        title "Pipette 900 µL of 50 percent Glycerol stock into #{number_overnights} Cyro tube(s)."
+        title "Pipette 900 µL of 50 percent Glycerol stock into Cyro tube(s)."
         warning "Make sure not to touch the inner side of the Glycerol bottle with the pipetter."
       }
       
@@ -34,7 +34,7 @@ class Protocol
         
         show 
         {
-          check "Pipette 900 µL of yeast overnight #{overnight.id} into a Cyro tube."
+          check "Pipette 900 µL of yeast overnight into a Cyro tube."
           check "Cap the Cryo tube and then vortex on a table top vortexer for about 20 seconds"
         }
         
