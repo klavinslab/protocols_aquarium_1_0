24538db507257c3c1ff7d1b95880eda8b1f5c207
arjunkhakhar <arjunkhakhar@gmail.com>
2014-09-17 20:31:51 -0700

    Update make_glycerol_stock_yeast.rb

--- cloning/Krill_Workflows/Yeast_Workflow/make_glycerol_stock_yeast.rb
+++ cloning/Krill_Workflows/Yeast_Workflow/make_glycerol_stock_yeast.rb
@@ -1,1 +1,22 @@
-abcd
+needs "aqualib/lib/standard"
+needs "aqualib/lib/cloning"
+
+class Protocol
+
+  include Standard
+  include Cloning
+
+
+  def arguments
+    {
+      overnights: (SampleType.where("name='Overnight suspension culture'")[0])
+    }
+  end
+  
+  def main
+
+    
+  
+  end
+  
+end
