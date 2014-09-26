3ccda6b5e9546d5fb4ea552edb0d6f617e7a3519
pochekailov <s.pochekailov@gmail.com>
2014-09-25 22:19:38 -0700

    Create 025_origami_purification.pl

---
+++ origami/workflow/025_origami_purification.pl
@@ -0,0 +1,4 @@
+## This protocol starts with raw origami solution containing excess of staples,
+## and go through DNA origami purification. It ends with DNA origami purified from
+## staple strands.
+
