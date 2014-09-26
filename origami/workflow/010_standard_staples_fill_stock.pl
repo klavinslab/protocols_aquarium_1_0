a30fbcd05194a26aa27394d3bc1d15623299540c
pochekailov <s.pochekailov@gmail.com>
2014-09-25 22:15:24 -0700

    Create 010_standard_staples_fill_stock.pl

---
+++ origami/workflow/010_standard_staples_fill_stock.pl
@@ -0,0 +1,8 @@
+##DNA origami requires 3 kind of stocks for creation.
+##  1 - common stock of staples. Here are the staples, which will not be modified in any experiment.
+##  2 - filler stock of standard staples. Those are ones, which in other experiments may be substitured to custom
+##      staples, but in this particular experiment the standard ones are used.
+##  3 - stock of custom staples. Those are staples which are different from standard.
+##This protocol creates a stock of filler standard staples.
+##They are specific for a given experiment.
+
