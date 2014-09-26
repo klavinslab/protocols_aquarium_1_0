23eaa8973c147a67d26f8e916d005c3e6390bbe5
pochekailov <s.pochekailov@gmail.com>
2014-09-25 21:08:55 -0700

    Create 005_main_staple_stock.pl

---
+++ origami/workflow/005_main_staple_stock.pl
@@ -0,0 +1,4 @@
+##This protocol creates a stock of staple strands for DNA origami assembly
+##This is general stock, which you will use for all your origami experiments,
+##so exclude any staples you possibly want to modify.
+
