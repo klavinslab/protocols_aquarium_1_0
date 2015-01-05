b1f7545827859c35539768af526767399c75bdd2
lmbrettner <leandra.brettner@gmail.com>
2015-01-04 23:04:47 -0800

    Create safety.rb

---
+++ labw15/week1/safety.rb
@@ -0,0 +1,87 @@
+#information "This protocol teaches the essential safety practices and procedures of the lab."
+
+class Protocol
+	def main
+		show {
+		  title "No eating or drinking in the lab."
+		  note "This is a wet lab that works with live cultures. Do not bring food or drinks into the lab, 
+		  do not apply cosmetics in the lab, and do not make any other type of contact with mucous 
+		  (ex. contact lenses or touching one's mouth)."
+		  image  "sign_caution-chem-no-eat-drinking-area1-lg"
+		}
+
+		show {
+		  title "You must wash your hands"
+		  note "Before leaving the lab, immediately following contact with potentially infectious agents, 
+		  and immediately after taking off gloves or other protective equipment."
+		  image  "handwashing_sink_without_map"
+		}
+
+		show {
+		  title "In case of a fire be able to locate AND use fire extinguisher."
+		  note "Be aware when using flammable reagents. In case of a serious fire call 911. No smoking in the lab."
+		  image  "Fire_Extinguisher_without_map"
+		}
+
+		show {
+		  title "In case of a serious chemical spill be able to locate and operate both the eye wash station and chemical shower."
+		  note "Familiarize yourself with the directions."
+		  image "shower_and_eyewash_without_map"
+		}
+
+		show {
+		  title "All biologically contaminated disposables must be disposed in the biohazard waste."
+		  note "This includes items such as pipette tips, petri dishes, and gloves. Do not dispose of sharps in the biohazard waste bag."
+		  image "Biohazard_Box_without_map"
+		}
+
+		show {
+		  title "When using flammable, acidic, or basic reagents the chemicals MUST be used within the fume hood with the sash as low as possible."
+		  note "Always wear gloves when using ANY regeants in the fume hood. Be aware of which chemicals are present in the hood while doing any work within the fume hood."
+		  image "Fume_Hood_without_map"
+		}
+
+		show {
+		  title "In case of a cut, burn, or other health concern that arises while in the lab use one of the first aid kits."
+		  note "Familiarize yourself with the contents and locations of each first aid kit."
+		  image "First_aid_kits_without_map"
+		}
+
+		show {
+		  title "All broken glassware must be disposed of in broken glass container."
+		  note "Never place any broken glassware in the biohazard waste box, trash cans, or any other recepticles."
+		  image "Sharps_Container_without_map"
+		}
+
+		show {
+		  title "Sharps, such as needles, scalpels, microscope slides, razor blades etc. must be disposed of in sharps container."
+		  note "Never place any sharps in the biohazard waste box, trash cans, or any other recepticles."
+		  image "Sharps_Container_without_map"
+		}
+
+		show {
+		  title "All dirty glassware should be cleaned at the dishwashing station"
+		  note " Do not place dirty glassware with clean items that may be hung and drying or drying in the drying area. To clean up /decontaminate a biohazard spill, first dispose of biohazard waste in biohazard bag and then to decontaminate a surface or disposable allow the object in question to soak in 10 percent bleach solution for 20min then dispose of bleached waste in regular waste stream."
+		  image "dishwashing_sink_without_map"
+		}
+
+		show {
+		  title "UV Shield must be used when using UV transilluminator."
+		  note "Do not look directly at or expose skin to UV light as it may cause permanent damage."
+		  image "UV_transilluminator_without_map"
+		}
+
+		show {
+		  title "Gloves must be used when using potentially dangerous chemicals."
+		  note "Dispose of goves after each use when handling dangerous chemicals and/or potentially infectious agents. Immediately replace punctured or torn gloves. All gloves must be disposed of in the biohazard box. "
+		  image "Latex_Gloves_without_map"
+		}
+
+		show {
+		  title "Eye Protection"
+		  note "Eye Protection must be: used during any activites with risk of splashing or aerosolization and as required by laboratory instructor."
+		  image  "eye_protection"
+		}
+	end
+end
+
