d13d7983226218743545d38cddb52746b2a28724
Chris Takahashi <cnt@uw.edu>
2015-01-13 17:20:26 -0800

    working boil cells

--- recombineering/validation/boil_cells.rb
+++ recombineering/validation/boil_cells.rb
@@ -7,7 +7,7 @@ class Protocol
   end
 
   def arguments
-    { overnight_ids: [0] }
+    { overnight_ids: [14947,14948,13992,13993] }
   end
   def main
     oNight_ids = input[:overnight_ids]
@@ -23,7 +23,7 @@ class Protocol
     tube_no=0
     overnights.each do |overnight|
       tube_no = tube_no +1
-      tbl << [overnight,tube_no]
+      tbl << [overnight.id,tube_no]
     end
     
     show {
@@ -47,32 +47,85 @@ class Protocol
     end
 
     n_stripwell = overnights.length/12+1
-    current_SW = 0
-    filled_SWs = []
-    
-    #move overnights to a stripwell
-    filled_SWs = produce spread overnights, "Stripwell",1,12
+    onight_samples = overnights.map{|x| x.sample}
+
+    labels = ["A"]
+    label = labels[0]
+    while labels.length<n_stripwell do
+      labels.append label.next!
+    end
 
     show {
-      title "Transfer to a PCR strip well tube."
-      note "transfer 100 &micro;L from tubes 1-#{overnights.length} to wells 
-        1-#{overnights.length} marking them appropriatly and using additional
-        strips if necessary.  Throw out the centrifuge tubes and remaining 
-        cells once you're done with this step."
-      warning "It is important to transfer 1 to 1, 2 to 2 so samples don't 
-        get confused"
+      title "take #{n_stripwell} stripwells"
+      note "take #{n_stripwell} stripwell tubes and label each strip according
+      to the table below:"
+      table [labels]
     }
+
+    ii = 0
+    move_table = [["1.5ml tube","stripwell","position"]]
+    while ii<overnights.length do
+      sw = ii/12
+      pos = ii%12+1
+      move_table.push([ii+1,labels[sw],pos.to_s])
+      ii=ii+1
+    end
+
+    show {
+      title "Transfer to strip well tubes."
+      note "transfer 100 &micro;L from tubes 1-#{overnights.length} to the 
+        stripwell according to the table below."
+      table move_table
+    }
+ 
     show {
       title "Boil cells"
       note "Place the strip wells into a free thermocycler and find a protocol 
           labeled boil.  Adjust parameters to 95C and 5 min if necessary."
-      check "while you wait, label #{overnights.length} new 1.5 mL centrifuge 
-        tubes 1-#{overnights.length}"
+      note "while you wait:"
+      check "discard any 1.5ml tubes you may still have laying around"
+      check "label #{overnights.length} new 1.5 mL centrifuge 
+        tubes 1 through #{overnights.length}."
     }
+
     show {
       title "Transfer back to centrifuge tubes."
       note "Transfer the entire contents (100 &micro;L) of each strip well 
-        into the tubes you just labeled."
+        into the tubes you just labeled according to the following table:"
+      table move_table
     }
+
+    genomes = []
+    overnights.each do |x|
+      genomes.append produce x.sample.make_item "Genome Prep"
+    end
+
+    tube_ids = genomes.map {|x| x.id.to_s}
+
+    show {
+      title "Spin at max RPM for 2 min"
+      note "Spin the 1.5ml tubes you just transferred at max RPM in the 
+      centrifuge"
+      note "while you wait:"
+      check "label #{overnights.length} 1.5 mL centrifuge tubes according to
+        the following table:"
+      table [tube_ids]
+    }
+
+    gp_table = (Array (1..tube_ids.length)).zip(tube_ids)
+
+    show {
+      title "CAREFULLY transfer supernatant to new tubes"
+      note "Very carefully, with minimal shock or vibration 
+        (pretend it's nitroglycerin), remove each tube from the centrifuge and 
+        transfer 50 &micro;L off the top to the tube you just labeled according
+        to the following table"
+      warning "Becareful not to disturb the pellet.  We don't want any of the 
+      pellet, which may be tiny, fragile, and invisible."
+      table gp_table
+    }
+
+    release genomes, interactive: true
+
   end
 end
