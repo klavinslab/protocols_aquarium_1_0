90e475cd2515bb9446604e6c6637f9554d708d92
Eric Klavins <klavins@uw.edu>
2016-05-18 10:44:22 -0700

    Updated protocols/developer/test1.rb

--- developer/test1.rb
+++ developer/test1.rb
@@ -2,19 +2,9 @@ class Protocol
 
   def main
 
-    o = op input
-
-    o.input.all.take
-    o.output.all.produce
-
     show do
-      title "Instructions here!"
+      title "Hello World!"
     end
-
-    o.input.all.release
-    o.output.all.release
-
-    return o.result
 
   end
 
