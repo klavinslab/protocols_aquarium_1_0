7aa2897cdbd15c6240c2f60af2eb520a95e97fb6
Eric Klavins <klavins@uw.edu>
2016-05-19 13:36:16 -0700

    Updated protocols/developer/test3.rb

--- developer/test3.rb
+++ developer/test3.rb
@@ -1,5 +1,19 @@
 class Protocol
 
+  def purchase_info ot
+
+    if ot.object_type[:materials] && ot.object_type[:labor]
+      "basic"
+    elsif ot.handler == "sample_container" && ot.object_type[:samples]
+      "sample"
+    elsif ot.handler == "collection" && ot.object_type[:samples]
+      "collection"
+    else
+      nil
+    end
+
+  end
+
   def main
 
     ots = ObjectType.all.select { |ot|
