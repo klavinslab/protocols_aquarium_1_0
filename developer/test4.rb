992d2b0434fea39adb9267274a271be21363f1c0
Eric Klavins <klavins@uw.edu>
2016-05-20 17:09:04 -0700

    Updated protocols/developer/test4.rb

--- developer/test4.rb
+++ developer/test4.rb
@@ -14,21 +14,25 @@ class Protocol
   def main
     yeast_strains = find(:sample, { sample_type: { name: "Yeast Strain" } })
     changes = []
+    i = 0
     yeast_strains.each do |yeast_strain|
-      description = yeast_strain.description
-      if description
-        if description.include? "QC_length"
-          length = description.split('QC_length')[-1].split(':')[-1].to_i
-          yeast_strain.set_property "QC_length", length
-          yeast_strain.save
-          changes.push "#{yeast_strain} QC_length changed"
-        end
-        if description.include? "comp_cell_limit: no"
-          yeast_strain.set_property "Comp_cell_limit", "No"
-          yeast_strain.save
-          changes.push "#{yeast_strain} Comp_cell_limit changed"
+      if i < 5
+        description = yeast_strain.description
+        if description
+          if description.include? "QC_length"
+            length = description.split('QC_length')[-1].split(':')[-1].to_i
+            yeast_strain.set_property "QC_length", length
+            yeast_strain.save
+            changes.push "#{yeast_strain} QC_length changed"
+          end
+          if description.include? "comp_cell_limit: no"
+            yeast_strain.set_property "Comp_cell_limit", "No"
+            yeast_strain.save
+            changes.push "#{yeast_strain} Comp_cell_limit changed"
+          end
         end
       end
+      i += 1
     end
     show {
       note yeast_strains.length
