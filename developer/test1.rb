5597bdc8172e9820c19f27b9ec951883e19c1ed7
Eric Klavins <klavins@uw.edu>
2016-05-23 10:09:49 -0700

    Repo::save: Updated protocols/developer/test1.rb via the Aquarium developer tool.

--- developer/test1.rb
+++ developer/test1.rb
@@ -2,12 +2,14 @@ class Protocol
 
   def main
 
+    x = 1/0
+
     show do
       title "Hello World!"
     end
     
     show do
-      title "Goodbye World!
+      title "Goodbye World!"
     end    
 
   end
