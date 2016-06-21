1a92f70d74146e219ef1ae254bfbe9cc063fe9f0
Eric Klavins <klavins@uw.edu>
2016-06-21 08:51:49 -0700

    Repo::save: Updated protocols/test.rb via the Aquarium developer tool.

---
+++ test.rb
@@ -0,0 +1,14 @@
+class Protocol
+    
+    def main
+        
+        a = arguments
+        
+        show do
+            title "Arguments"
+            note a
+        end
+        
+    end
+    
+end
