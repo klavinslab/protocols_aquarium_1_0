841b8128f854b8b6178137da0a9a3204a6c59f7e
Eric Klavins <klavins@uw.edu>
2016-07-28 11:54:07 -0700

    saving

--- planner/pcr.rb
+++ planner/pcr.rb
@@ -2,18 +2,20 @@ class Protocol
 
   def main
 
+    puts "Job #{jid}"
+
     Operation.where(job_id: jid).each do |op|
 
-      puts "\e[91mOperation #{op.id} (#{op.name})\e[39m"
+      puts "\e[97mOperation #{op.id} (#{op.name})\e[39m"
 
       op.inputs.each do |input|
         input.retrieve
-        puts "\e[91m  Input #{input.info}\e[39m"
+        puts "\e[97m  Input #{input.info}\e[39m"
       end
 
       op.outputs.each do |output|
         output.make
-        puts "\e[91m  Output #{output.info}\e[39m"
+        puts "\e[97m  Output #{output.info}\e[39m"
       end
 
     end
