4226e96a32af2fac0f7d59f111951c3f06b49694
Eric Klavins <klavins@uw.edu>
2016-07-28 12:47:42 -0700

    saving

--- planner/make_primer_aliquot.rb
+++ planner/make_primer_aliquot.rb
@@ -2,23 +2,25 @@ class Protocol
 
   def main
 
-    puts "Job #{jid}"
+    puts "\e[97mJob #{jid}\e[39m"
 
     Operation.where(job_id: jid).each do |op|
 
-      puts "\e[97mOperation #{op.id} (#{op.name} for #{op.user.name})\e[39m"
+      puts "\e[97m  Operation #{op.id} (#{op.name} for #{op.user.name})\e[39m"
 
       op.inputs.each do |input|
         input.retrieve
-        puts "\e[97m  Input #{input.info}\e[39m"
+        puts "\e[97m    Input #{input.info}\e[39m"
       end
 
       op.outputs.each do |output|
         output.make
-        puts "\e[97m  Output #{output.info}\e[39m"
+        puts "\e[97m    Made output #{output.info}\e[39m"
       end
 
     end
+
+    puts ""
 
     return {}
 
