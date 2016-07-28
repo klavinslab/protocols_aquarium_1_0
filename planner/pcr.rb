ebe6d2e420e5b37d06c99f6a422169c47ab2e508
Eric Klavins <klavins@uw.edu>
2016-07-28 11:21:40 -0700

    saving

--- planner/pcr.rb
+++ planner/pcr.rb
@@ -3,7 +3,10 @@ class Protocol
   def main
 
     Operation.where(job_id: jid).each do |op|
-      puts "\e[91mOperation #{op.id} is part of job #{jid}\e[39m"
+      puts "\e[91mOperation #{op.id} (#{op.name})\e[39m"
+      op.inputs.each do |input|
+        puts "\e[91mInput #{input.name}\e[39m"
+      end
     end
 
     return {}
