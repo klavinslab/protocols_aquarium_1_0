3c885301e77f46861344bdce8543cd4a4fc84aa1
Eric Klavins <klavins@uw.edu>
2016-08-02 20:42:31 -0700

    saving

---
+++ planner/make.rb
@@ -0,0 +1,20 @@
+protocols = [ 
+  "order_primer.rb",
+  "receive_primer.rb",
+  "make_primer_aliquot.rb",
+  "pcr.rb",
+  "run_gel.rb",
+  "extract_fragment.rb",
+  "purify_gel.rb",
+  "gibson_assembly.rb",
+  "transform_e_coli.rb",
+  "plate_e_coli.rb",
+  "e_coli_overnight.rb",
+  "miniprep.rb",
+  "sequencing.rb" ]
+
+  protocols.each do |p|
+
+    system("cp default.rb #{p}")
+
+  end
