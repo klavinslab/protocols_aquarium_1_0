3d3c24ea04c95c6ec8b042f5f06c54005590390c
Eric Klavins <klavins@uw.edu>
2016-08-03 10:01:57 -0700

    saving

--- planner/make_primer_aliquot.rb
+++ planner/make_primer_aliquot.rb
@@ -4,6 +4,9 @@ class Protocol
 
     puts "\e[97mJob #{jid}\e[39m"
 
+    input_collections = {}
+    output_collections = {}
+
     Operation.where(job_id: jid).each do |op|
 
       puts "\e[97m  Operation #{op.id} (#{op.name} for #{op.user.name})\e[39m"
@@ -16,7 +19,12 @@ class Protocol
       op.outputs.each do |output|
 
         if output.part?
+
           puts "  Got a part for #{output.name}. What to do??"
+          unless output_collections[output.name]
+            puts "  output.object_type.inspect"
+          end
+
         else
           output.make
           puts "\e[97m    Made output #{output.info}\e[39m"
